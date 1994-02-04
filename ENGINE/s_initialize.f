ad8b13e0d07694d55207a1246d02c40af0ddf143
cdaq <>
1994-02-04 20:47:40 +0000

    Add read titles to regpar calls

--- ENGINE/s_initialize.f
+++ ENGINE/s_initialize.f
@@ -11,9 +11,12 @@
 *-   Created  8-Nov-1993   Kevin B. Beard
 *-   Modified 20-Nov-1993  KBB for new errors
 *-    $Log$
-*-    Revision 1.2  1994/02/03 14:28:27  cdaq
-*-    Make clear that last arg of reg calls is a title.  Use null for now.
+*-    Revision 1.3  1994/02/04 20:47:40  cdaq
+*-    Add read titles to regpar calls
 *-
+c Revision 1.2  1994/02/03  14:28:27  cdaq
+c Make clear that last arg of reg calls is a title.  Use null for now.
+c
 c Revision 1.1  1994/02/02  21:37:55  cdaq
 c Initial revision
 c
@@ -34,41 +37,49 @@ c
        INCLUDE 'gen_data_structures.cmn'
        INCLUDE 'gen_routines.dec'
 *
-       INTEGER ierr,nulltitle
+       INTEGER ierr
+       CHARACTER*80 title
 *
 *--------------------------------------------------------
        err= ' '
 *
-       nulltitle = 0
-       ierr= regparmint('SMAX_DC_HITS',SMAX_DC_HITS,nulltitle)
+       title= 'SOS drift chamber max. # of hits'
+       ierr= regparmint('SMAX_DC_HITS',SMAX_DC_HITS,title)
        IF(ierr.NE.0) err= 'unable to register "SMAX_DC_HITS"'
        ABORT= ierr.NE.0
 *
-       ierr= regparmint('SNUM_DC_PLANES',SNUM_DC_PLANES,nulltitle)
+       title= 'SOS drift chamber number of planes'
+       ierr= regparmint('SNUM_DC_PLANES',SNUM_DC_PLANES,title)
        IF(ierr.NE.0) err= 'unable to register "SNUM_DC_PLANES"'
        ABORT= ierr.NE.0 .or. ABORT
 *
-       ierr= regparmint('SMAX_SCIN_HITS',SMAX_SCIN_HITS,nulltitle)
+       title= 'SOS scintillator max. # of hits'
+       ierr= regparmint('SMAX_SCIN_HITS',SMAX_SCIN_HITS,title)
        IF(ierr.NE.0) err= 'unable to register "SMAX_SCIN_HITS"'
        ABORT= ierr.NE.0 .or. ABORT
 *
-       ierr= regparmint('SNUM_SCIN_PLANES',SNUM_SCIN_PLANES,nulltitle)
+       title= 'SOS scintillator number of planes'
+       ierr= regparmint('SNUM_SCIN_PLANES',SNUM_SCIN_PLANES,title)
        IF(ierr.NE.0) err= 'unable to register "SNUM_SCIN_PLANES"'
        ABORT= ierr.NE.0 .or. ABORT
 *
-       ierr= regparmint('SMAX_CAL_BLOCKS',SMAX_CAL_BLOCKS,nulltitle)
+       title= 'SOS calorimeter max. # of hits'
+       ierr= regparmint('SMAX_CAL_BLOCKS',SMAX_CAL_BLOCKS,title)
        IF(ierr.NE.0) err= 'unable to register "SMAX_CAL_BLOCKS"'
        ABORT= ierr.NE.0 .or. ABORT
 *
-       ierr= regparmint('SMAX_CER_HITS',SMAX_CER_HITS,nulltitle)
+       title= 'SOS cerenkov max. # of hits'
+       ierr= regparmint('SMAX_CER_HITS',SMAX_CER_HITS,title)
        IF(ierr.NE.0) err= 'unable to register "SMAX_CER_HITS"'
        ABORT= ierr.NE.0 .or. ABORT
 *
-       ierr= regparmint('SNTRACKS_MAX',SNTRACKS_MAX,nulltitle)
+       title= 'SOS max. # of tracks'
+       ierr= regparmint('SNTRACKS_MAX',SNTRACKS_MAX,title)
        IF(ierr.NE.0) err= 'unable to register "SNTRACKS_MAX"'
        ABORT= ierr.NE.0 .or. ABORT
 *
-       ierr= regparmint('SNTRACKHITS_MAX',SNTRACKHITS_MAX,nulltitle)
+       title= 'SOS max. # of track hits'
+       ierr= regparmint('SNTRACKHITS_MAX',SNTRACKHITS_MAX,title)
        IF(ierr.NE.0) err= 'unable to register "SNTRACKHITS_MAX"'
        ABORT= ierr.NE.0 .or. ABORT
 *
