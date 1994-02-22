68f8bb9b3745ba899512ba55b857caac901e84c5
cdaq <>
1994-02-22 18:52:19 +0000

    (SAW) Move regpar declarations to gen_routines.dec.  Make title arg null.

--- HTRACKING/h_register_param.f
+++ HTRACKING/h_register_param.f
@@ -12,149 +12,152 @@
 *-
 *- All standards are from "Proposal for Hall C Analysis Software
 *- Vade Mecum, Draft 1.0" by D.F.Geesamn and S.Wood, 7 May 1993
+* $Log$
+* Revision 1.2  1994/02/22 18:52:06  cdaq
+* (SAW) Move regpar declarations to gen_routines.dec.  Make title arg null.
+*
 *-
 *--------------------------------------------------------
-       IMPLICIT NONE
-       SAVE
+      IMPLICIT NONE
+      SAVE
 *
-       character*50 here
-       parameter (here= 'H_register_param')
+      character*16 here
+      parameter (here= 'H_register_param')
 *
-       logical ABORT
-       character*(*) err
+      logical ABORT
+      character*(*) err
 *
-       INCLUDE 'gen_data_structures.cmn'
-       INCLUDE 'gen_routines.dec'
-       INCLUDE 'hms_tracking.cmn'
-       INCLUDE 'hms_geometry.cmn'
+      INCLUDE 'gen_data_structures.cmn'
+      INCLUDE 'gen_routines.dec'
+      INCLUDE 'hms_tracking.cmn'
+      INCLUDE 'hms_geometry.cmn'
 *
-       INTEGER ierr,flag
-       EXTERNAL regparmrealarray,regparmreal,regparmintarray
-       EXTERNAL regparmint
+      INTEGER ierr
 *
 *--------------------------------------------------------
-       err= ' '
+      err= ' '
+      abort = .false.
 *
 *
-       ierr= regparmreal('raddeg',raddeg,flag)
-       IF(ierr.NE.0) err= 'unable to register "raddeg"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('hdc_num_planes',hdc_num_planes,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdc_num_planes"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('hdc_num_chambers',hdc_num_chambers,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdc_num_chambers"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmrealarray('hdc_zpos',hdc_zpos,HMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdc_zpos"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmrealarray('hdc_alpha_angle',hdc_alpha_angle,
-     &             HMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdc_alpha_angle"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmrealarray('hdc_beta_angle',hdc_beta_angle,
-     &             HMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdc_beta_angle"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmrealarray('hdc_gamma_angle',hdc_gamma_angle,
-     &             HMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdc_gamma_angle"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmrealarray('hdc_sigma',hdc_sigma,
-     &             HMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdc_sigma"'
-       ierr= regparmrealarray('hdc_pitch',hdc_pitch,
-     &             HMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdc_pitch"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmrealarray('hdc_nrwire',hdc_nrwire,
-     &             HMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdc_nrwire"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmrealarray('hdc_central_wire',hdc_central_wire,
-     &             HMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdc_central_wire"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmrealarray('hdc_xcenter',hdc_xcenter,
-     &             HMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdc_xcenter"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmrealarray('hdc_ycenter',hdc_ycenter,
-     &             HMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdc_ycenter"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmintarray('hdc_chamber_planes',hdc_chamber_planes,
-     &             HMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdc_chamber_planes"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmintarray('hdc_wire_counting',hdc_wire_counting,
-     &             HMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdc_wire_counting"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmreal('hdrift_velocity',hdrift_velocity,
-     &             flag)
-       IF(ierr.NE.0) err= 'unable to register "hdrift_velocity"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmreal('hxt_track_criterion',hxt_track_criterion,
-     &             flag)
-       IF(ierr.NE.0) err= 'unable to register "hxt_track_criterion"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmreal('hyt_track_criterion',hyt_track_criterion,
-     &             flag)
-       IF(ierr.NE.0) err= 'unable to register "hyt_track_criterion"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmreal('hxpt_track_criterion',hxpt_track_criterion,
-     &             flag)
-       IF(ierr.NE.0) err= 'unable to register "hxpt_track_criterion"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmreal('hypt_track_criterion',hypt_track_criterion,
-     &             flag)
-       IF(ierr.NE.0) err= 'unable to register "hypt_track_criterion"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmintarray('hmin_hit',hmin_hit,hmax_num_chambers
-     &             ,flag)
-       IF(ierr.NE.0) err= 'unable to register "hmin_hit"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmintarray('hmin_combos',hmin_combos,
-     &             hmax_num_chambers,flag)
-       IF(ierr.NE.0) err= 'unable to register "hmin_combos"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmrealarray('hspace_point_criterion',
-     &             hspace_point_criterion,
-     &             hmax_num_chambers,flag)
-       IF(ierr.NE.0) err= 'unable to register "hspace_point_criterion"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('hluno',hluno,flag)
-       IF(ierr.NE.0) err= 'unable to register "hluno"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('hdebugflagpsi',hdebugflagpsi,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdebugflagpsi"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('hdebugflaggeometry',hdebugflaggeometry,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdebugflaggeometry"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('hdebugflagpr',hdebugflagpr,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdebugflagppr"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('hdebugflagstubs',hdebugflagstubs,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdebugflagstubs"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('hdebuglinkstubs',hdebuglinkstubs,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdebuglinkstubs"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('hdebugtrackprint',hdebugtrackprint,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdebugtrackprint"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('hdebugstubchisq',hdebugstubchisq,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdebugstubchisq"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('hdebugtartrackprint',hdebugtartrackprint,flag)
-       IF(ierr.NE.0) err= 'unable to register "hdebugtartrackprint"'
-       ABORT= ierr.EQ.0 .or. ABORT
-*
-       IF(ABORT) THEN
+*       ierr= regparmreal('raddeg',raddeg,0)
+*       IF(ierr.NE.0) err= 'unable to register "raddeg"'
+*       ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('hdc_num_planes',hdc_num_planes,0)
+      IF(ierr.NE.0) err= 'unable to register "hdc_num_planes"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('hdc_num_chambers',hdc_num_chambers,0)
+      IF(ierr.NE.0) err= 'unable to register "hdc_num_chambers"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmrealarray('hdc_zpos',hdc_zpos,HMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "hdc_zpos"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmrealarray('hdc_alpha_angle',hdc_alpha_angle,
+     &     HMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "hdc_alpha_angle"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmrealarray('hdc_beta_angle',hdc_beta_angle,
+     &     HMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "hdc_beta_angle"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmrealarray('hdc_gamma_angle',hdc_gamma_angle,
+     &     HMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "hdc_gamma_angle"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmrealarray('hdc_sigma',hdc_sigma,
+     &     HMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "hdc_sigma"'
+      ierr= regparmrealarray('hdc_pitch',hdc_pitch,
+     &     HMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "hdc_pitch"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmrealarray('hdc_nrwire',hdc_nrwire,
+     &     HMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "hdc_nrwire"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmrealarray('hdc_central_wire',hdc_central_wire,
+     &     HMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "hdc_central_wire"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmrealarray('hdc_xcenter',hdc_xcenter,
+     &     HMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "hdc_xcenter"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmrealarray('hdc_ycenter',hdc_ycenter,
+     &     HMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "hdc_ycenter"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmintarray('hdc_chamber_planes',hdc_chamber_planes,
+     &     HMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "hdc_chamber_planes"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmintarray('hdc_wire_counting',hdc_wire_counting,
+     &     HMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "hdc_wire_counting"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmreal('hdrift_velocity',hdrift_velocity,
+     &     0)
+      IF(ierr.NE.0) err= 'unable to register "hdrift_velocity"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmreal('hxt_track_criterion',hxt_track_criterion,
+     &     0)
+      IF(ierr.NE.0) err= 'unable to register "hxt_track_criterion"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmreal('hyt_track_criterion',hyt_track_criterion,
+     &     0)
+      IF(ierr.NE.0) err= 'unable to register "hyt_track_criterion"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmreal('hxpt_track_criterion',hxpt_track_criterion,
+     &     0)
+      IF(ierr.NE.0) err= 'unable to register "hxpt_track_criterion"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmreal('hypt_track_criterion',hypt_track_criterion,
+     &     0)
+      IF(ierr.NE.0) err= 'unable to register "hypt_track_criterion"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmintarray('hmin_hit',hmin_hit,hmax_num_chambers
+     &     ,0)
+      IF(ierr.NE.0) err= 'unable to register "hmin_hit"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmintarray('hmin_combos',hmin_combos,
+     &     hmax_num_chambers,0)
+      IF(ierr.NE.0) err= 'unable to register "hmin_combos"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmrealarray('hspace_point_criterion',
+     &     hspace_point_criterion,
+     &     hmax_num_chambers,0)
+      IF(ierr.NE.0) err= 'unable to register "hspace_point_criterion"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('hluno',hluno,0)
+      IF(ierr.NE.0) err= 'unable to register "hluno"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('hdebug0psi',hdebug0psi,0)
+      IF(ierr.NE.0) err= 'unable to register "hdebug0psi"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('hdebug0geometry',hdebug0geometry,0)
+      IF(ierr.NE.0) err= 'unable to register "hdebug0geometry"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('hdebug0pr',hdebug0pr,0)
+      IF(ierr.NE.0) err= 'unable to register "hdebug0ppr"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('hdebug0stubs',hdebug0stubs,0)
+      IF(ierr.NE.0) err= 'unable to register "hdebug0stubs"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('hdebuglinkstubs',hdebuglinkstubs,0)
+      IF(ierr.NE.0) err= 'unable to register "hdebuglinkstubs"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('hdebugtrackprint',hdebugtrackprint,0)
+      IF(ierr.NE.0) err= 'unable to register "hdebugtrackprint"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('hdebugstubchisq',hdebugstubchisq,0)
+      IF(ierr.NE.0) err= 'unable to register "hdebugstubchisq"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('hdebugtartrackprint',hdebugtartrackprint,0)
+      IF(ierr.NE.0) err= 'unable to register "hdebugtartrackprint"'
+      ABORT= ierr.EQ.0 .or. ABORT
+*     
+      IF(ABORT) THEN
          call G_add_path(here,err)
-       ENDIF
+      ENDIF
 *
-       RETURN
-       END
+      RETURN
+      END
--- STRACKING/s_register_param.f
+++ STRACKING/s_register_param.f
@@ -13,147 +13,149 @@
 *- All standards are from "Proposal for Hall C Analysis Software
 *- Vade Mecum, Draft 1.0" by D.F.Geesamn and S.Wood, 7 May 1993
 * $Log$
-* Revision 1.1  1994/02/22 18:42:21  cdaq
+* Revision 1.2  1994/02/22 18:52:19  cdaq
+* (SAW) Move regpar declarations to gen_routines.dec.  Make title arg null.
+*
+* Revision 1.1  1994/02/22  18:42:21  cdaq
 * Initial revision
 *
 *-
 *--------------------------------------------------------
-       IMPLICIT NONE
-       SAVE
+      IMPLICIT NONE
+      SAVE
 *
-       character*50 here
-       parameter (here= 'S_register_param')
+      character*16 here
+      parameter (here= 'S_register_param')
 *
-       logical ABORT
-       character*(*) err
+      logical ABORT
+      character*(*) err
 *
-       INCLUDE 'gen_data_structures.cmn'
-       INCLUDE 'gen_routines.dec'
-       INCLUDE 'sos_tracking.cmn'
-       INCLUDE 'sos_geometry.cmn'
+      INCLUDE 'gen_data_structures.cmn'
+      INCLUDE 'gen_routines.dec'
+      INCLUDE 'sos_tracking.cmn'
+      INCLUDE 'sos_geometry.cmn'
 *
-       INTEGER ierr,flag
-       EXTERNAL regparmrealarray,regparmreal,regparmintarray
-       EXTERNAL regparmint
+      INTEGER ierr
 *
 *--------------------------------------------------------
-       err= ' '
+      err= ' '
+      abort = .false.
 *
 *
-       ierr= regparmreal('raddeg',raddeg,flag)
-       IF(ierr.NE.0) err= 'unable to register "raddeg"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('sdc_num_planes',sdc_num_planes,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdc_num_planes"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('sdc_num_chambers',sdc_num_chambers,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdc_num_chambers"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmrealarray('sdc_zpos',sdc_zpos,SMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdc_zpos"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmrealarray('sdc_alpha_angle',sdc_alpha_angle,
-     &             SMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdc_alpha_angle"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmrealarray('sdc_beta_angle',sdc_beta_angle,
-     &             SMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdc_beta_angle"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmrealarray('sdc_gamma_angle',sdc_gamma_angle,
-     &             SMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdc_gamma_angle"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmrealarray('sdc_sigma',sdc_sigma,
-     &             SMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdc_sigma"'
-       ierr= regparmrealarray('sdc_pitch',sdc_pitch,
-     &             SMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdc_pitch"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmrealarray('sdc_nrwire',sdc_nrwire,
-     &             SMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdc_nrwire"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmrealarray('sdc_central_wire',sdc_central_wire,
-     &             SMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdc_central_wire"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmrealarray('sdc_xcenter',sdc_xcenter,
-     &             SMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdc_xcenter"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmrealarray('sdc_ycenter',sdc_ycenter,
-     &             SMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdc_ycenter"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmintarray('sdc_chamber_planes',sdc_chamber_planes,
-     &             SMAX_NUM_DC_PLANES,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdc_chamber_planes"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmreal('sxt_track_criterion',sxt_track_criterion,
-     &             flag)
-       IF(ierr.NE.0) err= 'unable to register "sxt_track_criterion"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmreal('syt_track_criterion',syt_track_criterion,
-     &             flag)
-       IF(ierr.NE.0) err= 'unable to register "syt_track_criterion"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmreal('sxpt_track_criterion',sxpt_track_criterion,
-     &             flag)
-       IF(ierr.NE.0) err= 'unable to register "sxpt_track_criterion"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmreal('sypt_track_criterion',sypt_track_criterion,
-     &             flag)
-       IF(ierr.NE.0) err= 'unable to register "sypt_track_criterion"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmintarray('smin_hit',smin_hit,smax_num_chambers
-     &             ,flag)
-       IF(ierr.NE.0) err= 'unable to register "smin_hit"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmintarray('smin_combos',smin_combos,
-     &             smax_num_chambers,flag)
-       IF(ierr.NE.0) err= 'unable to register "smin_combos"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmrealarray('sspace_point_criterion',
-     &             sspace_point_criterion,
-     &             smax_num_chambers,flag)
-       IF(ierr.NE.0) err= 'unable to register "sspace_point_criterion"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmreal('sdrift_velocity',sdrift_velocity,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdrift_velocity"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('sluno',sluno,flag)
-       IF(ierr.NE.0) err= 'unable to register "sluno"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('sdebugflagpsi',sdebugflagpsi,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdebugflagpsi"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('sdebugflaggeometry',sdebugflaggeometry,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdebugflaggeometry"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('sdebugflagpr',sdebugflagpr,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdebugflagppr"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('sdebugflagstubs',sdebugflagstubs,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdebugflagstubs"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('sdebuglinkstubs',sdebuglinkstubs,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdebuglinkstubs"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('sdebugtrackprint',sdebugtrackprint,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdebugtrackprint"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('sdebugstubchisq',sdebugstubchisq,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdebugstubchisq"'
-       ABORT= ierr.EQ.0 .or. ABORT
-       ierr= regparmint('sdebugtartrackprint',sdebugtartrackprint,flag)
-       IF(ierr.NE.0) err= 'unable to register "sdebugtartrackprint"'
-       ABORT= ierr.EQ.0 .or. ABORT
+*      ierr= regparmreal('raddeg',raddeg,0)
+*      IF(ierr.NE.0) err= 'unable to register "raddeg"'
+*      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('sdc_num_planes',sdc_num_planes,0)
+      IF(ierr.NE.0) err= 'unable to register "sdc_num_planes"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('sdc_num_chambers',sdc_num_chambers,0)
+      IF(ierr.NE.0) err= 'unable to register "sdc_num_chambers"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmrealarray('sdc_zpos',sdc_zpos,SMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "sdc_zpos"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmrealarray('sdc_alpha_angle',sdc_alpha_angle,
+     &     SMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "sdc_alpha_angle"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmrealarray('sdc_beta_angle',sdc_beta_angle,
+     &     SMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "sdc_beta_angle"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmrealarray('sdc_gamma_angle',sdc_gamma_angle,
+     &     SMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "sdc_gamma_angle"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmrealarray('sdc_sigma',sdc_sigma,
+     &     SMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "sdc_sigma"'
+      ierr= regparmrealarray('sdc_pitch',sdc_pitch,
+     &     SMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "sdc_pitch"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmrealarray('sdc_nrwire',sdc_nrwire,
+     &     SMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "sdc_nrwire"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmrealarray('sdc_central_wire',sdc_central_wire,
+     &     SMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "sdc_central_wire"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmrealarray('sdc_xcenter',sdc_xcenter,
+     &     SMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "sdc_xcenter"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmrealarray('sdc_ycenter',sdc_ycenter,
+     &     SMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "sdc_ycenter"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmintarray('sdc_chamber_planes',sdc_chamber_planes,
+     &     SMAX_NUM_DC_PLANES,0)
+      IF(ierr.NE.0) err= 'unable to register "sdc_chamber_planes"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmreal('sxt_track_criterion',sxt_track_criterion,
+     &     0)
+      IF(ierr.NE.0) err= 'unable to register "sxt_track_criterion"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmreal('syt_track_criterion',syt_track_criterion,
+     &     0)
+      IF(ierr.NE.0) err= 'unable to register "syt_track_criterion"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmreal('sxpt_track_criterion',sxpt_track_criterion,
+     &     0)
+      IF(ierr.NE.0) err= 'unable to register "sxpt_track_criterion"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmreal('sypt_track_criterion',sypt_track_criterion,
+     &     0)
+      IF(ierr.NE.0) err= 'unable to register "sypt_track_criterion"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmintarray('smin_hit',smin_hit,smax_num_chambers
+     &     ,0)
+      IF(ierr.NE.0) err= 'unable to register "smin_hit"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmintarray('smin_combos',smin_combos,
+     &     smax_num_chambers,0)
+      IF(ierr.NE.0) err= 'unable to register "smin_combos"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmrealarray('sspace_point_criterion',
+     &     sspace_point_criterion,
+     &     smax_num_chambers,0)
+      IF(ierr.NE.0) err= 'unable to register "sspace_point_criterion"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmreal('sdrift_velocity',sdrift_velocity,0)
+      IF(ierr.NE.0) err= 'unable to register "sdrift_velocity"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('sluno',sluno,0)
+      IF(ierr.NE.0) err= 'unable to register "sluno"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('sdebug0psi',sdebug0psi,0)
+      IF(ierr.NE.0) err= 'unable to register "sdebug0psi"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('sdebug0geometry',sdebug0geometry,0)
+      IF(ierr.NE.0) err= 'unable to register "sdebug0geometry"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('sdebug0pr',sdebug0pr,0)
+      IF(ierr.NE.0) err= 'unable to register "sdebug0ppr"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('sdebug0stubs',sdebug0stubs,0)
+      IF(ierr.NE.0) err= 'unable to register "sdebug0stubs"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('sdebuglinkstubs',sdebuglinkstubs,0)
+      IF(ierr.NE.0) err= 'unable to register "sdebuglinkstubs"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('sdebugtrackprint',sdebugtrackprint,0)
+      IF(ierr.NE.0) err= 'unable to register "sdebugtrackprint"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('sdebugstubchisq',sdebugstubchisq,0)
+      IF(ierr.NE.0) err= 'unable to register "sdebugstubchisq"'
+      ABORT= ierr.EQ.0 .or. ABORT
+      ierr= regparmint('sdebugtartrackprint',sdebugtartrackprint,0)
+      IF(ierr.NE.0) err= 'unable to register "sdebugtartrackprint"'
+      ABORT= ierr.EQ.0 .or. ABORT
 *
-       IF(ABORT) THEN
+      IF(ABORT) THEN
          call G_add_path(here,err)
-       ENDIF
+      ENDIF
 *
-       RETURN
-       END
+      RETURN
+      END
