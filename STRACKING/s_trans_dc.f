823084494c5d4d04187c3fd9889162356c124688
cdaq <>
1994-04-13 18:56:40 +0000

    (DFG) Add call to s_fill_dc_dec_hist, remove s_raw_dump_all call

--- STRACKING/s_trans_dc.f
+++ STRACKING/s_trans_dc.f
@@ -1,4 +1,4 @@
-       SUBROUTINE S_TRANS_DC(ABORT,err)
+      SUBROUTINE S_TRANS_DC(ABORT,err)
 *--------------------------------------------------------
 *-
 *-   Purpose and Methods : Translate SOS raw drift and start time 
@@ -12,10 +12,11 @@
 *-   Output: ABORT           - success or failure
 *-         : err             - reason for failure, if any
 *- 
-*-   Created 19-JAN-1994   D. F. Geesaman
-*-                           Dummy Shell routine
 * $Log$
-* Revision 1.3  1994/03/24 19:59:03  cdaq
+* Revision 1.4  1994/04/13 18:56:40  cdaq
+* (DFG) Add call to s_fill_dc_dec_hist, remove s_raw_dump_all call
+*
+* Revision 1.3  1994/03/24  19:59:03  cdaq
 * (DFG) add print routines and flags
 *       check plane number and wire number for validity
 *
@@ -26,81 +27,81 @@
 * Initial revision
 *
 *--------------------------------------------------------
-       IMPLICIT NONE
-       SAVE
-*
-       character*50 here
-       parameter (here= 'S_TRANS_DC')
-*
-       logical ABORT
-       character*(*) err
-*
-       include 'gen_data_structures.cmn'
-       include 'gen_constants.par'
-       include 'gen_units.par'
-       include 'sos_tracking.cmn'
-       include 'sos_geometry.cmn'          
-*
+      IMPLICIT NONE
+      SAVE
+*     
+      character*50 here
+      parameter (here= 'S_TRANS_DC')
+*     
+      logical ABORT
+      character*(*) err
+*     
+      include 'gen_data_structures.cmn'
+      include 'gen_constants.par'
+      include 'gen_units.par'
+      include 'sos_tracking.cmn'
+      include 'sos_geometry.cmn'          
+*     
 *--------------------------------------------------------
-       real*4 s_drift_time_calc,s_drift_dist_calc,s_wire_center_calc
-       external s_drift_time_calc
-       external s_drift_dist_calc
-       external s_wire_center_calc
-       integer*4 ihit,goodhit,old_wire,old_plane,wire,plane
-*
-       ABORT= .FALSE.
-       err= ' '
-*      Dump raw bank if debug flag set
-       if(sdebugprintrawdc.ne.0) then
-          call s_print_raw_dc(ABORT,err)
-       endif
-       old_wire = -1
-       old_plane = -1
-       goodhit = 0
-*      Are there any raw hits
-       if(SDC_RAW_TOT_HITS.gt.0) then
-*      loop over all raw hits
+      real*4 s_drift_time_calc,s_drift_dist_calc,s_wire_center_calc
+      external s_drift_time_calc
+      external s_drift_dist_calc
+      external s_wire_center_calc
+      integer*4 ihit,goodhit,old_wire,old_plane,wire,plane
+*     
+      ABORT= .FALSE.
+      err= ' '
+      old_wire = -1
+      old_plane = -1
+      goodhit = 0
+*     Are there any raw hits
+      if(SDC_RAW_TOT_HITS.gt.0) then
+*     loop over all raw hits
          do ihit=1,SDC_RAW_TOT_HITS
-           plane = SDC_RAW_PLANE_NUM(ihit)
-           wire  = SDC_RAW_WIRE_NUM(ihit)
-*      check valid plane and wire number
-          if(plane.gt.0 .and. plane.le. sdc_num_planes) then
-*      test for valid wire number
-            if(wire .gt. 0 .and. wire .le. sdc_nrwire(plane) ) then
-*      test for multiple hit on the same wire
-             if(plane .eq. old_plane .and. wire .eq. old_wire ) then
-                swire_mult(wire,plane) = swire_mult(wire,plane)+1
-             else
+            plane = SDC_RAW_PLANE_NUM(ihit)
+            wire  = SDC_RAW_WIRE_NUM(ihit)
+*     check valid plane and wire number
+            if(plane.gt.0 .and. plane.le. sdc_num_planes) then
+*     test for valid wire number
+               if(wire .gt. 0 .and. wire .le. sdc_nrwire(plane) ) then
+*     test for multiple hit on the same wire
+                  if(plane .eq. old_plane .and. wire .eq. old_wire ) then
+                     swire_mult(wire,plane) = swire_mult(wire,plane)+1
+                  else
 
-*          valid hit proceed with decoding
-                goodhit = goodhit + 1
-                SDC_PLANE_NUM(goodhit) = SDC_RAW_PLANE_NUM(ihit)
-                SDC_WIRE_NUM(goodhit) = SDC_RAW_WIRE_NUM(ihit)
-                SDC_TDC(goodhit) = SDC_RAW_TDC(ihit)
-                SDC_WIRE_CENTER(goodhit) = s_wire_center_calc(plane,wire)
-     &
-                SDC_DRIFT_TIME(goodhit) = 
-     &               s_drift_time_calc(plane,wire,SDC_TDC(goodhit))
-                SDC_DRIFT_DIS(goodhit) =
-     &               s_drift_dist_calc(plane,wire,SDC_DRIFT_TIME(goodhit))
-                SDC_HITS_PER_PLANE(plane) = SDC_HITS_PER_PLANE(plane) + 1
-             endif                       ! end test on duplicate wire
-             old_plane = plane
-             old_wire  = wire
-            endif                     ! end test on valid wire number
-          endif                       ! end test on valid plane number
-         enddo                        ! end loop over raw hits
-*
-*      set total number of good hits
-*
-            SDC_TOT_HITS = goodhit
+*     valid hit proceed with decoding
+                     goodhit = goodhit + 1
+                     SDC_PLANE_NUM(goodhit) = SDC_RAW_PLANE_NUM(ihit)
+                     SDC_WIRE_NUM(goodhit) = SDC_RAW_WIRE_NUM(ihit)
+                     SDC_TDC(goodhit) = SDC_RAW_TDC(ihit)
+                     SDC_WIRE_CENTER(goodhit) = s_wire_center_calc(plane
+     $                    ,wire)
+                     SDC_DRIFT_TIME(goodhit) = 
+     &                    s_drift_time_calc(plane,wire,SDC_TDC(goodhit))
+                     SDC_DRIFT_DIS(goodhit) =
+     &                    s_drift_dist_calc(plane,wire
+     $                    ,SDC_DRIFT_TIME(goodhit))
+                     SDC_HITS_PER_PLANE(plane) = SDC_HITS_PER_PLANE(plane)
+     $                    + 1
+                  endif                 ! end test on duplicate wire
+                  old_plane = plane
+                  old_wire  = wire
+               endif                    ! end test on valid wire number
+            endif                       ! end test on valid plane number
+         enddo                          ! end loop over raw hits
+*     
+*     set total number of good hits
+*     
+         SDC_TOT_HITS = goodhit
 
-                
-       endif                !  end test on SDC_RAW_TOT_HITS.gt.0
-*      Dump decoded banks if flag is set
-       if(sdebugprintdecodeddc.ne.0) then
-          call s_print_decoded_dc(ABORT,err)
-       endif
-*
-       RETURN
-       END
+*     Histogram SDC_DECODED_DC
+         call s_fill_dc_dec_hist(ABORT,err)
+
+      endif                             !  end test on SDC_RAW_TOT_HITS.gt.0
+*     Dump decoded banks if flag is set
+      if(sdebugprintdecodeddc.ne.0) then
+         call s_print_decoded_dc(ABORT,err)
+      endif
+*     
+      RETURN
+      END
