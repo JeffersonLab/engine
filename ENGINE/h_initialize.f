7f11f229ca71b4fdeb6b9ba81491fac06e5d323b
cdaq <>
1994-05-12 19:34:06 +0000

    (DFG) Add call to h_targ_trans_init

--- ENGINE/h_initialize.f
+++ ENGINE/h_initialize.f
@@ -1,4 +1,4 @@
-       SUBROUTINE H_initialize(ABORT,err)
+      SUBROUTINE H_initialize(ABORT,err)
 *--------------------------------------------------------
 *-       Prototype C analysis routine
 *-
@@ -11,9 +11,12 @@
 *-   Created  8-Nov-1993   Kevin B. Beard
 *-   Modified 20-Nov-1993   KBB for new errors
 *-    $Log$
-*-    Revision 1.7  1994/04/13 04:31:00  cdaq
-*-    (DFG) Add initialize for scin and cal
+*-    Revision 1.8  1994/05/12 19:34:06  cdaq
+*-    (DFG) Add call to h_targ_trans_init
 *-
+* Revision 1.7  1994/04/13  04:31:00  cdaq
+* (DFG) Add initialize for scin and cal
+*
 * Revision 1.6  1994/04/12  17:20:27  cdaq
 * (KBB) Add ntuple call
 *
@@ -39,6 +42,8 @@
 *
       logical ABORT
       character*(*) err
+      character*20  err1
+      integer*4 istat
 *
 *
 *--------------------------------------------------------
@@ -49,9 +54,24 @@
 *
 *     calculate secondary scintillator and time of flight parameters
       call h_init_scin(ABORT,err)
+      if(ABORT) then
+         call g_add_path(here,err)
+      endif
 *
 *     calculate secondary calorimeter parameters
       call h_init_cal(ABORT,err)
+      if(ABORT) then
+         call g_add_path(here,err)
+      endif
+*     read in Optical matrix elements
+      call h_targ_trans_init(ABORT,err,istat)
+      if(ABORT) then
+         call g_build_note(':istat=@','@',istat,' ',1.,'(I3)',err1)
+         call G_prepend(err1,err)
+         call g_rep_err(ABORT,err)
+         call g_add_path(here,err)
+      endif
+
 *
       call h_ntuple_init(ABORT,err)
 *
