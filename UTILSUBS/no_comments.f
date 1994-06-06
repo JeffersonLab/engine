0c2930c225336976311cdcfb2fe06e34c150a078
cdaq <>
1994-06-06 04:39:27 +0000

    (KBB) Speedup

--- UTILSUBS/no_comments.f
+++ UTILSUBS/no_comments.f
@@ -1,7 +1,10 @@
       SUBROUTINE NO_Comments(string) 
 *
 * $Log$
-* Revision 1.1  1994/02/22 20:01:25  cdaq
+* Revision 1.2  1994/06/06 04:39:27  cdaq
+* (KBB) Speedup
+*
+* Revision 1.1  1994/02/22  20:01:25  cdaq
 * Initial revision
 *
 *
@@ -10,13 +13,15 @@
       data flag/'!@#$%^&*<>[]{}*;?:"()~/'/ 
       integer string_length	!FUNCTION
 c
-c         strips out comments [including "quotes"]
+c     strips out comments [including "quotes"]
 c
       do i=1,string_length(string)
-        if(INDEX(flag,string(i:i)).ne.0) then
-             string(i:)=' '
-             return
-           endif 
+         if(INDEX(flag,string(i:i)).ne.0) then
+            string(i:)=' '
+            return
+         elseif(string(i:).EQ.' ') then
+            return
+         endif 
       enddo
       return 
       end
