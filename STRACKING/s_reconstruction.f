bea70152eeb8a4ea48a8a06d8f62aef6170ce281
cdaq <>
1994-06-07 04:46:21 +0000

    (DFG) add s_recon_num and bypass switches

--- STRACKING/s_reconstruction.f
+++ STRACKING/s_reconstruction.f
@@ -9,9 +9,12 @@
 *-         : err             - reason for failure, if any
 *- 
 *-    $Log$
-*-    Revision 1.4  1994/05/13 03:34:52  cdaq
-*-    (DFG) Put s_prt_track_tests here. Remove from s_tof
+*-    Revision 1.5  1994/06/07 04:46:21  cdaq
+*-    (DFG) add s_recon_num and bypass switches
 *-
+* Revision 1.4  1994/05/13  03:34:52  cdaq
+* (DFG) Put s_prt_track_tests here. Remove from s_tof
+*
 * Revision 1.3  1994/04/13  18:30:40  cdaq
 * (DFG) add call to s_raw_dump_all and comment out some returns after ABORT's
 *
@@ -40,6 +43,8 @@
       INCLUDE 'gen_constants.par'
       INCLUDE 'gen_units.par'
       include 'sos_scin_parms.cmn'
+      include 'sos_bypass_switches.cmn'
+      include 'sos_statistics.cmn'
 *
 *     local variables
       integer*4 istat
@@ -47,6 +52,9 @@
 *
       ABORT= .TRUE.
       err= ':no events analyzed!'
+* increment reconstructed number
+      s_recon_num= s_recon_num + 1
+*
 *     dump all raw data
       call s_raw_dump_all(ABORT,err)
       if(ABORT) then
@@ -57,35 +65,42 @@
 *     TRANSLATE SCINTILATORS AND CALCULATE START TIME
 *     SOS_RAW_SCIN ====> SOS_DECODED_SCIN
 *     
-      call S_TRANS_SCIN(ABORT,err)
-      if(ABORT)  then
-         call G_add_path(here,err)
-*         return
-      endif                                     ! end test on SCIN ABORT
-
+      If(sbypass_trans_scin.eq.0) then
+        call S_TRANS_SCIN(ABORT,err)
+        if(ABORT)  then
+           call G_add_path(here,err)
+*          return
+        endif                                     ! end test on SCIN ABORT
+      endif                              ! end test on sbypass_trans_scin
 *
 *      TRANSLATE CALORIMETER 
 *      SOS_RAW_CAL ====> SOS_DECODED_CAL
 *
-      call S_TRANS_CAL(ABORT,err)
-      if(ABORT)   then
+      if(sbypass_trans_cal.eq.0) then
+        call S_TRANS_CAL(ABORT,err)
+        if(ABORT)   then
          call G_add_path(here,err)
 *         return
-      endif                                     ! end test on CAL ABORT
+        endif                                     ! end test on CAL ABORT
+      endif                                    ! end test on sbypass_trans_cal
 *
 *      TRANLATE DRIFT CHAMBERS
 *      SOS_RAW_DC + SOS_DECODED_SCIN ====>  SOS_DECODED_DC
-      call S_TRANS_DC(ABORT,err)
-      if(ABORT) then
-         call G_add_path(here,err)
-         return
-      endif                                     ! end test on S_TRANS_DC ABORT
+      if(sbypass_trans_dc.eq.0) then
+        call S_TRANS_DC(ABORT,err)
+        if(ABORT) then
+          call G_add_path(here,err)
+          return
+        endif                                     ! end test on S_TRANS_DC ABORT
+      endif                                     ! end test on sbypass_trans_dc
 *
-      call S_TRACK(ABORT,err)
-      if(ABORT)  then
-         call G_add_path(here,err)
-         return
-      endif                                     ! end test on S_TRACK ABORT
+      if(sbypass_track.eq.0) then
+        call S_TRACK(ABORT,err)
+        if(ABORT)  then
+           call G_add_path(here,err)
+           return
+        endif                                     ! end test on S_TRACK ABORT
+      endif                               ! end test on sbypass_track
 *     only proceed if the number of tracks is greater than one
 *
       if(SNTRACKS_FP .lt. 1) then
@@ -98,37 +113,45 @@
 *     Project tracks back to target
 *     SOS_FOCAL_PLANE  ====>  SOS_TARGET
 *
+       if(sbypass_targ_trans.eq. 0) then
          call S_TARG_TRANS(ABORT,err,istat)
          if(ABORT) then
             call G_add_path(here,err)
             return
          endif                                  ! end test on S_TARG_TRANS ABORT
+       endif                                 ! end test on sbypass_target_trans
 *
 *     Now begin to process particle identification information
 *     First scintillator and time of flight
 *     SOS_RAW_SCIN ====> SOS_TRACK_TESTS
 *
+       if(sbypass_tof.eq.0) then
          call S_TOF(ABORT,err)
          if(ABORT) then
             call G_add_path(here,err)
             return
          endif                                  ! end test of S_TOF ABORT
+       endif                                    ! end test on sbypass_tof
 *      Next Calorimeter information
 *      SOS_DECODED_CAL ====> SOS_TRACK_TESTS
 *
+       if(sbypass_cal.eq.0) then
          call S_CAL(ABORT,err)
          if(ABORT) then
             call G_add_path(here,err)
             return
          endif                                  ! end test of S_CAL ABORT
+       endif                                    ! end test on sbypass_cal
 *     Next Cerenkov information
 *     SOS_DECODED_CER ====> SOS_TRACK_TESTS
 *
+       if(sbypass_cer.eq.0) then
          call S_CER(ABORT,err)
          if(ABORT) then
             call G_add_path(here,err)
 *            return
          endif                                  ! end test of S_CER ABORT
+       endif                                    ! end test on sbypass_cer
 *
 *     Dump SOS_TRACK_TESTS if sdebugprinttracktests is set
           if( sdebugprinttracktests .ne. 0 ) then
@@ -137,11 +160,13 @@
 *     Combine results in SOS physics analysis
 *     SOS_TARGET + SOS_TRACK_TESTS ====>  SOS_PHYSICS
 *
+        if(sbypass_physics.eq.0) then
          call S_PHYSICS(ABORT,err)
          if(ABORT) then
             call G_add_path(here,err)
 *            return
          endif                                  ! end test of S_PHYSICS ABORT
+        endif                                   ! end test on sbypass_physics
 *
       endif                                     ! end test no tracks found       
 *      
