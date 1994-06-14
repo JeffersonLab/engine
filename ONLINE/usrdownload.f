c94ae2884629bf202a3e2ad0b3d6a653ebf238a9
cdaq <>
1994-06-14 20:43:41 +0000

    Initial revision

---
+++ ONLINE/usrdownload.f
@@ -0,0 +1,15 @@
+      subroutine usrdownload(fname)
+*
+*     ONLINE ENGINE - Hall C online Analyzer
+*
+* $Log$
+* Revision 1.1  1994/06/14 20:43:41  cdaq
+* Initial revision
+*
+      implicit none
+      save
+      character*(*) fname
+*
+      return
+      end
+
---
+++ ONLINE/usrdump.f
@@ -0,0 +1,25 @@
+      subroutine usrdump
+*
+* $Log$
+* Revision 1.1  1994/06/14 20:42:33  cdaq
+* Initial revision
+*
+*
+      implicit none
+      save
+
+      character*7 here
+      parameter (here='usrdump')
+*
+      logical OK,ABORT
+      character*800 err
+*
+      call g_dump_histograms(ABORT,err)
+*
+      if(ABORT .or. err.ne.' ') then
+         call g_add_path(here,err)
+         call dalogmsg(err)
+      endif
+*
+      return 
+      end
