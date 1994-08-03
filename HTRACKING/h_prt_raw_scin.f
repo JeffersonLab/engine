c041e9dc64c9566c4b65c1c318c2f630314788fc
cdaq <>
1994-08-03 14:19:31 +0000

    (JRA) Fix variable names

--- HTRACKING/h_prt_raw_scin.f
+++ HTRACKING/h_prt_raw_scin.f
@@ -1,4 +1,4 @@
-       SUBROUTINE  h_prt_raw_scin(ABORT,err)
+      SUBROUTINE  h_prt_raw_scin(ABORT,err)
 *--------------------------------------------------------
 *-
 *-   Purpose and Methods : Dump HMS_RAW_SCIN BANKS
@@ -10,38 +10,44 @@
 *- 
 *-   Created 29-FEB-1994   D. F. Geesaman
 * $Log$
-* Revision 1.1  1994/04/13 15:43:19  cdaq
+* Revision 1.2  1994/08/03 14:19:31  cdaq
+* (JRA) Fix variable names
+*
+* Revision 1.1  1994/04/13  15:43:19  cdaq
 * Initial revision
 *
 *--------------------------------------------------------
-       IMPLICIT NONE
-       SAVE
+      IMPLICIT NONE
+      SAVE
 *
-       character*50 here
-       parameter (here= 'h_prt_raw_scin')
+      character*50 here
+      parameter (here= 'h_prt_raw_scin')
 *
-       logical ABORT
-       character*(*) err
+      logical ABORT
+      character*(*) err
 *
-       integer*4 j
-       include 'gen_data_structures.cmn'
-       include 'gen_constants.par'
-       include 'gen_units.par'
-       include 'hms_tracking.cmn'
+      integer*4 j
+      include 'gen_data_structures.cmn'
+      include 'gen_constants.par'
+      include 'gen_units.par'
+      include 'hms_tracking.cmn'
+      include 'tmp_pedestals.dte'
 *
 *--------------------------------------------------------
-       ABORT = .FALSE.
-       err = ' '
-       write(hluno,'(''        HMS_RAW_SCIN BANKS'')')
-       write(hluno,'(''     HSCIN_TOT_HITS='',I4)') HSCIN_TOT_HITS
-       if(HSCIN_TOT_HITS.GT.0) then
-         write(hluno,'('' Num  Plane    Counter        ADC_POS''
-     &   '' ADC_NEG  TDC_POS  TDC_NEG'')')
-         write(hluno,'(1x,i2,2x,i3,7x,i4,8x,4i8)')
-     &     (j,HSCIN_PLANE_NUM(j),HSCIN_COUNTER_NUM(j),
-     &        HSCIN_ADC_POS(j),HSCIN_ADC_NEG(j),
-     &        HSCIN_TDC_POS(j),HSCIN_TDC_NEG(j),
-     &        j=1,HSCIN_TOT_HITS )
-       endif
-       RETURN
-       END
+      ABORT = .FALSE.
+      err = ' '
+      write(hluno,'(''        HMS_RAW_SCIN BANKS'')')
+      write(hluno,'(''     HSCIN_ALL_TOT_HITS='',I4)') HSCIN_ALL_TOT_HITS
+      if(HSCIN_ALL_TOT_HITS.GT.0) then
+        write(hluno,'('' Num  Plane    Counter        ADC_POS''
+     &       '' ADC_NEG  TDC_POS  TDC_NEG'')')
+        write(hluno,'(1x,i2,2x,i3,7x,i4,8x,4i8)')
+     &       (j,HSCIN_ALL_PLANE_NUM(j),HSCIN_ALL_COUNTER_NUM(j),
+     &       (HSCIN_ALL_ADC_POS(j)-HSCIN_ALL_PED_POS(j)),
+     &       (HSCIN_ALL_ADC_NEG(j)-HSCIN_ALL_PED_NEG(j)),
+     &       HSCIN_ALL_TDC_POS(j),
+     &       HSCIN_ALL_TDC_NEG(j),
+     &       j=1,HSCIN_ALL_TOT_HITS )
+      endif
+      RETURN
+      END
