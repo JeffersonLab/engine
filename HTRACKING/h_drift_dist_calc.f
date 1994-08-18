dfa99c9dd9b1f92195275d55e80f70b8a3979908
cdaq <>
1994-08-18 03:34:21 +0000

    (DJM) Use a lookup table to map fractional area to distance.  (worked for the prototype chamber).  Lots of parameters but easier than fitting. Could not use a single map for all 12 planes since drift time spectra a very different.

--- HTRACKING/h_drift_dist_calc.f
+++ HTRACKING/h_drift_dist_calc.f
@@ -5,7 +5,13 @@
 *
 *     d.f. geesaman              17 feb 1994
 * $Log$
-* Revision 1.2  1994/07/27 19:00:07  cdaq
+* Revision 1.3  1994/08/18 03:34:21  cdaq
+* (DJM) Use a lookup table to map fractional area to distance.  (worked for
+* the prototype chamber).  Lots of parameters but easier than fitting.
+* Could not use a single map for all 12 planes since drift time spectra a
+* very different.
+*
+* Revision 1.2  1994/07/27  19:00:07  cdaq
 * (DJM) map fractional area to distance. worked for the prototype chamber!
 * (DFG) Add two regions of drift (commented out)
 *
@@ -13,44 +19,44 @@
 * Initial revision
 *  
       implicit none
-      include "gen_data_structures.cmn"
-      include "hms_geometry.cmn"
+      include 'gen_data_structures.cmn'
+      include 'hms_geometry.cmn'
+      include 'hms_tracking.cmn'        ! for lookup tables
 *
 *     input
 *
       integer*4  plane      !  plane number of hit
       integer*4  wire       !  wire number  of hit
+      integer*4  ilo,ihi    !  interpolate between bins ilo and ilo+1
       real*4     time       !  drift time in ns
-      real*4     timep      !  drift time in ns wrt expansion point
-      real*4     fract      !  fraction of integrated drift time spectrum
 *
 *     output
 *
       real*4     h_drift_dist_calc        !  drift distance in cm
 *
-*      if( time.gt. hdrift_t0_break(plane)) then
-*          h_drift_dist_calc = (time-hdrift_t0_break(plane))*hdrift_velocity
-*     &                        + hdrift_x0_break(plane)
-*      else
-*          h_drift_dist_calc = hdrift_x0_break(plane) *
-*     &                        (abs(time)/hdrift_t0_break(plane))**.6666666
-*      endif
 
-* test time to get polynomial from blowing up on us. relax the following
-* time window if offsets are poorly known at startup
-      timep = time - 225.               !expand polynomial about point of zero slope.
-      if( time.ge.-50. .and. time.le.250.) then
-         fract = 1. - 6.5209162e-5*timep**2 - 1.9512024e-6*timep**3 
-     &        - 1.9665608e-8*timep**4 - 7.3967721e-11*timep**5 
-     &        - 9.3595921e-14*timep**6
-         h_drift_dist_calc = 0.5*fract
+* look in the appropriate drift time to distance table and perform a linear
+* interpolation. minimum and maximum distance values are 0.0cm and 0.5cm. 
+      if( hdriftbinsz.eq.0.0)then
+        fractinterp = -1.0
+        h_drift_dist_calc = 0.5*fractinterp
+        return
+      endif      
+      ilo = int(time/hdriftbinsz) - int(hdrift1stbin/hdriftbinsz) + 1
+      ihi = ilo + 1
+      if( ilo.ge.1 .and. ihi.le.hdriftbins)then 
+        fractinterp = fract(ilo,plane) + 
+     &    ( (fract(ilo+1,plane)-fract(ilo,plane))/hdriftbinsz )*
+     &    (time - (ilo-1)*hdriftbinsz - hdrift1stbin)
       else
-         if( time.lt.-50.) then
-            h_drift_dist_calc = 0.0     !set to minimum value
-         else
-            if( time.gt. 250.)h_drift_dist_calc = 0.5 !set to maximum value
-         endif      
+        if( ilo.lt.1 )then
+          fractinterp = 0.0
+        else
+          if( ihi.gt.hdriftbins )fractinterp = 1.0
+        endif
       endif
+
+      h_drift_dist_calc = 0.5*fractinterp
 
       return
       end
