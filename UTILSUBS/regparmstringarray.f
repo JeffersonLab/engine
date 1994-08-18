129b9773e95f156a9c0ce93a02f7709dbeae0374
cdaq <>
1994-08-18 03:50:34 +0000

    Initial revision

---
+++ UTILSUBS/regparmstringarray.f
@@ -0,0 +1,12 @@
+      integer function regparmstringarray(name, strings, size)
+*
+* $Log$
+* Revision 1.1  1994/08/18 03:50:34  cdaq
+* Initial revision
+*
+      character*(*) name, strings(*)
+      integer*4 size
+      
+      regparmstringarray = 0
+      return
+      end
