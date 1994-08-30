ec90bcdc29498a5e116ad7a015b56cc68af34958
cdaq <>
1994-08-30 14:45:44 +0000

    (SAW) Add call to report generator

--- ENGINE/c_proper_shutdown.f
+++ ENGINE/c_proper_shutdown.f
@@ -10,9 +10,12 @@
 *- 
 *-   Created  20-Nov-1993   Kevin B. Beard for new error standards
 *-    $Log$
-*-    Revision 1.2  1994/04/12 17:12:20  cdaq
-*-    (KBB) Add ntuple call
+*-    Revision 1.3  1994/08/30 14:45:44  cdaq
+*-    (SAW) Add call to report generator
 *-
+* Revision 1.2  1994/04/12  17:12:20  cdaq
+* (KBB) Add ntuple call
+*
 * Revision 1.1  1994/02/04  21:07:58  cdaq
 * Initial revision
 *
@@ -23,11 +26,17 @@
       IMPLICIT NONE
       SAVE
 *
+      include 'gen_routines.dec'
+      include 'gen_filenames.cmn'
+      include 'coin_filenames.cmn'
+*
       character*50 here
       parameter (here= 'C_proper_shutdown')
 *
-      logical ABORT
+      logical ABORT, report_abort
       character*(*) err
+*
+      integer ierr
 *
 *--------------------------------------------------------
 *-chance to flush any statistics, etc.
@@ -38,7 +47,13 @@
 *
       call c_ntuple_shutdown(ABORT,err)
 *
-      IF(ABORT) THEN
+      ierr = threpa(c_report_blockname, g_report_output_filename)
+      if(ierr.ne.0) then
+        call g_append(err,'& threpa failed to append report')
+        report_abort = .true.
+      endif
+*
+      IF(ABORT.or.report_abort) THEN
          call G_add_path(here,err)
       ELSE
          err= ' '
@@ -46,3 +61,4 @@
 *     
       RETURN
       END
+
