1368d5c25e67170d15ada31354266e7674da71c0
cdaq <>
1994-09-13 19:39:14 +0000

    (JRA) Add preshower energy

--- HTRACKING/h_cal.f
+++ HTRACKING/h_cal.f
@@ -16,7 +16,10 @@
 *-      Created: 15 Mar 1994      Tsolak A. Amatuni
 *
 * $Log$
-* Revision 1.2  1994/04/12 21:24:55  cdaq
+* Revision 1.3  1994/09/13 19:39:14  cdaq
+* (JRA) Add preshower energy
+*
+* Revision 1.2  1994/04/12  21:24:55  cdaq
 * (DFG) Put in real code and change name of print routine.
 *
 * Revision 1.1  1994/02/19  06:12:35  cdaq
@@ -39,12 +42,13 @@
       include 'gen_data_structures.cmn'
       include 'hms_calorimeter.cmn'
 *
-      do nt=1,hntracks_max
+      do nt=1,hntracks_fp
          htrack_e1(nt)=0.
          htrack_e2(nt)=0.
          htrack_e3(nt)=0.
          htrack_e4(nt)=0.
          htrack_et(nt)=0.
+         htrack_preshower_e(nt)=0.
       enddo
 *
       call h_clusters_cal(abort,errmsg)
@@ -77,6 +81,7 @@
             htrack_e3(nt)=cor*hcluster_e3(nc)
             htrack_e4(nt)=cor*hcluster_e4(nc)
             htrack_et(nt)=cor*hcluster_et(nc)
+            htrack_preshower_e(nt)=cor*hcluster_e1(nc)
          endif                          !End ... if nc > 0
       enddo                             !End loop over detector tracks
 *
