97b617b6c7024ad0063931c8f17e4f3b6899967d
cdaq <>
1994-09-13 21:49:06 +0000

    (JRA) Calculate wire chamber offsets

--- HTRACKING/h_wire_center_calc.f
+++ HTRACKING/h_wire_center_calc.f
@@ -8,7 +8,10 @@
 *     modified   dfg  18 feb 1994
 *                         add option to reverse plane wire numbering
 * $Log$
-* Revision 1.2  1994/02/22 05:34:03  cdaq
+* Revision 1.3  1994/09/13 21:49:06  cdaq
+* (JRA) Calculate wire chamber offsets
+*
+* Revision 1.2  1994/02/22  05:34:03  cdaq
 * (SAW) Remove FLOAT call on floating arg
 *
 * Revision 1.1  1994/02/19  06:22:00  cdaq
@@ -33,11 +36,11 @@
       if(hdc_wire_counting(plane).eq.0) then 
 *          normal ordering
            h_wire_center_calc = (FLOAT(wire)-hdc_central_wire(plane))
-     &                     * hdc_pitch(plane)
+     &                     * hdc_pitch(plane) - hdc_center(plane)
       else        
            h_wire_center_calc = 
      &         ((hdc_nrwire(plane) + FLOAT(1 - wire))- hdc_central_wire(plane))
-     &                     * hdc_pitch(plane)
+     &                     * hdc_pitch(plane) - hdc_center(plane)
       endif
       return
       end
