c03864306f3351851258cee085cfed556a5e0524
cdaq <>
1994-11-22 20:15:35 +0000

    (SPB) Bring up to date with h_reset_event

--- ENGINE/s_reset_event.f
+++ ENGINE/s_reset_event.f
@@ -12,9 +12,12 @@
 *-   Created  2-Nov-1993   Kevin B. Beard
 *-   Modified 20-Nov-1993   KBB for new errors
 *-      $Log$
-*-      Revision 1.5  1994/06/22 20:51:22  cdaq
-*-      (SAW) Zero out the miscleaneous hits array
+*-      Revision 1.6  1994/11/22 20:15:35  cdaq
+*-      (SPB) Bring up to date with h_reset_event
 *-
+* Revision 1.5  1994/06/22  20:51:22  cdaq
+* (SAW) Zero out the miscleaneous hits array
+*
 * Revision 1.4  1994/03/24  22:01:43  cdaq
 * Reflect changes in gen_data_structures.cmn
 *
@@ -44,6 +47,7 @@
       character*(*) err
 *
       INCLUDE 'gen_data_structures.cmn'
+      INCLUDE 'sos_tracking.cmn'
 *
       INTEGER track,hit,block,i,j,plane
 *
@@ -63,10 +67,10 @@
          SDC_WIRE_NUM(hit)= 0.
          SDC_TDC(hit)= 0.
       ENDDO
+      SDC_TOT_HITS= 0
       DO plane= 1,SMAX_NUM_DC_PLANES
          SDC_HITS_PER_PLANE(plane)= 0
       ENDDO
-      SDC_TOT_HITS= 0
 *     
 *     SOS SCINTILLATOR HITS
 *     
@@ -81,11 +85,18 @@
          SSCIN_ADC_NEG(hit)= 0
          SSCIN_TDC_POS(hit)= 0
          SSCIN_TDC_NEG(hit)= 0
+         SSCIN_ALL_PLANE_NUM(hit)= 0
+         SSCIN_ALL_COUNTER_NUM(hit)= 0
+         SSCIN_ALL_ADC_POS(hit)= 0
+         SSCIN_ALL_ADC_NEG(hit)= 0
+         SSCIN_ALL_TDC_POS(hit)= 0
+         SSCIN_ALL_TDC_NEG(hit)= 0
       ENDDO
       DO plane= 1,SNUM_SCIN_PLANES
          SSCIN_HITS_PER_PLANE(plane)= 0
       ENDDO
       SSCIN_TOT_HITS= 0
+      SSCIN_ALL_TOT_HITS= 0
 *     
 *     SOS CALORIMETER HITS
 *     
@@ -127,11 +138,11 @@
          SYP_FP(track)= 0.
          SCHI2_FP(track)= 0.
          SNFREE_FP(track)= 0.
-         Do j= 1,4
-            do i= 1,4
-               SDEL_FP(i,j,track)= 0.
-            enddo
-         EndDo
+*         Do j= 1,4
+*            do i= 1,4
+*               SDEL_FP(i,j,track)= 0.
+*            enddo
+*         EndDo
          Do hit= 1,SNTRACKHITS_MAX
             SNTRACK_HITS(track,hit)= 0
          EndDo
@@ -149,6 +160,7 @@
          SDELTA_TAR(track)= 0.
          SP_TAR(track)= 0.
          SCHI2_TAR(track)= 0.
+         SDEL_TAR(5,5,track)= 0.
          SNFREE_TAR(track)= 0.
          SLINK_TAR_FP(track)= 0.
          Do j= 1,5
@@ -158,6 +170,54 @@
          EndDo
       ENDDO
       SNTRACKS_TAR= 0
+      DO track=1, SNTRACKS_MAX
+         SNBLOCKS_CAL(track)=0
+         STRACK_E1(track)=0
+         STRACK_E2(track)=0
+         STRACK_E3(track)=0
+         STRACK_E4(track)=0
+         STRACK_ET(track)=0
+         STRACK_PRESHOWER_E(track)=0
+          do hit = 1, SMAX_SCIN_HITS
+            SSCIN_HIT(track,hit)= 0
+          enddo
+          do plane = 1, SNUM_SCIN_PLANES
+             SDEDX(track,plane) = 0
+          enddo
+        SNUM_SCIN_HIT(track)=0
+        SBETA(track)=0
+        SBETA_CHISQ(track)=0
+        STIME_AT_FP(track)=0
+      ENDDO
+      
+      SSP=0
+      SSENERGY=0
+      SSDELTA=0
+      SSTHETA=0
+      SSPHI=0
+      SSMINV=0
+      SSZBEAM=0
+      SSDEDX1=0
+      SSDEDX2=0
+      SSDEDX3=0
+      SSDEDX4=0
+      SSBETA=0
+      SSTRACK_ET=0
+      SSTRACK_PRESHOWER_E=0
+      SSTIME_AT_FP=0
+      SSX_FP=0
+      SSY_FP=0
+      SSXP_FP=0
+      SSYP_FP=0
+      SSCHI2PERDEG=0
+      SSX_TAR=0
+      SSY_TAR=0
+      SSXP_TAR=0
+      SSYP_TAR=0
+      SSNUM_FPTRACK=0
+      SSNUM_TARTRACK=0
+      SSID_LUND=0
+      SSNFREE_FP=0
 *     
       ABORT= .FALSE.
       err= ' '
