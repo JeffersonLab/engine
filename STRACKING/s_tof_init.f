881ccadad44beba5df1e07dc64a1cbe54620fb18
cdaq <>
1994-11-23 14:24:18 +0000

    (SPB) Recopied from hms file and modified names for SOS

--- STRACKING/s_tof_init.f
+++ STRACKING/s_tof_init.f
@@ -1,4 +1,4 @@
-        subroutine s_tof_init(abort,err)
+      subroutine s_tof_init(abort,err)
 
 *-------------------------------------------------------------------
 * author: John Arrington
@@ -9,7 +9,10 @@
 *
 * modifications: 31 Mar 1994    DFG  Check for 0 hits
 * $Log$
-* Revision 1.2  1994/06/01 15:40:08  cdaq
+* Revision 1.3  1994/11/23 14:23:05  cdaq
+* (SPB) Recopied from hms file and modified names for SOS
+*
+* Revision 1.2  1994/06/01  15:40:08  cdaq
 * (SAW) Change declaration of err to *(*)
 *
 * Revision 1.1  1994/04/13  18:45:03  cdaq
@@ -17,26 +20,27 @@
 *
 *-------------------------------------------------------------------
 
-        implicit none
+      implicit none
 
-        include 'gen_data_structures.cmn'
-        include 'sos_scin_parms.cmn'
-        include 'sos_scin_tof.cmn'
+      include 'gen_data_structures.cmn'
+      include 'sos_scin_parms.cmn'
+      include 'sos_scin_tof.cmn'
 
-        logical abort
-        character*(*) err
-        character*20 here
-        parameter (here = 's_tof_init')
+      logical abort
+      character*(*) err
+      character*20 here
+      parameter (here = 's_tof_init')
 
-        integer*4 ihit,plane,counter
-        save
+      integer*4 ihit,plane,counter
+      save
 
-        if(sscin_tot_hits.gt.0) then
+      if(sscin_tot_hits.gt.0) then
         do ihit = 1 , sscin_tot_hits
 
-          plane = sscin_plane_num(ihit)     !from s_raw_scin common block.
+          plane = sscin_plane_num(ihit) !from s_raw_scin common block.
           counter = sscin_counter_num(ihit)
-
+          
+          sscin_slop(ihit) = shodo_slop(plane)
           sscin_pos_sigma(ihit) = shodo_pos_sigma(plane,counter)
           sscin_neg_sigma(ihit) = shodo_neg_sigma(plane,counter)
           sscin_center_coord(ihit) = shodo_center_coord(plane,counter)
@@ -46,34 +50,46 @@
           sscin_pos_time_offset(ihit) = shodo_pos_time_offset(plane,counter)
           sscin_neg_time_offset(ihit) = shodo_neg_time_offset(plane,counter)
 
-          if (plane .eq. 1) then                   !1x
+          if (plane .eq. 1) then        !1x
             sscin_zpos(ihit) = sscin_1x_zpos
+            if (2*int(float(counter)/2.) .eq. counter) then !even tube, in back.
+              sscin_zpos(ihit) = sscin_zpos(ihit) + sscin_1x_dzpos
+            endif
             sscin_pos_coord(ihit) = sscin_1x_left
             sscin_neg_coord(ihit) = sscin_1x_right
             sscin_width(ihit) = sscin_1x_size
-          else if (plane .eq. 2) then              !1y
+          else if (plane .eq. 2) then   !1y
             sscin_zpos(ihit) = sscin_1y_zpos
+            if (2*int(float(counter)/2.) .eq. counter) then !even tube, in back.
+              sscin_zpos(ihit) = sscin_zpos(ihit) + sscin_1y_dzpos
+            endif
             sscin_pos_coord(ihit) = sscin_1y_bot
             sscin_neg_coord(ihit) = sscin_1y_top
             sscin_width(ihit) = sscin_1y_size
-          else if (plane .eq. 3) then              !2x
+          else if (plane .eq. 3) then   !2x
             sscin_zpos(ihit) = sscin_2x_zpos
+            if (2*int(float(counter)/2.) .eq. counter) then !even tube, in back.
+              sscin_zpos(ihit) = sscin_zpos(ihit) + sscin_2x_dzpos
+            endif
             sscin_pos_coord(ihit) = sscin_2x_left
             sscin_neg_coord(ihit) = sscin_2x_right
             sscin_width(ihit) = sscin_2x_size
-          else if (plane .eq. 4) then              !2y
+          else if (plane .eq. 4) then   !2y
             sscin_zpos(ihit) = sscin_2y_zpos
+            if (2*int(float(counter)/2.) .eq. counter) then !even tube, in back.
+              sscin_zpos(ihit) = sscin_zpos(ihit) + sscin_2y_dzpos
+            endif
             sscin_pos_coord(ihit) = sscin_2y_bot
             sscin_neg_coord(ihit) = sscin_2y_top
             sscin_width(ihit) = sscin_2y_size
           else
-              abort = .true.
-              write(err,*) 'Trying to init. sos hodoscope plane',plane
-              call g_prepend(here,err)
-              return
+            abort = .true.
+            write(err,*) 'Trying to init. sos hodoscope plane',plane
+            call g_prepend(here,err)
+            return
           endif
 
         enddo
-        endif      ! end test on zero hits
-        return
-        end
+      endif                             ! end test on zero hits
+      return
+      end
--- STRACKING/s_track_fit.f
+++ STRACKING/s_track_fit.f
@@ -5,16 +5,20 @@
 *
 *     d.f. geesaman         8 Sept 1993
 * $Log$
-* Revision 1.1  1994/02/21 16:42:27  cdaq
-* Initial revision
+* Revision 1.2  1994/11/23 14:24:18  cdaq
+* (SPB) Recopied from hms file and modified names for SOS
 *
+* Revision 1.1  1994/02/21  16:42:27  cdaq
+* Initial revision
 *
       implicit none
       include "gen_data_structures.cmn"
       include "sos_tracking.cmn"
 *
       external S_FCNCHISQ
-      real*8 S_FCNCHISQ 
+      external S_DPSIFUN
+      real*8 S_DPSIFUN
+      real*8 S_FCNCHISQ
 *
 *     local variables
 *
@@ -25,8 +29,11 @@
       integer*4 itrack                        ! track loop index
       integer*4 i                             ! loop index
       integer*4 ierr                          ! error return flag
-      integer*4 dummy,ivarbl                  ! dummy MINUIT variable
+      integer*4 ivarbl                  ! dummy MINUIT variable
+      integer*4 ihit, plane, hit
+      real*8 pos
       real*8 ray(snum_fpray_param)
+      real*8 ray1(4), ray2(4)
       real*8 error(snum_fpray_param)
       real*8 initialray(snum_fpray_param)
       real*8 initialsteps(snum_fpray_param)
@@ -45,6 +52,19 @@
 *
       ABORT= .FALSE.
       ierr=0
+*  initailize residuals
+
+      do itrack=1,SNTRACKS_MAX
+        do plane=1,SMAX_NUM_DC_PLANES
+          sdc_residual(itrack,plane)=1000
+          sdc_sing_res(itrack,plane)=1000
+          sdc1_sing_res(plane)=1000
+          sdc2_sing_res(plane)=1000
+          sdc1_dbl_res(plane)=1000
+          sdc2_dbl_res(plane)=1000
+
+        enddo
+      enddo
 *     test for no tracks
       if(SNTRACKS_FP.ge.1) then
         do itrack=1,SNTRACKS_FP
@@ -83,6 +103,64 @@
           endif                               ! end test on degrees of freedom
         enddo                                 ! end loop over tracks
       endif
+* calculate residuals for each chamber if in single stub mode
+* and there were 2 tracks found one in first chanber and one in the second
+
+      if (ssingle_stub.ne.0) then
+        if (SNTRACKS_FP.eq.2) then
+          itrack=1
+          ihit=2
+          hit=SNTRACK_HITS(itrack,ihit)
+          plane=SDC_PLANE_NUM(hit)
+          if (plane.le.6) then
+            itrack=2
+            hit=SNTRACK_HITS(itrack,ihit)
+            plane=SDC_PLANE_NUM(hit)
+            if (plane.ge.7) then
+
+* condition of above met calculating residuals  
+* assigning rays to tracks in each chamber
+* ray1 is ray from first chamber fit
+* ray2 is ray from second chamber fit
+
+              ray1(1)=dble(SX_FP(1))
+              ray1(2)=dble(SY_FP(1))
+              ray1(3)=dble(SXP_FP(1))
+              ray1(4)=dble(SYP_FP(1))
+              ray2(1)=dble(SX_FP(2))
+              ray2(2)=dble(SY_FP(2))
+              ray2(3)=dble(SXP_FP(2))
+              ray2(4)=dble(SYP_FP(2))
+
+              itrack=1
+* loop over hits in second chamber
+              do ihit=1,SNTRACK_HITS(itrack+1,1)
+
+* calculate residual in second chamber from first chamber track
+                hit=SNTRACK_HITS(itrack+1,ihit+1)
+                plane=SDC_PLANE_NUM(hit)
+                pos=S_DPSIFUN(ray1,plane)
+                sdc_residual(itrack,plane)=SDC_WIRE_COORD(hit)-pos
+                enddo
+
+              itrack=2
+* loop over hits in first chamber
+              do ihit=1,SNTRACK_HITS(itrack-1,1)
+
+* calculate residual in first chamber from second chamber track
+                hit=SNTRACK_HITS(itrack-1,ihit+1)
+                plane=SDC_PLANE_NUM(hit)
+                pos=S_DPSIFUN(ray2,plane)
+                sdc_residual(itrack,plane)=SDC_WIRE_COORD(hit)-pos
+* djm 8/31/94 stuff this variable into 1d array we can register
+                sdc1_dbl_res(plane) = sdc_residual(2,plane)
+
+              enddo
+            endif                       ! end plane ge 7
+          endif                         ! end plane le 6
+        endif                           ! end SNTRACKS_FP eq 2
+      endif                             ! end ssignle_stub .ne. 0
+
 *     test if we want to dump out trackfit results
       if(sdebugtrackprint.ne.0) then
          call s_print_tracks
