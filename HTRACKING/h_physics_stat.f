5f8381aff8530b1b341454dfac2d263c44b65226
cdaq <>
1995-02-23 15:37:57 +0000

    (JRA) Move scint eff's to h_scin_eff, add call to h_cal_eff

--- HTRACKING/h_physics_stat.f
+++ HTRACKING/h_physics_stat.f
@@ -16,7 +16,10 @@
 *- 
 *-   Created 10-JUN-1994     D. F. Geesaman
 * $Log$
-* Revision 1.2  1994/06/15 20:22:49  cdaq
+* Revision 1.3  1995/02/23 15:37:57  cdaq
+* (JRA) Move scint eff's to h_scin_eff, add call to h_cal_eff
+*
+* Revision 1.2  1994/06/15  20:22:49  cdaq
 * (DFG) Add scin plane efficiency
 *
 c Revision 1.1  1994/06/15  19:09:37  cdaq
@@ -31,7 +34,6 @@ c-
 *
       logical ABORT
       character*(*) err
-      integer*4 ierr
 *
       INCLUDE 'gen_data_structures.cmn'
       INCLUDE 'gen_routines.dec'
@@ -42,7 +44,7 @@ c-
       INCLUDE 'hms_statistics.cmn'
 *
 *     local variables 
-      integer*4 goodtrack,tothits,ihit,sigma,hitnum,plane
+      integer*4 goodtrack,tothits,ihit,hitnum,plane
       real*4 normsigma
       real*8 ray(4)                     ! xt,yt,xpt,ypt
       EXTERNAL  H_DPSIFUN
@@ -58,43 +60,34 @@ c-
       tothits=HNTRACK_HITS(goodtrack,1)
       if(tothits.gt.0) then
 *     get ray parameters
-         ray(1) = DBLE(HX_FP(goodtrack))
-         ray(2) = DBLE(HY_FP(goodtrack))
-         ray(3) = DBLE(HXP_FP(goodtrack))
-         ray(4) = DBLE(HYP_FP(goodtrack))
+        ray(1) = DBLE(HX_FP(goodtrack))
+        ray(2) = DBLE(HY_FP(goodtrack))
+        ray(3) = DBLE(HXP_FP(goodtrack))
+        ray(4) = DBLE(HYP_FP(goodtrack))
 
 *     loop over all hits in track
          
 
-         do ihit = 1, tothits
-            hitnum=HNTRACK_HITS(goodtrack,1+ihit)
-            plane = HDC_PLANE_NUM(hitnum)
-            normsigma = (HDC_WIRE_COORD(hitnum)
-     $           - REAL(H_DPSIFUN(ray,plane)))/hdc_sigma(plane)
-            hplanehitctr(plane) = hplanehitctr(plane) + 1
-            hplanesigmasq(plane) = hplanesigmasq(plane) + normsigma
-     $           *normsigma
-            hmeasuredsigma(plane) = SQRT(hplanesigmasq(plane) 
-     &           / FLOAT(hplanehitctr(plane)))
-            hchambereff(plane)=FLOAT(hplanehitctr(plane))
-     $           /FLOAT(hgoodtracksctr)
-         enddo                          ! endloop over hits in track
+        do ihit = 1, tothits
+          hitnum=HNTRACK_HITS(goodtrack,1+ihit)
+          plane = HDC_PLANE_NUM(hitnum)
+          normsigma = (HDC_WIRE_COORD(hitnum)
+     $         - REAL(H_DPSIFUN(ray,plane)))/hdc_sigma(plane)
+          hplanehitctr(plane) = hplanehitctr(plane) + 1
+          hplanesigmasq(plane) = hplanesigmasq(plane) + normsigma
+     $         *normsigma
+          hmeasuredsigma(plane) = SQRT(hplanesigmasq(plane) 
+     &         / FLOAT(hplanehitctr(plane)))
+          hchambereff(plane)=FLOAT(hplanehitctr(plane))
+     $         /FLOAT(hgoodtracksctr)
+        enddo                           ! endloop over hits in track
       endif                             ! end test on zero hits
 *     
 *     Scintillator efficiencies
-      tothits = HNUM_SCIN_HIT(goodtrack)
-      if(tothits.gt.0) then
-*     loop over all hits in track
-         
-
-         do ihit = 1, tothits
-            hitnum=HSCIN_HIT(goodtrack,ihit)
-            plane = HSCIN_PLANE_NUM(hitnum)
-            hscinplanehitctr(plane) = hscinplanehitctr(plane) + 1
-            hscinplaneeff(plane) = FLOAT(hscinplanehitctr(plane))
-     &           /FLOAT(hgoodtracksctr)
-         enddo                          ! endloop over hits in track
-      endif                             ! end test on zero hits
+      call h_scin_eff
 *     
+*     Calorimeter efficiencies
+      call h_cal_eff
+*
       RETURN
       END
