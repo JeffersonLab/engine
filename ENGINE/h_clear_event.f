b1fa56e9ee0a632a1d526d3b9f088d27ad9a25f1
cdaq <>
1995-03-13 18:12:46 +0000

    (SAW) Include file ordering

--- ENGINE/h_clear_event.f
+++ ENGINE/h_clear_event.f
@@ -11,9 +11,12 @@
 *- 
 *-   Created  29-Oct-1993   Kevin B. Beard
 *-    $Log$
-*-    Revision 1.8  1994/10/11 20:27:35  cdaq
-*-    (JRA) Include additional common blocks
+*-    Revision 1.9  1995/03/13 18:12:46  cdaq
+*-    (SAW) Include file ordering
 *-
+* Revision 1.8  1994/10/11  20:27:35  cdaq
+* (JRA) Include additional common blocks
+*
 * Revision 1.7  1994/09/20  17:29:41  cdaq
 * (SAW) Add include of hms_tracking.cmn
 *
@@ -54,8 +57,8 @@
       INCLUDE 'gen_data_structures.cmn'
       INCLUDE 'hms_tracking.cmn'
       INCLUDE 'hms_statistics.cmn'
-      INCLUDE 'hms_scin_tof.cmn'
       INCLUDE 'hms_scin_parms.cmn'
+      INCLUDE 'hms_scin_tof.cmn'
       INCLUDE 'hms_calorimeter.cmn'
 
 *
