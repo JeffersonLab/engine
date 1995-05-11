1bbc83f7a14ed1aaec7d1c4d7ffa4bcbd7997b2d
cdaq <>
1995-05-11 16:16:11 +0000

    (SAW) Don't get g_config_filename from environment if it is already set       from the command line and allow %d run number substitution in it.

--- ENGINE/g_init_filenames.f
+++ ENGINE/g_init_filenames.f
@@ -23,9 +23,13 @@
 *-    Modified   3-Dec-1993 Kevin Beard, Hampton U.
 *-    Modified   8-Dec-1993 Kevin Beard; rewrote parsing,added 'data' type
 *-    $Log$
-*-    Revision 1.8  1995/04/01 19:46:13  cdaq
-*-    (SAW) One report file for each of g, h, s, c instead of a single report file
+*-    Revision 1.9  1995/05/11 16:16:11  cdaq
+*-    (SAW) Don't get g_config_filename from environment if it is already set
+*-          from the command line and allow %d run number substitution in it.
 *-
+* Revision 1.8  1995/04/01  19:46:13  cdaq
+* (SAW) One report file for each of g, h, s, c instead of a single report file
+*
 * Revision 1.7  1994/10/19  19:51:55  cdaq
 * (SAW) Add g_label variable for labels on reports
 *
@@ -63,8 +67,10 @@
       include 'sos_filenames.cmn'
       include 'coin_filenames.cmn'
       include 'gen_routines.dec'
+      include 'gen_run_info.cmn'
 *
       integer ierr
+      character*132 file
 *
 *-all crucial setup information here; failure is fatal
 *
@@ -75,7 +81,7 @@
       g_ctp_parm_filename = ' '
       g_ctp_test_filename = ' '
       g_ctp_hist_filename = ' '
-      g_data_source_filename= ' '        !undefined
+      g_data_source_filename= ' '
       g_alias_filename = ' '
       g_histout_filename = ' '
       g_decode_map_filename = ' '
@@ -100,7 +106,8 @@
 *
       g_label = ' '                     ! Label for reports etc.
 *
-      call getenv(env_var,g_config_filename)
+      if(g_config_filename.eq.' ')      ! Only if not already set
+     $     call getenv(env_var,g_config_filename)
 *
       ABORT= g_config_filename.EQ.' '
       IF(ABORT) THEN
@@ -108,9 +115,11 @@
         RETURN
       ENDIF
 *
-      ierr =  thload(g_config_filename)         ! Config file is now
+      file = g_config_filename
+      call g_sub_run_number(file,gen_run_number)
+      ierr =  thload(file)         ! Config file is now a CTP parm file
       if(ierr.ne.0) goto 999
-      ierr = thbook()                             ! a CTP parm file
+      ierr = thbook()
       if(ierr.eq.0) then
          g_config_loaded = .true.
       else
@@ -119,8 +128,7 @@
 
       ABORT= .NOT.g_config_loaded
       IF(ABORT) THEN
-        error= ':opened OK, but thload command failed from "'//
-     &                                  g_config_filename//'"'
+        error= ':opened OK, but thload command failed from "'//file//'"'
         call G_add_path(here,error)
       ELSE
         error= ' '
@@ -130,7 +138,7 @@
 *
 999   g_config_loaded= .FALSE.
       ABORT= .NOT.g_config_loaded
-      error= ':unable to open file "'//g_config_filename//'"'
+      error= ':unable to open file "'//file//'"'
       call G_add_path(here,error)
       return
 *
