bcf6dafb5c6a30b868f65f1b7daf9d732e637093
cdaq <>
1995-05-11 16:18:51 +0000

    (SAW) Allow %d run number substitution in data source filename

--- ENGINE/g_open_source.f
+++ ENGINE/g_open_source.f
@@ -9,9 +9,12 @@
 *- 
 *-   Created  30-Nov-1993   Kevin B. Beard
 *-    $Log$
-*-    Revision 1.2  1995/01/27 20:11:48  cdaq
-*-    (SAW) Add setting of ABORT
+*-    Revision 1.3  1995/05/11 16:18:51  cdaq
+*-    (SAW) Allow %d run number substitution in data source filename
 *-
+* Revision 1.2  1995/01/27  20:11:48  cdaq
+* (SAW) Add setting of ABORT
+*
 * Revision 1.1  1994/02/04  22:11:29  cdaq
 * Initial revision
 *
@@ -23,25 +26,27 @@
       IMPLICIT NONE
       SAVE
 *
-      character*40 here
+      character*13 here
       parameter (here= 'G_open_source')
 *
       logical ABORT
       character*(*) err
 *
       INCLUDE 'gen_filenames.cmn'
+      INCLUDE 'gen_run_info.cmn'
 *
-      logical OK
-      character*8  flag           !must be declared exactly
       integer*4 status
       integer*4 evopen                          ! CODA routine
+      character*132 file
 *
 *--------------------------------------------------------
       err= ' '
       ABORT = .FALSE.
       g_data_source_in_hndl= 0
 *
-      status = evopen(g_data_source_filename,'r',g_data_source_in_hndl)
+      file = g_data_source_filename
+      call g_sub_run_number(file,gen_run_number)
+      status = evopen(file,'r',g_data_source_in_hndl)
       if(status.ne.0) then
 *         call cemsg(status,0,err)
          g_data_source_opened = .false.
@@ -50,7 +55,7 @@
       endif
 *
       IF(.not.g_data_source_opened) THEN
-        err= ':could not open "'//g_data_source_filename//'"'
+        err= ':could not open "'//file//'"'
         call G_add_path(here,err)
         ABORT = .TRUE.
       ENDIF
