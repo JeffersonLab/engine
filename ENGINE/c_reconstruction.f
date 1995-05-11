0e085da3f51bc0ce7a60be166bac0f8c6caf90c9
cdaq <>
1995-05-11 15:48:15 +0000

    (SAW) Add call to c_physics for coincidence variables

--- ENGINE/c_reconstruction.f
+++ ENGINE/c_reconstruction.f
@@ -11,9 +11,12 @@
 *-   Created  8-Nov-1993   Kevin B. Beard, HU
 *-   Modified 20-Nov-1993   KBB for new errors
 *-    $Log$
-*-    Revision 1.3  1994/06/17 03:16:29  cdaq
-*-    (KBB) Remove not yet written warning
+*-    Revision 1.4  1995/05/11 15:48:15  cdaq
+*-    (SAW) Add call to c_physics for coincidence variables
 *-
+* Revision 1.3  1994/06/17  03:16:29  cdaq
+* (KBB) Remove not yet written warning
+*
 * Revision 1.2  1994/02/04  21:09:43  cdaq
 * Fix indentation
 *
@@ -37,14 +40,22 @@ c
       INCLUDE 'gen_data_structures.cmn'
       INCLUDE 'gen_constants.par'
       INCLUDE 'gen_units.par'
+      INCLUDE 'coin_bypass_switches.cmn'
 *
 *--------------------------------------------------------
 *
       ABORT= .FALSE.
       err = ' '
-*      err= here//':not yet written!'            !warning
 *
-      IF(ABORT) call G_add_path(here,err)
+      if(cbypass_physics.eq.0) then
+        call c_physics(abort,err)
+        IF(ABORT) call G_add_path(here,err)
+        return
+      endif
+*
+*     Successful return
+*
+      abort = .false.
 *
       RETURN
       END
