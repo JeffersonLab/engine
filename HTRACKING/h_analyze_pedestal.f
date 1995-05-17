aca6d014d14b3a34e70249803880992356c61664
cdaq <>
1995-05-17 13:56:04 +0000

    (JRA) Add Cernekov pedestals, cosmetic changes

--- HTRACKING/h_analyze_pedestal.f
+++ HTRACKING/h_analyze_pedestal.f
@@ -1,7 +1,10 @@
       subroutine h_analyze_pedestal(ABORT,err)
 *
 * $Log$
-* Revision 1.1  1995/04/01 19:36:40  cdaq
+* Revision 1.2  1995/05/17 13:56:04  cdaq
+* (JRA) Add Cernekov pedestals, cosmetic changes
+*
+* Revision 1.1  1995/04/01  19:36:40  cdaq
 * Initial revision
 *
 *
@@ -16,7 +19,9 @@
 *
       integer*4 ihit
       integer*4 pln,cnt
+      integer*4 row,col
       integer*4 blk
+      integer*4 pmt
 *
       INCLUDE 'gen_data_structures.cmn'
       INCLUDE 'hms_pedestals.cmn'
@@ -29,9 +34,9 @@
         pln = hscin_all_plane_num(ihit)
         cnt = hscin_all_counter_num(ihit)
         hhodo_pos_ped_sum2(pln,cnt) = hhodo_pos_ped_sum2(pln,cnt) +
-     &          hscin_all_adc_pos(ihit) * hscin_all_adc_pos(ihit)
+     &          hscin_all_adc_pos(ihit)*hscin_all_adc_pos(ihit)
         hhodo_neg_ped_sum2(pln,cnt) = hhodo_neg_ped_sum2(pln,cnt) +
-     &          hscin_all_adc_neg(ihit) * hscin_all_adc_neg(ihit)
+     &          hscin_all_adc_neg(ihit)*hscin_all_adc_neg(ihit)
         hhodo_pos_ped_sum(pln,cnt) = hhodo_pos_ped_sum(pln,cnt) +
      &          hscin_all_adc_pos(ihit)
         hhodo_neg_ped_sum(pln,cnt) = hhodo_neg_ped_sum(pln,cnt) +
@@ -44,11 +49,23 @@
 * CALORIMETER PEDESTALS
 *
 
-      do blk = 1 , hcal_tot_hits
-        hcal_ped_sum2(blk) = hcal_ped_sum2(blk) +
-     &          hscin_all_adc_pos(blk) * hscin_all_adc_pos(blk)
-        hcal_ped_sum(blk) = hcal_ped_sum(blk) + hscin_all_adc_pos(blk)
+      do ihit = 1 , hcal_tot_hits
+        row = hcal_row(ihit)
+        col = hcal_column(ihit)
+        blk = row + (col-1)*hmax_cal_rows
+        hcal_ped_sum2(blk) = hcal_ped_sum2(blk) + hcal_adc(ihit)*hcal_adc(ihit)
+        hcal_ped_sum(blk) = hcal_ped_sum(blk) + hcal_adc(ihit)
         hcal_ped_num(blk) = hcal_ped_num(blk) + 1
+      enddo
+*
+*
+* CERENKOV PEDESTALS
+*
+      do ihit = 1 , hcer_tot_hits
+        pmt=hcer_tube_num(ihit)      ! no sparsification yet - NEED TO FIX!!!!
+        hcer_ped_sum2(pmt) = hcer_ped_sum2(pmt) + hcer_adc(ihit)*hcer_adc(ihit)
+        hcer_ped_sum(pmt) = hcer_ped_sum(pmt) + hcer_adc(ihit)
+        hcer_ped_num(pmt) = hcer_ped_num(pmt) + 1
       enddo
 
       return
