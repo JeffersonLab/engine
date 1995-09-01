d124a8ded39eede25319c612a33f0e82c65d5aac
cdaq <>
1995-09-01 15:46:13 +0000

    (JRA) Add call to sos sieve slit ntuple

--- ENGINE/g_ntuple_shutdown.f
+++ ENGINE/g_ntuple_shutdown.f
@@ -12,8 +12,11 @@
 *- 
 *-   Created  30-June-1995 SAW
 *  $Log$
-*  Revision 1.1  1995/07/27 19:00:55  cdaq
-*  Initial revision
+*  Revision 1.2  1995/09/01 15:46:13  cdaq
+*  (JRA) Add call to sos sieve slit ntuple
+*
+* Revision 1.1  1995/07/27  19:00:55  cdaq
+* Initial revision
 *
 *--------------------------------------------------------
       IMPLICIT NONE
@@ -31,7 +34,7 @@
 *
       call s_ntuple_shutdown(ABORT,err)
 *
-c*      call s_sv_nt_shutdown(ABORT,err)
+      call s_sv_nt_shutdown(ABORT,err)
 *
       call c_ntuple_shutdown(ABORT,err)
 *
