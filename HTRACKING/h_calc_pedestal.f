af3427339e6901f1c58af4551326ea0d3b273c50
cdaq <>
1995-10-09 20:12:30 +0000

    (JRA) Note pedestals that differ by 2 sigma from parameter file

--- HTRACKING/h_calc_pedestal.f
+++ HTRACKING/h_calc_pedestal.f
@@ -1,6 +1,9 @@
       subroutine h_calc_pedestal(ABORT,err)
 *
 * $Log$
+* Revision 1.6  1995/10/09 20:12:10  cdaq
+* (JRA) Note pedestals that differ by 2 sigma from parameter file
+*
 * Revision 1.5  1995/08/31 14:58:48  cdaq
 * (JRA) Change threshold limits
 *
@@ -29,7 +32,9 @@
       integer*4 pln,cnt
       integer*4 blk
       integer*4 pmt
+      integer*4 ind
       real*4 sig2
+      real*4 num
 *
       INCLUDE 'hms_data_structures.cmn'
       INCLUDE 'hms_pedestals.cmn'
@@ -39,63 +44,110 @@
 *
 * HODOSCOPE PEDESTALS
 *
+      ind = 0
       do pln = 1 , hnum_scin_planes
         do cnt = 1 , hnum_scin_elements
-          if (hhodo_pos_ped_num(pln,cnt) .ge. hhodo_min_peds .and.
-     $        hhodo_min_peds .ne. 0) then
-            hscin_all_ped_pos(pln,cnt) = float(hhodo_pos_ped_sum(pln,cnt)) /
-     $              float(hhodo_pos_ped_num(pln,cnt))
-            sig2 = float(hhodo_pos_ped_sum2(pln,cnt))/
-     $           float(hhodo_pos_ped_num(pln,cnt))-
-     $           hscin_all_ped_pos(pln,cnt)**2
-!            if (sig2.le.0) write(6,*) 'pos ped(',pln,',',cnt,')**2 =',sig2
-            hhodo_all_sig_pos(pln,cnt) = sqrt(max(0.,sig2))
+
+*calculate new pedestal values, positive tubes first.
+          num=max(1.,float(hhodo_pos_ped_num(pln,cnt)))
+          hhodo_new_ped_pos(pln,cnt) = float(hhodo_pos_ped_sum(pln,cnt)) / num
+          sig2 = float(hhodo_pos_ped_sum2(pln,cnt))/num -
+     $           hhodo_new_ped_pos(pln,cnt)**2
+          hhodo_new_sig_pos(pln,cnt) = sqrt(max(0.,sig2))
+
+*note channels with 2 sigma difference from paramter file values.
+          if (abs(hscin_all_ped_pos(pln,cnt)-hhodo_new_ped_pos(pln,cnt))
+     &            .ge.(2.*hhodo_new_sig_pos(pln,cnt))) then
+            ind = ind + 1     !final value of 'ind' is saved at end of loop
+            hhodo_changed_plane(ind)=pln
+            hhodo_changed_element(ind)=cnt
+            hhodo_changed_sign(ind)= 1       !1=pos,2=neg.
+            hhodo_ped_change(ind) = hhodo_new_ped_pos(pln,cnt) - 
+     &                              hscin_all_ped_pos(pln,cnt)
+          endif   !large pedestal change
+
+*replace old peds (from param file) with calculated pedestals
+          if (num.gt.hhodo_min_peds .and. hhodo_min_peds.ne.0) then
+            hscin_all_ped_pos(pln,cnt)=hhodo_new_ped_pos(pln,cnt)
           endif
-          if (hhodo_neg_ped_num(pln,cnt) .ge. hhodo_min_peds .and.
-     $        hhodo_min_peds .ne. 0) then
-            hscin_all_ped_neg(pln,cnt) = float(hhodo_neg_ped_sum(pln,cnt)) /
-     $              float(hhodo_neg_ped_num(pln,cnt))
-            sig2 = float(hhodo_neg_ped_sum2(pln,cnt))/
-     $           float(hhodo_neg_ped_num(pln,cnt))-
-     $           hscin_all_ped_neg(pln,cnt)**2
-!            if (sig2.le.0) write(6,*) 'neg ped(',pln,',',cnt,')**2 =',sig2
-            hhodo_all_sig_neg(pln,cnt) = sqrt(max(0.,sig2))
+
+*do it all again for negative tubes.
+          num=max(1.,float(hhodo_neg_ped_num(pln,cnt)))
+          hhodo_new_ped_neg(pln,cnt) = float(hhodo_neg_ped_sum(pln,cnt)) / num
+          sig2 = float(hhodo_neg_ped_sum2(pln,cnt))/num -
+     $           hhodo_new_ped_neg(pln,cnt)**2
+          hhodo_new_sig_neg(pln,cnt) = sqrt(max(0.,sig2))
+
+          if (abs(hscin_all_ped_neg(pln,cnt)-hhodo_new_ped_neg(pln,cnt))
+     &            .ge.(2.*hhodo_new_sig_neg(pln,cnt))) then
+            ind = ind + 1
+            hhodo_changed_plane(ind)=pln
+            hhodo_changed_element(ind)=cnt
+            hhodo_changed_sign(ind)= 2       !1=pos, 2=neg.
+            hhodo_ped_change(ind) = hhodo_new_ped_neg(pln,cnt) - 
+     &                              hscin_all_ped_neg(pln,cnt)
+          endif   !large pedestal change
+
+          if (num.gt.hhodo_min_peds .and. hhodo_min_peds.ne.0) then
+            hscin_all_ped_neg(pln,cnt)=hhodo_new_ped_neg(pln,cnt)
           endif
-        enddo
-      enddo
+
+        enddo                      !counters
+      enddo                        !planes
+      hhodo_num_ped_changes = ind
 *
 *
 * CALORIMETER PEDESTALS
 *
+      ind = 0
       do blk = 1 , hmax_cal_blocks
-        if (hcal_ped_num(blk) .ge. hcal_min_peds .and.
-     $      hcal_min_peds .ne. 0) then
-          hcal_ped_mean(blk) = hcal_ped_sum(blk) / float(hcal_ped_num(blk))
-          sig2 = float(hcal_ped_sum2(blk))/float(hcal_ped_num(blk)) -
-     $         hcal_ped_mean(blk)**2
-!          if (sig2.le.0) write(6,*) 'cal ped(',blk,')**2 =',sig2
-          hcal_ped_rms(blk) = sqrt(max(0.,sig2))
-          hcal_threshold(blk) = min(50.,max(10.,3.*hcal_ped_rms(blk)))
+        num=max(1.,float(hcal_ped_num(blk)))
+        hcal_new_ped(blk) = hcal_ped_sum(blk) / num
+        sig2 = float(hcal_ped_sum2(blk))/num - hcal_new_ped(blk)**2
+        hcal_new_rms(blk) = sqrt(max(0.,sig2))
+
+        if (abs(hcal_ped_mean(blk)-hcal_new_ped(blk))
+     &                 .ge.(2.*hcal_new_rms(blk))) then
+          ind = ind + 1
+          hcal_changed_block(ind)=blk
+          hcal_ped_change(ind)=hcal_new_ped(blk)-hcal_ped_mean(blk) 
         endif
+
+        if (num.gt.hcal_min_peds .and. hcal_min_peds.ne.0) then
+          hcal_ped_mean(blk)=hcal_new_ped(blk)
+          hcal_ped_rms(blk)=hcal_new_rms(blk)
+          hcal_threshold(blk) = min(50.,max(10.,3.*hcal_new_rms(blk)))
+        endif
+
       enddo
+      hcal_num_ped_changes = ind
+
 
 *
 *
 * GAS CERENKOV PEDESTALS
 *
+      ind = 0
       do pmt = 1 , hmax_cer_hits
-        if (hcer_ped_num(pmt) .ge. hcer_min_peds .and.
-     $      hcer_min_peds .ne. 0) then
-          hcer_ped_mean(pmt) = float(hcer_ped_sum(pmt))
-     $         / float(hcer_ped_num(pmt))
-          sig2 = float(hcer_ped_sum2(pmt))/
-     $         float(hcer_ped_num(pmt))-
-     $         hcer_ped_mean(pmt)**2
-!          if (sig2.le.0) write(6,*) 'cer ped(',pmt,')**2 =',sig2
-          hcer_ped_rms(pmt) = sqrt(max(0.,sig2))
-          hcer_threshold(pmt) = max(4.,3.*hcer_ped_rms(pmt))
-        endif
+        num=max(1.,float(hcer_ped_num(pmt)))
+        hcer_new_ped(pmt) = float(hcer_ped_sum(pmt)) / num
+        sig2 = float(hcer_ped_sum2(pmt))/ num - hcer_new_ped(pmt)**2
+        hcer_new_rms(pmt) = sqrt(max(0.,sig2))
       enddo
+      if (abs(hcer_ped_mean(pmt)-hcer_new_ped(pmt))
+     &              .ge.(2.*hcer_new_rms(pmt))) then
+        ind = ind + 1
+        hcer_changed_tube(ind)=pmt
+        hcer_ped_change(ind)=hcer_new_ped(pmt)-hcer_ped_mean(pmt) 
+      endif
+      hcer_num_ped_changes = ind
+
+      if (num.gt.hcer_min_peds .and. hcer_min_peds.ne.0) then
+        hcer_ped_mean(pmt)=hcer_new_ped(pmt)
+        hcer_ped_rms(pmt)=hcer_new_rms(pmt)
+      endif
+
+c      hcer_threshold(pmt) = max(4.,3.*hcer_ped_rms(pmt))
 
       return
       end
--- STRACKING/s_calc_pedestal.f
+++ STRACKING/s_calc_pedestal.f
@@ -1,6 +1,9 @@
       subroutine s_calc_pedestal(ABORT,err)
 *
 * $Log$
+* Revision 1.6  1995/10/09 20:12:30  cdaq
+* (JRA) Note pedestals that differ by 2 sigma from parameter file
+*
 * Revision 1.5  1995/08/31 18:04:55  cdaq
 * (JRA) Change threshold limits
 *
@@ -29,7 +32,9 @@
       integer*4 pln,cnt
       integer*4 blk
       integer*4 pmt
+      integer*4 ind
       real*4 sig2
+      real*4 num
 *
       INCLUDE 'sos_data_structures.cmn'
       INCLUDE 'sos_pedestals.cmn'
@@ -39,62 +44,110 @@
 *
 * HODOSCOPE PEDESTALS
 *
+      ind = 0
       do pln = 1 , snum_scin_planes
         do cnt = 1 , snum_scin_elements
-          if (shodo_pos_ped_num(pln,cnt) .ge. shodo_min_peds .and.
-     &        shodo_min_peds .ne. 0) then
-            sscin_all_ped_pos(pln,cnt) = shodo_pos_ped_sum(pln,cnt)/
-     &              float(shodo_pos_ped_num(pln,cnt))
-            sig2 = float(shodo_pos_ped_sum2(pln,cnt))/
-     &              float(shodo_pos_ped_num(pln,cnt))-
-     &              sscin_all_ped_pos(pln,cnt)**2
-!            if (sig2.le.0) write(6,*) 'pos ped(',pln,',',cnt,')**2 =',sig2
-            shodo_all_sig_pos(pln,cnt) = sqrt(max(0.,sig2))
-           endif
-          if (shodo_neg_ped_num(pln,cnt) .ge. shodo_min_peds .and.
-     &        shodo_min_peds .ne. 0) then
-            sscin_all_ped_neg(pln,cnt) = shodo_neg_ped_sum(pln,cnt)/
-     &              float(shodo_neg_ped_num(pln,cnt))
-            sig2 = float(shodo_neg_ped_sum2(pln,cnt))/
-     &              float(shodo_neg_ped_num(pln,cnt))-
-     &              sscin_all_ped_neg(pln,cnt)**2
-!            if (sig2.le.0) write(6,*) 'neg ped(',pln,',',cnt,')**2 =',sig2
-            shodo_all_sig_neg(pln,cnt) = sqrt(max(0.,sig2))
+
+*calculate new pedestal values, positive tubes first.
+          num=max(1.,float(shodo_pos_ped_num(pln,cnt)))
+          shodo_new_ped_pos(pln,cnt) = float(shodo_pos_ped_sum(pln,cnt)) / num
+          sig2 = float(shodo_pos_ped_sum2(pln,cnt))/num -
+     $           shodo_new_ped_pos(pln,cnt)**2
+          shodo_new_sig_pos(pln,cnt) = sqrt(max(0.,sig2))
+
+*note channels with 2 sigma difference from paramter file values.
+          if (abs(sscin_all_ped_pos(pln,cnt)-shodo_new_ped_pos(pln,cnt))
+     &            .ge.(2.*shodo_new_sig_pos(pln,cnt))) then
+            ind = ind + 1     !final value of 'ind' is saved at end of loop
+            shodo_changed_plane(ind)=pln
+            shodo_changed_element(ind)=cnt
+            shodo_changed_sign(ind)= 1       !1=pos,2=neg.
+            shodo_ped_change(ind) = shodo_new_ped_pos(pln,cnt) -
+     &                              sscin_all_ped_pos(pln,cnt)
+          endif   !large pedestal change
+
+*replace old peds (from param file) with calculated pedestals
+          if (num.gt.shodo_min_peds .and. shodo_min_peds.ne.0) then
+            sscin_all_ped_pos(pln,cnt)=shodo_new_ped_pos(pln,cnt)
           endif
-        enddo
-      enddo
+
+*do it all again for negative tubes.
+          num=max(1.,float(shodo_neg_ped_num(pln,cnt)))
+          shodo_new_ped_neg(pln,cnt) = float(shodo_neg_ped_sum(pln,cnt)) / num
+          sig2 = float(shodo_neg_ped_sum2(pln,cnt))/num -
+     $           shodo_new_ped_neg(pln,cnt)**2
+          shodo_new_sig_neg(pln,cnt) = sqrt(max(0.,sig2))
+
+          if (abs(sscin_all_ped_neg(pln,cnt)-shodo_new_ped_neg(pln,cnt))
+     &            .ge.(2.*shodo_new_sig_neg(pln,cnt))) then
+            ind = ind + 1
+            shodo_changed_plane(ind)=pln
+            shodo_changed_element(ind)=cnt
+            shodo_changed_sign(ind)= 2       !1=pos, 2=neg.
+            shodo_ped_change(ind) = shodo_new_ped_neg(pln,cnt) -
+     &                              sscin_all_ped_neg(pln,cnt)
+          endif   !large pedestal change
+
+          if (num.gt.shodo_min_peds .and. shodo_min_peds.ne.0) then
+            sscin_all_ped_neg(pln,cnt)=shodo_new_ped_neg(pln,cnt)
+          endif
+
+        enddo                      !counters
+      enddo                        !planes
+      shodo_num_ped_changes = ind
 *
 *
 * CALORIMETER PEDESTALS
 *
-
+      ind = 0
       do blk = 1 , smax_cal_blocks
-        if (scal_ped_num(blk) .ge. scal_min_peds .and.
-     &      scal_min_peds .ne. 0) then
-          scal_ped_mean(blk) = scal_ped_sum(blk) / float(scal_ped_num(blk))
-          sig2 = float(scal_ped_sum2(blk))/float(scal_ped_num(blk)) -
-     &              scal_ped_mean(blk)**2
-!          if (sig2.le.0) write(6,*) 'cal ped(',blk,')**2 =',sig2
-          scal_ped_rms(blk) = sqrt(max(0.,sig2))
-          scal_threshold(blk) = min(50.,max(10.,3.*scal_ped_rms(blk)))
+        num=max(1.,float(scal_ped_num(blk)))
+        scal_new_ped(blk) = scal_ped_sum(blk) / num
+        sig2 = float(scal_ped_sum2(blk))/num - scal_new_ped(blk)**2
+        scal_new_rms(blk) = sqrt(max(0.,sig2))
+
+        if (abs(scal_ped_mean(blk)-scal_new_ped(blk))
+     &                 .ge.(2.*scal_new_rms(blk))) then
+          ind = ind + 1
+          scal_changed_block(ind)=blk
+          scal_ped_change(ind)=scal_new_ped(blk)-scal_ped_mean(blk)
         endif
+
+        if (num.gt.scal_min_peds .and. scal_min_peds.ne.0) then
+          scal_ped_mean(blk)=scal_new_ped(blk)
+          scal_ped_rms(blk)=scal_new_rms(blk)
+          scal_threshold(blk) = min(50.,max(10.,3.*scal_new_rms(blk)))
+        endif
+
       enddo
+      scal_num_ped_changes = ind
+
+
 *
 *
 * GAS CERENKOV PEDESTALS
 *
+      ind = 0
       do pmt = 1 , smax_cer_hits
-        if (scer_ped_num(pmt) .ge. scer_min_peds .and.
-     &      scer_min_peds .ne. 0) then
-          scer_ped_mean(pmt) = scer_ped_sum(pmt) / float(scer_ped_num(pmt))
-          sig2 = float(scer_ped_sum2(pmt))/
-     &           float(scer_ped_num(pmt))-
-     &           scer_ped_mean(pmt)**2
-!          if (sig2.le.0) write(6,*) 'cer ped(',pmt,')**2 =',sig2
-          scer_ped_rms(pmt) = sqrt(max(0.,sig2))
-          scer_threshold(pmt) = max(4.,3.*scer_ped_rms(pmt))
-        endif
+        num=max(1.,float(scer_ped_num(pmt)))
+        scer_new_ped(pmt) = float(scer_ped_sum(pmt)) / num
+        sig2 = float(scer_ped_sum2(pmt))/ num - scer_new_ped(pmt)**2
+        scer_new_rms(pmt) = sqrt(max(0.,sig2))
       enddo
+      if (abs(scer_ped_mean(pmt)-scer_new_ped(pmt))
+     &              .ge.(2.*scer_new_rms(pmt))) then
+        ind = ind + 1
+        scer_changed_tube(ind)=pmt
+        scer_ped_change(ind)=scer_new_ped(pmt)-scer_ped_mean(pmt)
+      endif
+      scer_num_ped_changes = ind
+
+      if (num.gt.scer_min_peds .and. scer_min_peds.ne.0) then
+        scer_ped_mean(pmt)=scer_new_ped(pmt)
+        scer_ped_rms(pmt)=scer_new_rms(pmt)
+      endif
+
+c      scer_threshold(pmt) = max(4.,3.*scer_ped_rms(pmt))
 
 *
 *
