f969b6523557fe9969e5cf2cc1599dc38837f342
cdaq <>
1995-10-10 13:11:20 +0000

    (JRA) Remove some unneeded validity tests

--- HTRACKING/h_fill_scin_raw_hist.f
+++ HTRACKING/h_fill_scin_raw_hist.f
@@ -1,4 +1,3 @@
-
       subroutine h_fill_scin_raw_hist(Abort,err)
 *
 *     routine to fill histograms with hms_raw_scin varibles
@@ -11,6 +10,9 @@
 *                                Add CTP flag to turn on histogramming
 *                                id's in hms_id_histid
 * $Log$
+* Revision 1.7  1995/10/10 13:11:20  cdaq
+* (JRA) Remove some unneeded validity tests
+*
 * Revision 1.6  1995/07/19 18:16:29  cdaq
 * (JRA) Fill hist's from "all" data structures
 *
@@ -39,11 +41,12 @@
 *
       logical ABORT
       character*(*) err
-      real*4  histval
+      real*4 histval
+      real*4 rcnt
       integer*4 pln,cnt,ihit
       include 'hms_data_structures.cmn'
       include 'hms_scin_parms.cmn'
-      include 'hms_id_histid.cmn'          
+      include 'hms_id_histid.cmn'
 *
       SAVE
 *--------------------------------------------------------
@@ -51,74 +54,68 @@
       ABORT= .FALSE.
       err= ' '
 *
-      histval = HSCIN_ALL_TOT_HITS
+      histval = hscin_all_tot_hits
       call hf1(hidscinrawtothits,histval,1.)
 * Make sure there is at least 1 hit
-      if(HSCIN_ALL_TOT_HITS .gt. 0 ) then
+      if(hscin_all_tot_hits .gt. 0 ) then
 * Loop over all hits
-        do ihit=1,HSCIN_ALL_TOT_HITS
-          pln=HSCIN_ALL_PLANE_NUM(ihit)
-          cnt=HSCIN_ALL_COUNTER_NUM(ihit)
-* Fill plane map                  
-          histval = FLOAT(HSCIN_ALL_PLANE_NUM(ihit))
+        do ihit=1,hscin_all_tot_hits
+          pln=hscin_all_plane_num(ihit)
+          cnt=hscin_all_counter_num(ihit)
+          rcnt=float(cnt)
+* Fill plane map
+          histval = float(pln)
           call hf1(hidscinplane,histval,1.)
-* Check for valid plane
-          if(pln.gt.0 .and. pln .le. hnum_scin_planes) then
 * Fill counter map
-            histval = float(cnt)
-            call hf1(hidscincounters(pln),histval,1.)
+          histval = rcnt
+          call hf1(hidscincounters(pln),histval,1.)
 * Fill ADC and TDC histograms
-            if((cnt .gt. 0) .and.
-     $           (cnt.le.hnum_scin_counters(pln)))then
-               
-              if (HSCIN_ALL_tdc_pos(ihit).ne.-1) then
-                histval = cnt
-                call hf1(hidscinallpostdc(pln),histval,1.)
-              endif
-              if (HSCIN_ALL_tdc_neg(ihit).ne.-1) then
-                histval = cnt
-                call hf1(hidscinallnegtdc(pln),histval,1.)
-              endif
-              if ((HSCIN_ALL_adc_pos(ihit)-hscin_all_ped_pos(pln,cnt))
-     $             .ge. 50) then
-                histval = cnt
-                call hf1(hidscinallposadc(pln),histval,1.)
-              endif
-              if ((HSCIN_ALL_adc_neg(ihit)-hscin_all_ped_neg(pln,cnt))
-     $             .ge. 50) then
-                histval = cnt
-                call hf1(hidscinallnegadc(pln),histval,1.)
-              endif
+          if (hscin_all_tdc_pos(ihit).ne.-1) then
+            histval = rcnt
+            call hf1(hidscinallpostdc(pln),histval,1.)
+          endif
+          if (hscin_all_tdc_neg(ihit).ne.-1) then
+            histval = rcnt
+            call hf1(hidscinallnegtdc(pln),histval,1.)
+          endif
+          if ((hscin_all_adc_pos(ihit)-hscin_all_ped_pos(pln,cnt))
+     $         .ge. 50) then
+            histval = rcnt
+            call hf1(hidscinallposadc(pln),histval,1.)
+          endif
+          if ((hscin_all_adc_neg(ihit)-hscin_all_ped_neg(pln,cnt))
+     $         .ge. 50) then
+            histval = rcnt
+            call hf1(hidscinallnegadc(pln),histval,1.)
+          endif
 
-              if (HSCIN_ALL_tdc_pos(ihit).eq.-1) then
-                histval = HSCIN_ALL_ADC_POS(ihit)-hscin_all_ped_pos(pln,cnt)
-                call hf1(hidsumposadc(pln),histval,1.)
-              endif
-              if (HSCIN_ALL_tdc_neg(ihit).eq.-1) then
-                histval = HSCIN_ALL_ADC_NEG(ihit)-hscin_all_ped_neg(pln,cnt)
-                call hf1(hidsumnegadc(pln),histval,1.)
-              endif
-              histval = FLOAT(HSCIN_ALL_TDC_POS(ihit))
-              call hf1(hidsumpostdc(pln),histval,1.)
-              histval = FLOAT(HSCIN_ALL_TDC_NEG(ihit))
-              call hf1(hidsumnegtdc(pln),histval,1.)
+          if (hscin_all_tdc_pos(ihit).eq.-1) then
+            histval = hscin_all_adc_pos(ihit)-hscin_all_ped_pos(pln,cnt)
+            call hf1(hidsumposadc(pln),histval,1.)
+          endif
+          if (hscin_all_tdc_neg(ihit).eq.-1) then
+            histval = hscin_all_adc_neg(ihit)-hscin_all_ped_neg(pln,cnt)
+            call hf1(hidsumnegadc(pln),histval,1.)
+          endif
+          histval = FLOAT(hscin_all_tdc_pos(ihit))
+          call hf1(hidsumpostdc(pln),histval,1.)
+          histval = FLOAT(hscin_all_tdc_neg(ihit))
+          call hf1(hidsumnegtdc(pln),histval,1.)
 
 * Do we want to histogram raw scintillators
 
-              if(hturnon_scin_raw_hist .ne. 0 ) then
-                histval = HSCIN_ALL_ADC_POS(ihit)-hscin_all_ped_pos(pln,cnt)
-                call hf1(hidscinposadc(pln,cnt),histval,1.)
-                histval = HSCIN_ALL_ADC_NEG(ihit)-hscin_all_ped_neg(pln,cnt)
-                call hf1(hidscinnegadc(pln,cnt),histval,1.)
-                histval = FLOAT(HSCIN_ALL_TDC_POS(ihit))
-                call hf1(hidscinpostdc(pln,cnt),histval,1.)
-                histval = FLOAT(HSCIN_ALL_TDC_NEG(ihit))
-                call hf1(hidscinnegtdc(pln,cnt),histval,1.)
-              endif                     ! end test on histogramming flag
-            endif                       ! end test on valid counter number
-          endif                         ! end test on valid plane number
+          if(hturnon_scin_raw_hist .ne. 0 ) then
+            histval = hscin_all_adc_pos(ihit)-hscin_all_ped_pos(pln,cnt)
+            call hf1(hidscinposadc(pln,cnt),histval,1.)
+            histval = hscin_all_adc_neg(ihit)-hscin_all_ped_neg(pln,cnt)
+            call hf1(hidscinnegadc(pln,cnt),histval,1.)
+            histval = FLOAT(hscin_all_tdc_pos(ihit))
+            call hf1(hidscinpostdc(pln,cnt),histval,1.)
+            histval = FLOAT(hscin_all_tdc_neg(ihit))
+            call hf1(hidscinnegtdc(pln,cnt),histval,1.)
+          endif                         ! end test on histogramming flag
         enddo                           ! end loop over hits
       endif                             ! end test on zero hits       
 
-      RETURN
-      END
+      return
+      end
