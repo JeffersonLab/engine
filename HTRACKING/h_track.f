426d7ec7f4f95f6180dd0473c5405ce4ec3e23a2
cdaq <>
1995-10-11 12:19:50 +0000

    (JRA) Only call tracking routines when it is warranted

--- HTRACKING/h_track.f
+++ HTRACKING/h_track.f
@@ -1,4 +1,4 @@
-       SUBROUTINE H_TRACK(ABORT,err)
+      SUBROUTINE H_TRACK(ABORT,err)
 *--------------------------------------------------------
 *-
 *-   Purpose and Methods :  Finds and fits tracks in HMS focal plane 
@@ -13,6 +13,9 @@
 *- 
 *-   Created 19-JAN-1994   D. F. Geesaman
 * $Log$
+* Revision 1.4  1995/10/11 12:19:50  cdaq
+* (JRA) Only call tracking routines when it is warranted
+*
 * Revision 1.3  1995/05/22 19:39:30  cdaq
 * (SAW) Split gen_data_data_structures into gen, hms, sos, and coin parts"
 *
@@ -23,65 +26,76 @@
 * Initial revision
 *
 *--------------------------------------------------------
-       IMPLICIT NONE
-       SAVE
+      IMPLICIT NONE
+      SAVE
 *
-       character*50 here
-       parameter (here= 'H_TRACK')
+      character*7 here
+      parameter (here= 'H_TRACK')
 *
-       logical ABORT
-       character*(*) err
-       integer*4 ierr
-       character*5  line_err
+      logical ABORT
+      character*(*) err
+      integer*4 ierr
+      character*5  line_err
 *
-       INCLUDE 'hms_data_structures.cmn'
-       INCLUDE 'gen_constants.par'
-       INCLUDE 'gen_units.par'
+      INCLUDE 'hms_data_structures.cmn'
+      INCLUDE 'hms_tracking.cmn'
+      INCLUDE 'gen_constants.par'
+      INCLUDE 'gen_units.par'
 *
 *--------------------------------------------------------
 *
 *
-      call H_PATTERN_RECOGNITION(ABORT,err)
-      if(ABORT) then
-           call G_add_path(here,err)
-           return
-      endif
+      ABORT = .false.
+      err = ' '
+
+      if (hdc_tot_hits.ne.0) then
+        call H_PATTERN_RECOGNITION(ABORT,err)
+        if(ABORT) then
+          call G_add_path(here,err)
+          return
+        endif
      
 *
-      call H_LEFT_RIGHT(ABORT,err)
-      if(ABORT) then
-           call G_add_path(here,err)
-           return
-      endif
+        if (hnspace_points_tot.ne.0) then
+          call H_LEFT_RIGHT(ABORT,err)
+          if(ABORT) then
+            call G_add_path(here,err)
+            return
+          endif
 *
-      call H_LINK_STUBS(ABORT,err)
-      if(ABORT) then
-           call G_add_path(here,err)
-           return
-      endif
+          call H_LINK_STUBS(ABORT,err)
+          if(ABORT) then
+            call G_add_path(here,err)
+            return
+          endif
 *
-      call H_TRACK_FIT(ABORT,err,ierr)
-      if(ABORT) then
-           call G_add_path(here,err)
-           return
-      endif
+          if (hntracks_fp.ne.0) then
+            call H_TRACK_FIT(ABORT,err,ierr)
+            if(ABORT) then
+              call G_add_path(here,err)
+              return
+            endif
 
 *     Check for internal error in H_TRACK_FIT
-      if(ierr.ne.0) then
-           line_err=' '
-           call CSETDI(ierr,line_err,1,5)
-           err='ERROR IN H_TRACK_FIT' // line_err
-           call G_add_path(here,err)
-           call G_LOG_MESSAGE(err)
-      endif                    
+            if(ierr.ne.0) then
+              line_err=' '
+              call CSETDI(ierr,line_err,1,5)
+              err='ERROR IN H_TRACK_FIT' // line_err
+              call G_add_path(here,err)
+              call G_LOG_MESSAGE(err)
+            endif                    
 *     histogram focal plane tracks
 *
-      call h_fill_dc_fp_hist(ABORT,err)
-      if(ABORT) then
-         call g_add_path(here,err)
-         return
-      endif
+            call h_fill_dc_fp_hist(ABORT,err)
+            if(ABORT) then
+              call g_add_path(here,err)
+              return
+            endif
 *
+          endif                         !(hntracks_fp.ne.0)
+        endif                           !(hnspace_points_tot.ne.0)
+      endif                             !(hdc_tot_hits.ne.0)
+
       return
       end
 
