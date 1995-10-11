45045800e455c3ef69aa3c376663269d86288aa9
cdaq <>
1995-10-11 18:15:12 +0000

    (JRA) Comment out MINUIT track fitting for now.

--- STRACKING/s_track_fit.f
+++ STRACKING/s_track_fit.f
@@ -5,6 +5,9 @@
 *
 *     d.f. geesaman         8 Sept 1993
 * $Log$
+* Revision 1.7  1995/10/11 18:15:12  cdaq
+* (JRA) Comment out MINUIT track fitting for now.
+*
 * Revision 1.6  1995/08/31 20:44:56  cdaq
 * (JRA) Don't fill single_residual arrray
 *
@@ -26,107 +29,190 @@
       implicit none
       include "sos_data_structures.cmn"
       include "sos_tracking.cmn"
+      include "sos_geometry.cmn"
 *
-      external S_FCNCHISQ
-      external S_DPSIFUN
-      real*8 S_DPSIFUN
-      real*8 S_FCNCHISQ
+c      external S_FCNCHISQ
+c      external S_DPSIFUN
+c      real*8 S_DPSIFUN
+c      real*8 S_FCNCHISQ
 *
 *     local variables
 *
       logical ABORT
-      character*50 here
+      character*11 here
       parameter (here='S_TRACK_FIT')
       character*(*) err
-      integer*4 itrack                        ! track loop index
-      integer*4 i                             ! loop index
-      integer*4 ierr                          ! error return flag
-      integer*4 ivarbl                  ! dummy MINUIT variable
-      integer*4 ihit, plane, hit
-      real*8 pos
-      real*8 ray(snum_fpray_param)
-      real*8 ray1(4)
-      real*8 error(snum_fpray_param)
-      real*8 initialray(snum_fpray_param)
-      real*8 initialsteps(snum_fpray_param)
-      real*8 zero
-      real*8 arglis(10)
-      real*8 bnd1,bnd2                     ! unused MUNUIT output variables
+      integer*4 itrk                    ! track loop index
+      integer*4 i,j                     ! loop index
+      integer*4 ierr                    ! error return flag
+c      integer*4 ivarbl                  ! dummy MINUIT variable
+      integer*4 ihit, pln, hit
+c      real*8 pos
+      real*8 dray(snum_fpray_param)
+      real*8 TT(snum_fpray_param)
+      real*8 AA(snum_fpray_param,snum_fpray_param)
+c      real*8 error(snum_fpray_param)
+c      real*8 initialray(snum_fpray_param)
+c      real*8 initialsteps(snum_fpray_param)
+c      real*8 zero
+c      real*8 arglis(10)
+c      real*8 bnd1,bnd2                     ! unused MUNUIT output variables
       real*8 chi2
-      real*8 fedm,errder                   ! unused MUNUIT output variables
-      integer*4 npari,nparz,istat          ! unused MUNUIT output variables
-      character*10 fitnames(4)
+c      real*8 fedm,errder                   ! unused MUNUIT output variables
+c      integer*4 npari,nparz,istat          ! unused MUNUIT output variables
+c      character*10 fitnames(4)
 
-      data initialray/0.D0,0.D0,.5D-2,.5D-2/     ! starting ray values
-      data initialsteps/1.D0,1.D0,.5E-2,.5D-2/
-      data zero/0.0D0/
-      data fitnames/' x_t ',' y_t ','tan(xp)','tan(yp)'/
+c      data initialray/0.D0,0.D0,.5D-2,.5D-2/     ! starting ray values
+c      data initialsteps/1.D0,1.D0,.5E-2,.5D-2/
+c      data zero/0.0D0/
+c      data fitnames/' x_t ',' y_t ','tan(xp)','tan(yp)'/
 
-      save initialray,initialsteps,fitnames   ! starting ray, steps, names
+c      save initialray,initialsteps,fitnames   ! starting ray, steps, names
+      integer*4 remap(snum_fpray_param)
+      data remap/5,6,3,4/
+      save remap
 *
       ABORT= .FALSE.
       ierr=0
 *  initailize residuals
 
-      do plane=1,SDC_NUM_PLANES
-        do itrack=1,SNTRACKS_MAX
-          sdc_double_residual(itrack,plane)=1000
-          sdc_single_residual(itrack,plane)=1000
+      do pln=1,sdc_num_planes
+        do itrk=1,sntracks_fp
+          sdc_double_residual(itrk,pln)=1000
+          sdc_single_residual(itrk,pln)=1000
         enddo
-c        sdc_sing_res(plane)=1000
-        sdc_dbl_res(plane)=1000
+c fill the 1d arrays from the 2d arrays in h_physics (don't clear here).
+c        sdc_sing_res(pln)=1000
+        sdc_dbl_res(pln)=1000
       enddo
 
 *     test for no tracks
-      if(SNTRACKS_FP.ge.1) then
-        do itrack=1,SNTRACKS_FP
-            strack_fit_num=itrack
-*     are there enough degrees of freedom
-          SNFREE_FP(itrack)=SNTRACK_HITS(itrack,1)-snum_fpray_param
-          if(SNFREE_FP(itrack).gt.0) then
-*     initialize parameters
-           do i=1,snum_fpray_param
-            call MNPARM(i,fitnames(i),initialray(i),initialsteps(i),
-     &                  zero,zero,ierr)  
-            if(ierr.ne.0) then
-              write(sluno,'(a,i,a)') ' Unable to define parameter no.',i,
-     &                           fitnames(i)
-              ierr=1
-              go to 1000                     ! error return
-            endif
-           enddo                              ! end loop over track param
-           do i=1,10
-            arglis(i)=0.
-           enddo
-*     Do track fit on track number strack_fit_num (passes in sos_tracking)
-           Call MNSETI(' Track Fitting in SOS Spectrometer')
-           Call MNEXCM(S_FCNCHISQ,'MIGRAD',arglis,0,ierr)
-           Call MNEXCM(S_FCNCHISQ,'MINOS',arglis,0,ierr)
-           Call MNSTAT(chi2,fedm,errder,npari,nparz,istat)
-           do i=1,snum_fpray_param
-             call MNPOUT(i,fitnames(i),ray(i),error(i),bnd1,bnd2,ivarbl)
-           enddo     
-           SX_FP(itrack)=real(ray(1))
-           SY_FP(itrack)=real(ray(2))
-           SZ_FP(itrack)=0.                   ! z=0 of tracking.
-           SXP_FP(itrack)=real(ray(3))
-           SYP_FP(itrack)=real(ray(4))
-           SCHI2_FP(itrack)=real(chi2)
+      if(sntracks_fp.ge.1) then
+        do itrk=1,sntracks_fp
+            strack_fit_num=itrk
 
-* calculate residuals
-           ray1(1)=dble(SX_FP(itrack))
-           ray1(2)=dble(SY_FP(itrack))
-           ray1(3)=dble(SXP_FP(itrack))
-           ray1(4)=dble(SYP_FP(itrack))
-           do ihit=2,SNTRACK_HITS(itrack,1)+1
-             hit=SNTRACK_HITS(itrack,ihit)
-             plane=SDC_PLANE_NUM(hit)
-             pos=S_DPSIFUN(ray1,plane)
-             sdc_single_residual(itrack,plane)=SDC_WIRE_COORD(hit)-pos
-c             sdc_sing_res(plane)=sdc_single_residual(itrack,plane)
-           enddo
-         endif                          ! end test on degrees of freedom
-       enddo                            ! end loop over tracks
+*     are there enough degrees of freedom
+          snfree_fp(itrk)=sntrack_hits(itrk,1)-snum_fpray_param
+          if(snfree_fp(itrk).gt.0) then
+
+c*     initialize parameters
+c           do i=1,snum_fpray_param
+c            call MNPARM(i,fitnames(i),initialray(i),initialsteps(i),
+c     &                  zero,zero,ierr)  
+c            if(ierr.ne.0) then
+c              write(sluno,'(a,i,a)') ' Unable to define parameter no.',i,
+c     &                           fitnames(i)
+c              ierr=1
+c              go to 1000                     ! error return
+c            endif
+c           enddo                              ! end loop over track param
+c           do i=1,10
+c            arglis(i)=0.
+c           enddo
+c*     Do track fit on track number strack_fit_num (passes in sos_tracking)
+c           Call MNSETI(' Track Fitting in SOS Spectrometer')
+c           Call MNEXCM(S_FCNCHISQ,'MIGRAD',arglis,0,ierr)
+c           Call MNEXCM(S_FCNCHISQ,'MINOS',arglis,0,ierr)
+c           Call MNSTAT(chi2,fedm,errder,npari,nparz,istat)
+c           do i=1,snum_fpray_param
+c             call MNPOUT(i,fitnames(i),ray(i),error(i),bnd1,bnd2,ivarbl)
+c           enddo     
+c           sx_fp(itrk)=real(ray(1))
+c           sy_fp(itrk)=real(ray(2))
+c           sz_fp(itrk)=0.                   ! z=0 of tracking.
+c           sxp_fp(itrk)=real(ray(3))
+c           syp_fp(itrk)=real(ray(4))
+c           schi2_fp(itrk)=real(chi2)
+c
+c* calculate residuals
+c           ray1(1)=dble(sx_fp(itrk))
+c           ray1(2)=dble(sy_fp(itrk))
+c           ray1(3)=dble(sxp_fp(itrk))
+c           ray1(4)=dble(syp_fp(itrk))
+c           do ihit=2,sntrack_hits(itrk,1)+1
+c             hit=sntrack_hits(itrk,ihit)
+c             pln=sdc_plane_num(hit)
+c             pos=s_dpsifun(ray1,pln)
+c             sdc_single_residual(itrk,pln)=sdc_wire_coord(hit)-pos
+c           enddo
+c         endif                          ! end test on degrees of freedom
+c       enddo                            ! end loop over tracks
+c      endif
+
+*     initialize parameters
+            do i=1,snum_fpray_param
+              TT(i)=0.
+              do ihit=2,sntrack_hits(itrk,1)+1
+                hit=sntrack_hits(itrk,ihit)
+                pln=sdc_plane_num(hit)
+                TT(i)=TT(i)+((sdc_wire_coord(hit)*
+     &               splane_coeff(remap(i),pln))
+     &               /(sdc_sigma(pln)*sdc_sigma(pln)))
+              enddo
+            enddo
+            do i=1,snum_fpray_param
+              do j=1,snum_fpray_param
+                AA(i,j)=0.
+                if(j.lt.i)then
+                  AA(i,j)=AA(j,i)
+                else
+                  do ihit=2,sntrack_hits(itrk,1)+1
+                    hit=sntrack_hits(itrk,ihit)
+                    pln=sdc_plane_num(hit)
+                    AA(i,j)=AA(i,j) + (
+     &                   splane_coeff(remap(i),pln)*splane_coeff(remap(j)
+     $                   ,pln)/(sdc_sigma(pln)*sdc_sigma(pln)))
+                  enddo                 ! end loop on ihit
+                endif                   ! end test on j .lt. i
+              enddo                     ! end loop on j
+            enddo                       ! end loop on i
+*
+*     solve four by four equations
+            call solve_four_by_four(TT,AA,dray,ierr)
+*
+            if(ierr.ne.0) then
+              dray(1)=10000.
+              dray(2)=10000.
+              dray(3)=2.
+              dray(4)=2.
+            else
+*     calculate chi2
+              chi2=0.
+
+*              ray(1)=dray(1)
+*              ray(2)=dray(2)
+*              ray(3)=dray(3)
+*              ray(4)=dray(4)
+
+* calculate hit coord at each plane for chisquared and efficiency calculations.
+              do pln=1,sdc_num_planes
+                sdc_track_coord(itrk,pln)=splane_coeff(remap(1),pln)*dray(1)
+     &               +splane_coeff(remap(2),pln)*dray(2)
+     &               +splane_coeff(remap(3),pln)*dray(3)
+     &               +splane_coeff(remap(4),pln)*dray(4)
+              enddo
+
+              do ihit=2,sntrack_hits(itrk,1)+1
+                hit=sntrack_hits(itrk,ihit)
+                pln=sdc_plane_num(hit)
+
+* note chi2 is single precision
+
+                sdc_single_residual(itrk,pln)=
+     &              sdc_wire_coord(hit)-sdc_track_coord(itrk,pln)
+                chi2=chi2+
+     &              (sdc_single_residual(itrk,pln)/sdc_sigma(pln))**2
+              enddo
+            endif
+
+            sx_fp(itrk)=dray(1)
+            sy_fp(itrk)=dray(2)
+            sz_fp(itrk)=0.            ! z=0 of tracking.
+            sxp_fp(itrk)=dray(3)
+            syp_fp(itrk)=dray(4)
+          endif                         ! end test on degrees of freedom
+          schi2_fp(itrk)=chi2
+        enddo                           ! end loop over tracks
       endif
 
 *     test if we want to dump out trackfit results
