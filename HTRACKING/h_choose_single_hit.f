91b3e86bf81bb8594a8e711a1b513ddb54a5a320
cdaq <>
1996-01-16 21:45:35 +0000

    (JRA) Misc changes

--- HTRACKING/h_choose_single_hit.f
+++ HTRACKING/h_choose_single_hit.f
@@ -12,6 +12,9 @@
 *- 
 *-   Created 28-JUN-1994   D. F. Geesaman
 * $Log$
+* Revision 1.4  1996/01/16 21:45:35  cdaq
+* (JRA) Misc changes
+*
 * Revision 1.3  1995/05/22 19:39:07  cdaq
 * (SAW) Split gen_data_data_structures into gen, hms, sos, and coin parts"
 *
@@ -54,45 +57,43 @@
 *
 *
 *     loop over all space points
-      if(nspace_points .gt. 0) then
-        do point =1,nspace_points
-          startnum = space_point_hits(point,1)
-          finalnum=0
+      do point =1,nspace_points
+        startnum = space_point_hits(point,1)
+        finalnum=0
           
-          do j=3,startnum+2
-            goodhit(j) = 1
-          enddo
+        do j=3,startnum+2
+          goodhit(j) = 1
+        enddo
           
-          do j=3,startnum+1
-            hit1 = space_point_hits(point,j)
-            plane1 = HDC_PLANE_NUM(hit1)
-            drifttime1 = HDC_DRIFT_TIME(hit1)
-            do k=j+1,startnum+2
-              hit2 = space_point_hits(point,k)
-              plane2 = HDC_PLANE_NUM(hit2)
-              drifttime2 = HDC_DRIFT_TIME(hit2)
-              if(plane1 .eq. plane2 ) then
-                if(drifttime1.gt.drifttime2) then
-                  goodhit(j) = 0
-                else                    !if equal times, choose 1st hit(arbitrary)
-                  goodhit(k) = 0
-                endif
-              endif                     ! end test on equal planes
-            enddo                       ! end loop on k
-          enddo                         ! end loop on j
-          do j=3,startnum+2
-            if(goodhit(j).gt.0) then
-              finalnum = finalnum + 1
-              hits(finalnum)=space_point_hits(point,j)
-            endif                       ! end check on good hit
-          enddo
+        do j=3,startnum+1
+          hit1 = space_point_hits(point,j)
+          plane1 = hdc_plane_num(hit1)
+          drifttime1 = hdc_drift_time(hit1)
+          do k=j+1,startnum+2
+            hit2 = space_point_hits(point,k)
+            plane2 = hdc_plane_num(hit2)
+            drifttime2 = hdc_drift_time(hit2)
+            if(plane1 .eq. plane2 ) then
+              if(drifttime1.gt.drifttime2) then
+                goodhit(j) = 0
+              else                      !if equal times, choose 1st hit(arbitrary)
+                goodhit(k) = 0
+              endif
+            endif                       ! end test on equal planes
+          enddo                         ! end loop on k
+        enddo                           ! end loop on j
+        do j=3,startnum+2
+          if(goodhit(j).gt.0) then
+            finalnum = finalnum + 1
+            hits(finalnum)=space_point_hits(point,j)
+          endif                         ! end check on good hit
+        enddo
 *     copy good hits to space_point_hits
-          space_point_hits(point,1) = finalnum
-          do j = 1, finalnum
-            space_point_hits(point,j+2) = hits(j)
-          enddo                         ! end of copy 
-        enddo                           ! end loop on space points
-      endif                             ! end test on no space points      
+        space_point_hits(point,1) = finalnum
+        do j = 1, finalnum
+          space_point_hits(point,j+2) = hits(j)
+        enddo                           ! end of copy 
+      enddo                             ! end loop on space points
 *     
       return
       end
