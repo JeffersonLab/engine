220c74cf08c50712354ca877179c5f30e4611171
cdaq <>
1996-01-16 18:39:17 +0000

    (CB,SAW) Add current monitor calculations.  Make compatible with SAW's new          scaler header format.

--- ENGINE/g_analyze_scalers.f
+++ ENGINE/g_analyze_scalers.f
@@ -1,6 +1,10 @@
       subroutine g_analyze_scalers(event,ABORT,err)
 *
 * $Log$
+* Revision 1.8  1996/01/16 18:39:17  cdaq
+* (CB,SAW) Add current monitor calculations.  Make compatible with SAW's new
+*          scaler header format.
+*
 * Revision 1.7  1995/10/09 17:55:32  cdaq
 * (JRA) Add arrays for previous scalers and differences from previous scalers
 *
@@ -37,7 +41,8 @@ c
 *
       integer nroll(max_num_scalers)
       integer index
-      real*4 realscal
+      real*8 realscal
+      logical hmscrate
 *
       integer*4 jiand, jishft           ! Declare to help f2c
 *
@@ -55,7 +60,13 @@ c
 
       integer evtype, evlen, pointer
       integer scalid, countinmod, address, counter
-      
+*
+*     Temporary variables for beam current and charge calculations
+*
+      real*8 ave_current_bcm1, ave_current_bcm2, ave_current_bcm3
+      real*8 delta_time
+*
+      hmscrate=.false.
 *
       evtype = jishft(event(2),-16)
 *
@@ -69,20 +80,41 @@ c
 *
           scalid = jiand(jishft(event(pointer),-16),'FF'x)
           countinmod = jiand(event(pointer),'FFFF'x)
+          if(jiand(event(pointer),'FF000000'x).eq.'DA000000'x) then
+c     Old style header with scaler ID @ 00FF0000
+            scalid = jiand(jishft(event(pointer),-16),'FF'x)
+            address = scalid*16
 *
 *     Might want to check that count is not to big.
 *
-          if(countinmod.ne.16) then
-            err = 'Scaler module header word has count<>16'
-            ABORT = .true.
-            call g_add_path(here,err)
-            return                      ! Safest action
+            if(countinmod.ne.16) then
+              err = 'Scaler module header word has count<>16'
+              ABORT = .true.
+              call g_add_path(here,err)
+              return                    ! Safest action
+            endif
+          else
+c
+c     New style header with scaler ID @ FFF?0000
+c     (If ? is non zero, it means we are starting in the middle of a scaler)
+c     Allows for non multiple of 16 address starts
+c
+            address = jishft(event(pointer),-16)
+*
+*     Might want to check that count is not to big.
+*
+            if(countinmod.gt.16) then
+              err = 'Scaler module header word has count >16'
+              ABORT = .true.
+              call g_add_path(here,err)
+              return                    ! Safest action
+            endif
           endif
 *
           address = scalid*16
           do counter = 1,countinmod
             index=address+counter
-            realscal=float(event(pointer+counter))
+            realscal=dfloat(event(pointer+counter))
 
 * Save scaler value from previous scaler event:
             prev_scalers(index) = scalers(index)
@@ -90,7 +122,7 @@ c
             if (realscal.lt.-0.5) then
               realscal=realscal+4294967296.
             endif
-            if ( (realscal+float(nroll(index))*4294967296.) .ge.
+            if ( (realscal+dfloat(nroll(index))*4294967296.) .ge.
      &           scalers(index) ) then  ! 2**32 = 4.295e+9
               scalers(index) = realscal + nroll(index)*4294967296.
             else                        !32 bit scaler rolled over.
@@ -109,10 +141,29 @@ c
         return
       endif
 
+* check last scaler to see which crate was read out.
+      if (index.le.208) hmscrate=.true.
 
-! calculate time of run (must not be zero to avoid div. by zero).
+* calculate time of run (must not be zero to avoid div. by zero).
       g_time = max(0.001,scalers(g_clock_index)/g_clock_rate)
+
+* Calculate beam current and charge between scaler events
+
+      if (hmscrate) then        ! time and bcms are in hms crate
+        delta_time = max(delta_scalers(g_clock_index)/g_clock_rate,.0001)
+
+        ave_current_bcm1 = g_bcm1_gain*sqrt(max(0,
+     &        (delta_scalers(g_bcm1_index)/delta_time)-g_bcm1_offset))
+        ave_current_bcm2 = g_bcm2_gain*sqrt(max(0,
+     &        (delta_scalers(g_bcm2_index)/delta_time)-g_bcm2_offset))
+        ave_current_bcm3 = g_bcm3_gain*((delta_scalers(g_bcm3_index)
+     &        /delta_time) - g_bcm3_offset)
+
+        g_bcm1_charge = g_bcm1_charge + ave_current_bcm1*delta_time
+        g_bcm2_charge = g_bcm2_charge + ave_current_bcm2*delta_time
+        g_bcm3_charge = g_bcm3_charge + ave_current_bcm3*delta_time
+
+      endif
 
       return
       end
-
