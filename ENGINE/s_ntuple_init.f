4062d7436321c4102f5b175452711d8536551021
cdaq <>
1996-01-16 16:41:14 +0000

    (JRA) Modify ntuple contents

--- ENGINE/s_ntuple_init.f
+++ ENGINE/s_ntuple_init.f
@@ -10,6 +10,9 @@
 *
 *     Created: 8-Apr-1994  K.B.Beard, Hampton Univ.
 * $Log$
+* Revision 1.6  1996/01/16 16:41:14  cdaq
+* (JRA) Modify ntuple contents
+*
 * Revision 1.5  1995/09/01 13:38:59  cdaq
 * (JRA) Add Cerenkov photoelectron count to ntuple
 *
@@ -127,7 +130,7 @@
       m= m+1
       s_Ntuple_tag(m)= 'SCER_NPE' ! cerenkov photoelectron spectrum
       m= m+1
-      s_Ntuple_tag(m)= 'SSP'	! Lab momentum of chosen track in GeV/c
+      s_Ntuple_tag(m)= 'p'	! Lab momentum of chosen track in GeV/c
       m= m+1
       s_Ntuple_tag(m)= 'SSENERGY'! Lab total energy of chosen track in GeV
       m= m+1
@@ -142,51 +145,164 @@
       s_Ntuple_tag(m)= 'SSZBEAM'! Lab Z coordinate of intersection of beam
                                 ! track with spectrometer ray
       m= m+1
-      s_Ntuple_tag(m)= 'SSDEDX1'	! DEDX of chosen track in 1st scin plane
+      s_Ntuple_tag(m)= 'dedx1'  	! DEDX of chosen track in 1st scin plane
+c      m= m+1
+c      s_Ntuple_tag(m)= 'SSDEDX2'	! DEDX of chosen track in 2nd scin plane
+c      m= m+1
+c      s_Ntuple_tag(m)= 'SSDEDX3'	! DEDX of chosen track in 3rd scin plane
+c      m= m+1
+c      s_Ntuple_tag(m)= 'SSDEDX4'	! DEDX of chosen track in 4th scin plane
       m= m+1
-      s_Ntuple_tag(m)= 'SSDEDX2'	! DEDX of chosen track in 2nd scin plane
+      s_Ntuple_tag(m)= 'beta'		! BETA of chosen track
       m= m+1
-      s_Ntuple_tag(m)= 'SSDEDX3'	! DEDX of chosen track in 3rd scin plane
+      s_Ntuple_tag(m)= 'shtrk' ! 'SSTRACK_ET'	! Total shower energy of chosen track
       m= m+1
-      s_Ntuple_tag(m)= 'SSDEDX4'	! DEDX of chosen track in 4th scin plane
+      s_Ntuple_tag(m)= 'prtrk'!'SSTRACK_PRESHOWER_E' ! preshower of chosen track
       m= m+1
-      s_Ntuple_tag(m)= 'SSBETA'	! BETA of chosen track
+      s_Ntuple_tag(m)= 'fptime'
       m= m+1
-      s_Ntuple_tag(m)= 'SStrk_ET'! 'SSTRACK_ET'	! Total shower energy of chosen track
+      s_Ntuple_tag(m)= 'xfp'		! X focal plane position 
       m= m+1
-      s_Ntuple_tag(m)= 'SStrk_PRESHOWER_E'!'SSTRACK_PRESHOWER_E' ! preshower of chosen track
+      s_Ntuple_tag(m)= 'yfp'
       m= m+1
-      s_Ntuple_tag(m)= 'SSTIME_AT_FP'
+      s_Ntuple_tag(m)= 'xpfp'
       m= m+1
-      s_Ntuple_tag(m)= 'SSX_FP'		! X focal plane position 
+      s_Ntuple_tag(m)= 'ypfp'
       m= m+1
-      s_Ntuple_tag(m)= 'SSY_FP'
+      s_Ntuple_tag(m)= 'chi2'		! CHI2 per degree of freedom of chosen track.
       m= m+1
-      s_Ntuple_tag(m)= 'SSXP_FP'
+      s_Ntuple_tag(m)= 'ndof'
       m= m+1
-      s_Ntuple_tag(m)= 'SSYP_FP'
+      s_Ntuple_tag(m)= 'ytar'
       m= m+1
-      s_Ntuple_tag(m)= 'SSCHI2PERDEG'	! CHI2 per degree of freedom of chosen track.
+      s_Ntuple_tag(m)= 'xptar'
       m= m+1
-      s_Ntuple_tag(m)= 'SSX_TAR'
-      m= m+1
-      s_Ntuple_tag(m)= 'SSY_TAR'
-      m= m+1
-      s_Ntuple_tag(m)= 'SSXP_TAR'
-      m= m+1
-      s_Ntuple_tag(m)= 'SSYP_TAR'
+      s_Ntuple_tag(m)= 'yptar'
 *
-      m= m+1
-      s_Ntuple_tag(m)= 'SSNUM_FPTRACK'	! Index of focal plane track chosen
-      m= m+1
-      s_Ntuple_tag(m)= 'SSNUM_TARTRACK'	! Index of target track chosen
-      m= m+1
-      s_Ntuple_tag(m)= 'SSID_LUND'	! LUND particle ID code -- not yet filled
-      m= m+1
-      s_Ntuple_tag(m)= 'SSNFREE_FP'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'trknum'		! Index of focal plane track chosen
+c      m= m+1
+c      s_Ntuple_tag(m)= 'SSNUM_TARTRACK'	! Index of target track chosen
+c      m= m+1
+c      s_Ntuple_tag(m)= 'SSID_LUND'	! LUND particle ID code -- not yet filled
+c      m= m+1
+c      s_Ntuple_tag(m)= 'SSNFREE_FP'
 *
       m= m+1
       s_Ntuple_tag(m)= 'eventID'
+*
+      m= m+1
+      s_Ntuple_tag(m)= 'scintothits'
+      m= m+1
+      s_Ntuple_tag(m)= 'scinhits'
+      m= m+1
+      s_Ntuple_tag(m)= 'starttime'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'fptim1'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'fptim2'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'fptim3'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'fptim4'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'scinhit1'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'scinhit2'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'scinhit3'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'scinhit4'
+      m= m+1
+      s_Ntuple_tag(m)= 'sdc_raw_hits'
+      m= m+1
+      s_Ntuple_tag(m)= 'sdc_hits'
+*
+c      m= m+1
+c      s_Ntuple_tag(m)= 'sx_sp1'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'sy_sp1'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'sxp_sp1'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'sx_sp2'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'sy_sp2'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'sxp_sp2'
+*
+c      m= m+1
+c      s_Ntuple_tag(m)= 'res1'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'res2'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'res3'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'res4'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'res5'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'res6'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'res7'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'res8'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'res9'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'res10'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'res11'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'res12'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'tim1'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'tim2'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'tim3'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'tim4'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'tim5'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'tim6'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'tim7'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'tim8'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'tim9'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'tim10'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'tim11'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'tim12'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'dist1'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'dist2'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'dist3'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'dist4'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'dist5'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'dist6'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'dist7'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'dist8'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'dist9'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'dist10'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'dist11'
+c      m= m+1
+c      s_Ntuple_tag(m)= 'dist12'
 *
       s_Ntuple_size= m     !total size
 ***********end insert description of contents of SOS tuple********
@@ -223,10 +339,10 @@
       IF(ABORT) THEN
         err= ':unable to create '//msg
         call G_add_path(here,err)
-      ELSE
-        pat= ':created '//msg
-        call G_add_path(here,pat)
-        call G_log_message('INFO: '//pat)
+c      ELSE
+c        pat= ':created '//msg
+c        call G_add_path(here,pat)
+c        call G_log_message('INFO: '//pat)
       ENDIF
 *
       RETURN
--- ENGINE/s_ntuple_keep.f
+++ ENGINE/s_ntuple_keep.f
@@ -8,6 +8,9 @@
 *
 *     Created: 11-Apr-1994  K.B.Beard, Hampton U.
 * $Log$
+* Revision 1.6  1996/01/16 16:40:31  cdaq
+* (JRA) Modify ntuple contents
+*
 * Revision 1.5  1995/09/01 13:38:46  cdaq
 * (JRA) Add Cerenkov photoelectron count to ntuple
 *
@@ -37,6 +40,10 @@
       INCLUDE 's_ntuple.cmn'
       INCLUDE 'sos_data_structures.cmn'
       INCLUDE 'gen_event_info.cmn'
+      INCLUDE 'sos_tracking.cmn'
+      INCLUDE 'sos_physics_sing.cmn'
+      INCLUDE 'sos_scin_tof.cmn'
+      include "sos_track_histid.cmn"
 *
       logical HEXIST    !CERNLIB function
 *
@@ -70,12 +77,12 @@
                                 ! track with spectrometer ray
       m= m+1
       s_Ntuple_contents(m)= SSDEDX(1)	! DEDX of chosen track in 1st scin plane
-      m= m+1
-      s_Ntuple_contents(m)= SSDEDX(2)	! DEDX of chosen track in 2nd scin plane
-      m= m+1
-      s_Ntuple_contents(m)= SSDEDX(3)	! DEDX of chosen track in 3rd scin plane
-      m= m+1
-      s_Ntuple_contents(m)= SSDEDX(4)	! DEDX of chosen track in 4th scin plane
+c      m= m+1
+c      s_Ntuple_contents(m)= SSDEDX(2)	! DEDX of chosen track in 2nd scin plane
+c      m= m+1
+c      s_Ntuple_contents(m)= SSDEDX(3)	! DEDX of chosen track in 3rd scin plane
+c      m= m+1
+c      s_Ntuple_contents(m)= SSDEDX(4)	! DEDX of chosen track in 4th scin plane
       m= m+1
       s_Ntuple_contents(m)= SSBETA	! BETA of chosen track
       m= m+1
@@ -95,7 +102,7 @@
       m= m+1
       s_Ntuple_contents(m)= SSCHI2PERDEG	! CHI2 per degree of freedom of chosen track.
       m= m+1
-      s_Ntuple_contents(m)= SSX_TAR
+      s_Ntuple_contents(m)= float(SSNFREE_FP)
       m= m+1
       s_Ntuple_contents(m)= SSY_TAR
       m= m+1
@@ -103,17 +110,130 @@
       m= m+1
       s_Ntuple_contents(m)= SSYP_TAR
 *
-      m= m+1
-      s_Ntuple_contents(m)= float(SSNUM_FPTRACK)! Index of focal plane track chosen
-      m= m+1
-      s_Ntuple_contents(m)= float(SSNUM_TARTRACK)! Index of target track chosen
-      m= m+1
-      s_Ntuple_contents(m)= float(SSID_LUND)! LUND particle ID code -- not yet filled
-      m= m+1
-      s_Ntuple_contents(m)= float(SSNFREE_FP)
+c      m= m+1
+c      s_Ntuple_contents(m)= float(SSNUM_FPTRACK)! Index of focal plane track chosen
+c      m= m+1
+c      s_Ntuple_contents(m)= float(SSNUM_TARTRACK)! Index of target track chosen
+c      m= m+1
+c      s_Ntuple_contents(m)= float(SSID_LUND)! LUND particle ID code -- not yet filled
+c      m= m+1
+c      s_Ntuple_contents(m)= float(SSNFREE_FP)
 *
       m= m+1
       s_Ntuple_contents(m)= float(gen_event_ID_number)
+*
+      m= m+1
+      s_Ntuple_contents(m)= sscin_tot_hits
+      m= m+1
+      s_Ntuple_contents(m)= ssnum_scin_hit
+      m= m+1
+      s_Ntuple_contents(m)= sstart_time
+c      m= m+1
+c      s_Ntuple_contents(m)= s_fptime(1)
+c      m= m+1
+c      s_Ntuple_contents(m)= s_fptime(2)
+c      m= m+1
+c      s_Ntuple_contents(m)= s_fptime(3)
+c      m= m+1
+c      s_Ntuple_contents(m)= s_fptime(4)
+c      m= m+1
+c      s_Ntuple_contents(m)= ssscin_elem_hit(1)
+c      m= m+1
+c      s_Ntuple_contents(m)= ssscin_elem_hit(2)
+c      m= m+1
+c      s_Ntuple_contents(m)= ssscin_elem_hit(3)
+c      m= m+1
+c      s_Ntuple_contents(m)= ssscin_elem_hit(4)
+      m= m+1
+      s_Ntuple_contents(m)= sdc_raw_tot_hits
+      m= m+1
+      s_Ntuple_contents(m)= sdc_tot_hits
+*
+c      m= m+1
+c      s_Ntuple_contents(m)= ssx_sp1
+c      m= m+1
+c      s_Ntuple_contents(m)= ssy_sp1
+c      m= m+1
+c      s_Ntuple_contents(m)= ssxp_sp1
+c      m= m+1
+c      s_Ntuple_contents(m)= ssx_sp2
+c      m= m+1
+c      s_Ntuple_contents(m)= ssy_sp2
+c      m= m+1
+c      s_Ntuple_contents(m)= ssxp_sp2
+*
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_res(1)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_res(2)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_res(3)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_res(4)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_res(5)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_res(6)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_res(7)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_res(8)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_res(9)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_res(10)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_res(11)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_res(12)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_drifttime(1)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_drifttime(2)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_drifttime(3)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_drifttime(4)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_drifttime(5)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_drifttime(6)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_drifttime(7)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_drifttime(8)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_drifttime(9)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_drifttime(10)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_drifttime(11)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_drifttime(12)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_driftdis(1)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_driftdis(2)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_driftdis(3)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_driftdis(4)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_driftdis(5)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_driftdis(6)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_driftdis(7)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_driftdis(8)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_driftdis(9)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_driftdis(10)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_driftdis(11)
+c      m= m+1
+c      s_Ntuple_contents(m)= sdc_sing_driftdis(12)
 *
 ********************************************************
 *
