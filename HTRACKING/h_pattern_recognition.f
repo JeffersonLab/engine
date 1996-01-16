ba4a462555fde9fbf5c7787ce28e402e3c6d9173
cdaq <>
1996-01-16 21:53:24 +0000

    (JRA) Add code for easy space points

--- HTRACKING/h_pattern_recognition.f
+++ HTRACKING/h_pattern_recognition.f
@@ -14,6 +14,9 @@
 *-   Created 30-AUG-1993   D. F. Geesaman
 *-   Modified 19-JAN-1994  DFG    Include standard error form
 * $Log$
+* Revision 1.11  1996/01/16 21:53:24  cdaq
+* (JRA) Add code for easy space points
+*
 * Revision 1.10  1995/08/31 14:49:32  cdaq
 * (JRA) Fix typo
 *
@@ -55,7 +58,7 @@
       IMPLICIT NONE
       SAVE
 *
-      character*50 here
+      character*21 here
       parameter (here= 'H_PATTERN_RECOGNITION')
 *
       logical ABORT
@@ -64,143 +67,142 @@
       INCLUDE 'hms_data_structures.cmn'
       INCLUDE 'gen_constants.par'
       INCLUDE 'gen_units.par'
+      INCLUDE 'hms_tracking.cmn'
+      INCLUDE 'hms_geometry.cmn'
 *
-*
-      include "hms_tracking.cmn"
-      include "hms_geometry.cmn"
 *     local variables
       integer*4 hit_number(hmax_chamber_hits)
-      real*4 space_points(hmax_space_points,2)
       integer*4 space_point_hits(hmax_space_points,hmax_hits_per_point+2)
-      integer*4 i,j,k
+      integer*4 pln, isp, ihit, hit
+      integer*4 i,j,k,yy,yyprime
+      integer*4 yplane,yprimeplane
+      integer*4 ich, ip
+      logical easy_space_point
 *
-      integer*4 plane, wire, isp, ihit, hit
-      real*4 x_pos, x_drifttime_corr, y_pos, y_drifttime_corr
+      real*4 space_points(hmax_space_points,2)
+      real*4 xdist,ydist
+      real*4 time_corr
       real*4 h_drift_dist_calc
       external h_drift_dist_calc
+
 *
 *     temporary initialization
       ABORT= .FALSE.
-      err=':success'
+      err=' '
 *
 *   
-*     Chamber 1
-      hnspace_points(1)=0
-      hncham_hits(1)=HDC_HITS_PER_PLANE(1)+HDC_HITS_PER_PLANE(2)
-     &          +HDC_HITS_PER_PLANE(3)+HDC_HITS_PER_PLANE(4)
-     &          +HDC_HITS_PER_PLANE(5)+HDC_HITS_PER_PLANE(6)
-      if(hncham_hits(1).gt.2 .and. hncham_hits(1).lt. hmax_pr_hits(1))  then
-          do i=1,hncham_hits(1)
-             hit_number(i)=i
-          enddo
-          call find_space_points(hncham_hits(1),hit_number(1),
-     &        HDC_WIRE_CENTER(1),
-     &        HDC_PLANE_NUM(1),hspace_point_criterion(1),
-     &        hxsp(1),hysp(1),hmax_space_points,
-     &        hnspace_points(1), space_points, space_point_hits)
-*    
-*    If two hits in same plane, choose one with minimum drift time
-         call h_choose_single_hit(ABORT,err,hnspace_points(1),
-     &        space_point_hits)
-*     select on minimum number of combinations and hits
-         call select_space_points(hmax_space_points,
-     &        hnspace_points(1), space_points, space_point_hits,
-     &        hmin_hit(1),hmin_combos(1))
-          do i=1,hnspace_points(1)
-             hspace_points(i,1)=space_points(i,1)
-             hspace_points(i,2)=space_points(i,2)
-             hspace_point_hits(i,1)=space_point_hits(i,1)
-             hspace_point_hits(i,2)=space_point_hits(i,2)
-             do j=1,space_point_hits(i,1)
-               hspace_point_hits(i,j+2)=space_point_hits(i,j+2)
-             enddo
-          enddo
-      endif      
+      ihit = 0
+      hnspace_points_tot = 0
+      do ich=1,hdc_num_chambers
+        easy_space_point = .false.
+        hnspace_points(ich)=0
+        hncham_hits(ich)=0
 *
-*     chamber 2
-      hnspace_points(2)=0
-      hncham_hits(2)=HDC_HITS_PER_PLANE(7)+HDC_HITS_PER_PLANE(8)
-     &     +HDC_HITS_PER_PLANE(9)+HDC_HITS_PER_PLANE(10)
-     &     +HDC_HITS_PER_PLANE(11)+HDC_HITS_PER_PLANE(12)
-      if(hncham_hits(2).gt.2  .and. hncham_hits(2).lt. hmax_pr_hits(2))  then
-        do i=hncham_hits(1)+1,hncham_hits(1)+hncham_hits(2)
-*     type *,hncham_hits(1),hncham_hits(2),i
-          hit_number(i)=i
+*     For this loop to work, hdc_planes_per_chamber must be
+*     the number of planes per chamber.  (And all chambers must have the
+*     same number of planes.)
+*
+        do ip=(ich-1)*hdc_planes_per_chamber+1,ich*hdc_planes_per_chamber
+          hncham_hits(ich)=hncham_hits(ich)+hdc_hits_per_plane(ip)
         enddo
-        call find_space_points(hncham_hits(2),hit_number(hncham_hits(1)+1),
-     &       HDC_WIRE_CENTER(hncham_hits(1)+1),
-     &       HDC_PLANE_NUM(hncham_hits(1)+1),hspace_point_criterion(2),
-     &       hxsp(1),hysp(1),hmax_space_points,
-     &       hnspace_points(2), space_points, space_point_hits)
-*    
-*    If two hits in same plane, choose one with minimum drift time
-        call h_choose_single_hit(ABORT,err,hnspace_points(2),
-     &       space_point_hits)
-*     select on minimum number of combinations and hits
-        call select_space_points(hmax_space_points,
-     &       hnspace_points(2), space_points, space_point_hits,
-     &       hmin_hit(2),hmin_combos(2))
-        do i=1,hnspace_points(2)
-          k=hnspace_points(1)+i
-          hspace_points(k,1)=space_points(i,1)
-          hspace_points(k,2)=space_points(i,2)
-          hspace_point_hits(k,1)=space_point_hits(i,1)
-          hspace_point_hits(k,2)=space_point_hits(i,2)
-          do j=1,space_point_hits(i,1)
-            hspace_point_hits(k,j+2)=space_point_hits(i,j+2)
+        yplane=2+(ich-1)*hdc_planes_per_chamber
+        yprimeplane=5+(ich-1)*hdc_planes_per_chamber
+        if(hncham_hits(ich).ge.hmin_hit(ich) .and.
+     $       hncham_hits(ich).lt.hmax_pr_hits(ich))  then
+          do i=ihit+1,ihit+hncham_hits(ich)
+            hit_number(i)=i
+            if(hdc_plane_num(i).eq.yplane) yy=i
+            if(hdc_plane_num(i).eq.yprimeplane) yyprime=i
           enddo
-        enddo
-      endif      
+          if((hdc_hits_per_plane(yplane).eq.1) .and.
+     &         (hdc_hits_per_plane(yprimeplane).eq.1).and.
+     &         ((hdc_wire_center(yy)-hdc_wire_center(yyprime))**2.lt.
+     &         (hspace_point_criterion(ich))) .and.
+     &         (hncham_hits(ich).le.6)) then
+            call h_find_easy_space_point(hncham_hits(ich),hit_number(ihit+1),
+     &           hdc_wire_center(ihit+1),hdc_plane_num(ihit+1),
+     &           hspace_point_criterion(ich),hmax_space_points,yy-ihit,
+     &           yyprime-ihit,easy_space_point,hnspace_points(ich),
+     &           space_points,space_point_hits)
+            if (.not.easy_space_point) call find_space_points(hncham_hits(ich)
+     $           ,hit_number(ihit+1),hdc_wire_center(ihit+1)
+     $           ,hdc_plane_num(ihit+1),hspace_point_criterion(ich),hxsp(1)
+     $           ,hysp(1),hmax_space_points,hnspace_points(ich), space_points,
+     $           space_point_hits)
+          else
+            call find_space_points(hncham_hits(ich),hit_number(ihit+1),
+     &           hdc_wire_center(ihit+1),
+     &           hdc_plane_num(ihit+1),hspace_point_criterion(ich),
+     &           hxsp(1),hysp(1),hmax_space_points,
+     &           hnspace_points(ich), space_points, space_point_hits)
+          endif
+*
+          if (hnspace_points(ich).gt.0) then
+*    If two hits in same plane, choose one with minimum drift time
+            call h_choose_single_hit(ABORT,err,hnspace_points(ich),
+     &           space_point_hits)
+* Select on minimum number of combinations and hits
+            call select_space_points(hmax_space_points,hnspace_points(ich),
+     &           space_points,space_point_hits,hmin_hit(ich),hmin_combos(ich),
+     $           easy_space_point)
+          endif
 
-      do plane=1,hdc_num_planes
-        hdc_sing_wcenter(plane)=-100.
+
+          do i=1,hnspace_points(ich)
+            k=hnspace_points_tot+i
+            hspace_points(k,1)=space_points(i,1)
+            hspace_points(k,2)=space_points(i,2)
+            hspace_point_hits(k,1)=space_point_hits(i,1)
+            hspace_point_hits(k,2)=space_point_hits(i,2)
+            do j=1,space_point_hits(i,1)
+              hspace_point_hits(k,j+2)=space_point_hits(i,j+2)
+            enddo
+          enddo
+        endif      
+        hnspace_points_tot = hnspace_points_tot+ hnspace_points(ich)
+        ihit = ihit + hncham_hits(ich)
       enddo
-
-*     calculate total numbe of space points
-      hnspace_points_tot=hnspace_points(1)+hnspace_points(2)
 *
-*     Now we know rough hit positions in the chambers so we can
-*     Make wire velocity drift time corrections for each hit in the space
-*     point
-      if(h_wire_vel_correction.ne.0 .and. hnspace_points_tot.gt.0) then
+* Now we know rough hit positions in the chambers so we can make
+* wire velocity drift time corrections for each hit in the space point
+*
+* Assume all wires for a plane are read out on the same side (l/r or t/b).
+* If the wire is closer to horizontal, read out left/right.  If nearer
+* vertical, assume top/bottom.  (Note, this is not always true for the
+* SOS u and v planes.  They have 1 card each on the side, but the overall
+* time offset per card will cancel much of the error caused by this.  The
+* alternative is to check by card, rather than by plane and this is harder.
+*
+      if(hnspace_points_tot.gt.0) then
         do isp=1,hnspace_points_tot
-*     write(hluno,*)' ** space point',isp
-          x_pos = hspace_points(isp,1)
-          y_pos = hspace_points(isp,2)
-          x_drifttime_corr = hdc_x_central_time + y_pos/hdc_wire_velocity
-          y_drifttime_corr = hdc_y_central_time + x_pos/hdc_wire_velocity
-*     write(hluno,*)x_pos,x_drifttime_corr,y_pos,y_drifttime_corr
+          xdist = hspace_points(isp,1)
+          ydist = hspace_points(isp,2)
           do ihit=1,hspace_point_hits(isp,1)
             hit = hspace_point_hits(isp,ihit+2)
-            plane = HDC_PLANE_NUM(hit)
-            wire = HDC_WIRE_NUM(hit)
-            if(plane.eq.2 .or. plane.eq.5 .or. 
-     &           plane.eq.8 .or. plane.eq.11) then ! Y or Y'  plane
-              HDC_DRIFT_TIME(hit)=HDC_DRIFT_TIME(hit) - y_drifttime_corr
-            else                        ! X,U,V,X' plane
-              HDC_DRIFT_TIME(hit)=HDC_DRIFT_TIME(hit) - x_drifttime_corr
+            pln = hdc_plane_num(hit)
+            if (hdc_readout_x(pln)) then !readout from side
+              time_corr = ydist*hdc_readout_corr(pln)/hdc_wire_velocity
+            else                        !readout from top/bottom
+              time_corr = xdist*hdc_readout_corr(pln)/hdc_wire_velocity
             endif
-            HDC_DRIFT_DIS(hit) = h_drift_dist_calc
-     &           (plane,wire,HDC_DRIFT_TIME(hit))
-*     write(hluno,*)ihit,hit,HDC_DRIFT_TIME(hit),HDC_DRIFT_DIS(hit)
-
+            
+            hdc_drift_time(hit)=hdc_drift_time(hit) - hdc_central_time(pln)
+     &           + hdc_drifttime_sign(pln)*time_corr
+            hdc_drift_dis(hit) = h_drift_dist_calc
+     &           (pln,hdc_wire_num(hit),hdc_drift_time(hit))
+*
 * djm 8/25/94
 * Stuff drift time and distance into registered variables for histogramming and tests.
 * In the case of two separated hits per plane, the last one will be histogrammed.
-
-            hdc_sing_drifttime(plane) = HDC_DRIFT_TIME(hit)
-            hdc_sing_driftdis(plane) = HDC_DRIFT_DIS(hit)
-* djm 9/16/94
-* add some wire positions for each plane 
-            hdc_sing_wcenter(plane) = HDC_WIRE_CENTER(hit)
-
+            hdc_sing_drifttime(pln) = hdc_drift_time(hit)
+            hdc_sing_driftdis(pln) = hdc_drift_dis(hit)
           enddo
         enddo
       endif
 *     
-*     Histogram HDC_DECODED_DC
+*     Histogram hdc_DECODED_DC
       call h_fill_dc_dec_hist(ABORT,err)
-*     
+
 *     write out results if debugflagpr is set
       if(hdebugflagpr.ne.0) then
         call h_print_pr
