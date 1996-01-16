6d15ece26c70a95343905dbee1ffacb0a646b8f8
cdaq <>
1996-01-16 21:38:47 +0000

    (JRA) Make hcer_adc pedestal subtracted value

--- HTRACKING/h_trans_cer.f
+++ HTRACKING/h_trans_cer.f
@@ -7,6 +7,9 @@
 * h_trans_cer fills the hms_decoded_cer common block
 * with track independent corrections and parameters
 * $Log$
+* Revision 1.2  1996/01/16 21:38:47  cdaq
+* (JRA) Make hcer_adc pedestal subtracted value
+*
 * Revision 1.1  1995/08/30 15:30:15  cdaq
 * Initial revision
 *
@@ -22,7 +25,7 @@
       character*20 here
       parameter (here = 'h_trans_cer')
 
-      integer*4 nhit,tube,ped_sub_adc
+      integer*4 nhit,tube
 
       save
       
@@ -31,15 +34,16 @@
       hcer_num_hits = 0
       do tube=1,hcer_num_mirrors
         hcer_npe(tube) = 0.
+        hcer_adc(tube) = 0.
       enddo
       hcer_npe_sum = 0.
       do nhit = 1, hcer_tot_hits
         tube = hcer_tube_num(nhit)
-        ped_sub_adc = hcer_adc(nhit) - hcer_ped(tube)
-        if (ped_sub_adc .gt. hcer_width(tube)) then
+        hcer_adc(tube) = hcer_raw_adc(nhit) - hcer_ped(tube)
+        if (hcer_adc(tube) .gt. hcer_width(tube)) then
           hcer_num_hits = hcer_num_hits + 1
           hcer_tube_num(hcer_num_hits) = tube
-          hcer_npe(tube) = ped_sub_adc*hcer_adc_to_npe(tube)
+          hcer_npe(tube) = hcer_adc(tube) * hcer_adc_to_npe(tube)
           hcer_npe_sum = hcer_npe_sum + hcer_npe(tube)
         endif
       enddo
