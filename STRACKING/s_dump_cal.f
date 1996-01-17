19b834bafc991d2d92dd0863aac2cb749e7cd40b
cdaq <>
1996-01-17 18:07:51 +0000

    (JRA) Put track and delta cuts on what get's written out

--- STRACKING/s_dump_cal.f
+++ STRACKING/s_dump_cal.f
@@ -17,6 +17,9 @@
 * block.
 *
 * $Log$
+* Revision 1.2  1996/01/17 18:07:51  cdaq
+* (JRA) Put track and delta cuts on what get's written out
+*
 * Revision 1.1  1995/10/09 20:17:10  cdaq
 * Initial revision
 *
@@ -39,8 +42,10 @@
 *
 *  Write out cal fitting data.
 *
-      write(35,'(1x,52f7.1,1x,e11.4)') 
-     &      ((scal_realadc(blk),blk=1,smax_cal_blocks),ssp)
-
+      if (ssshtrk.ge.0.75 .and. abs(ssdelta).le.20
+     $     .and. scer_npe_sum.ge.0.5) then
+         write(35,'(1x,44f7.1,1x,e11.4)')
+     &        (scal_realadc(blk),blk=1,smax_cal_blocks),ssp
+      endif
       RETURN
       END
