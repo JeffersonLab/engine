a47e10ca5e425a7f4fb82ae1920cc5fdb1ded0b0
saw <>
1996-01-22 15:12:35 +0000

    (JRA) Add call to g_calc_beam_pedestal

--- ENGINE/g_calc_pedestal.f
+++ ENGINE/g_calc_pedestal.f
@@ -1,6 +1,9 @@
       subroutine g_calc_pedestal(ABORT,err)
 *
 * $Log$
+* Revision 1.2  1996/01/22 15:12:35  saw
+* (JRA) Add call to g_calc_beam_pedestal
+*
 * Revision 1.1  1995/04/01 19:37:06  cdaq
 * Initial revision
 *
@@ -12,6 +15,12 @@
 *
       logical ABORT
       character*(*) err
+*
+      call g_calc_beam_pedestal(ABORT,err)
+      if(ABORT) then
+         call G_add_path(here,err)
+         return
+      endif
 *
       call h_calc_pedestal(ABORT,err)
       if(ABORT) then
