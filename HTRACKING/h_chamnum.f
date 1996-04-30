9aa686832877b4cb6f486a0e7531d440889a8664
saw <>
1996-04-30 12:32:51 +0000

    (JRA) Remove (unneeded?) check on plane range

--- HTRACKING/h_chamnum.f
+++ HTRACKING/h_chamnum.f
@@ -2,6 +2,9 @@
 *     This function returns the chamber number of a space point
 *      d.f. geesaman              17 January  1994
 * $Log$
+* Revision 1.3  1996/04/30 12:32:51  saw
+* (JRA) Remove (unneeded?) check on plane range
+*
 * Revision 1.2  1995/05/22 19:39:07  cdaq
 * (SAW) Split gen_data_data_structures into gen, hms, sos, and coin parts"
 *
@@ -18,11 +21,11 @@
       integer*4 ispace_point
 *     local variables
       integer*4 plane
-      h_chamnum=0
+
+
       plane=HDC_PLANE_NUM(hspace_point_hits(ispace_point,3))
-      if(plane.gt.0 .and. plane.le. hdc_num_planes) then
-          h_chamnum=hdc_chamber_planes(plane)
-      endif
+      h_chamnum=hdc_chamber_planes(plane)
+
       return
       end
 *
