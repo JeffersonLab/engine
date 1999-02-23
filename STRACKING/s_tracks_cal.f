28184905e669961d1c2fcc9808bba48660b36531
csa <>
1999-02-23 19:01:45 +0000

    (JRA) Clean up logical structure, remove sdebugcalcpeds stuff

--- STRACKING/s_tracks_cal.f
+++ STRACKING/s_tracks_cal.f
@@ -14,7 +14,13 @@
 *-      Created: 15 Mar 1994      Tsolak A. Amatuni
 *-      Modified 25 Mar 1994      DFG
 *-                                Change name of print routine
+*-      Modified 9 Apr 1998       Added a switch to turn on the fiducial
+*-                                cut.  The default for this is now no cut.
+*-                                K.G. Vansyoc
 * $Log$
+* Revision 1.8  1999/02/23 19:01:45  csa
+* (JRA) Clean up logical structure, remove sdebugcalcpeds stuff
+*
 * Revision 1.7  1999/01/29 17:34:59  saw
 * Add variables for second tubes on shower counter
 *
@@ -47,9 +53,6 @@
       character*12 here
       parameter (here='S_TRACKS_CAL')
 *
-      integer*4 hit,blk
-      real*4 xblk
-
       integer*4 nt      !Track number
       integer*4 nc      !Cluster number
       real*4 xf         !X position of track on calorimeter front surface
@@ -59,41 +62,52 @@
       real*4 dz_f       !Distance along Z axis between focal plane and
       real*4 dz_b       !calorimeter front(f) and back(b) surfaces
       real*4 delta_x    !Distance between track & cluster in X projection
-*
+      logical*4 track_in_fv
+
       include 'sos_data_structures.cmn'
       include 'sos_calorimeter.cmn'
       include 'sos_tracking.cmn'
-*
-*
+
       sntracks_cal=0
       if(sntracks_fp.le.0) go to 100   !Return
 *
-*-----Compute impact point coordinates on the 
-*-----calorimeter front and back surfaces
+* Compute impact point coordinates on the calorimeter front and back surfaces
+*
       do nt=1,sntracks_fp
         dz_f=scal_zmin-sz_fp(nt)
         dz_b=scal_zmax-sz_fp(nt)
-*
+
         xf=sx_fp(nt)+sxp_fp(nt)*dz_f
         xb=sx_fp(nt)+sxp_fp(nt)*dz_b
-*
+
         yf=sy_fp(nt)+syp_fp(nt)*dz_f
         yb=sy_fp(nt)+syp_fp(nt)*dz_b
-*
-        scluster_track(nt)   =-1        !Track is outside the fiducial volume
+
         strack_xc(nt)        = xf
         strack_yc(nt)        = yf
+
+        track_in_fv = (xf.le.scal_fv_xmax  .and.  xf.ge.scal_fv_xmin  .and.
+     &                 xb.le.scal_fv_xmax  .and.  xb.ge.scal_fv_xmin  .and.
+     &                 yf.le.scal_fv_ymax  .and.  yf.ge.scal_fv_ymin  .and.
+     &                 yb.le.scal_fv_ymax  .and.  yb.ge.scal_fv_ymin)
+
+* Initialize scluster_track(nt)
+        if(scal_fv_test.eq.0) then         !not using fv test
+          scluster_track(nt)=-1
+        else                               !using fv test
+          if (track_in_fv) then
+            scluster_track(nt)=0   !Track is inside the fiducial volume
+          else
+            scluster_track(nt)=-1  !Track is outside the fiducial volume
+          endif
+        endif
+
 *
-*--------Is the track inside the fiducial volume?
-        if(xf.le.scal_fv_xmax  .and.  xf.ge.scal_fv_xmin  .and.
-     &       xb.le.scal_fv_xmax  .and.  xb.ge.scal_fv_xmin  .and.
-     &       yf.le.scal_fv_ymax  .and.  yf.ge.scal_fv_ymin  .and.
-     &       yb.le.scal_fv_ymax  .and.  yb.ge.scal_fv_ymin) then
+*----------If inside fv (or no test), search for a cluster matching this track
 *
-          scluster_track(nt)=0          !Track is inside the fiducial volume
+        if((scal_fv_test.ne.0.and.track_in_fv) .or. scal_fv_test.eq.0) then
+
           if(snclusters_cal.gt.0) then
-*
-*--------------Search for a cluster matching this track
             do nc=1,snclusters_cal
               delta_x=abs(xf-scluster_xc(nc))
               if(delta_x.le.(0.5*scal_block_xsize + scal_slop)) then
@@ -101,33 +115,12 @@
                 sntracks_cal      =sntracks_cal+1
               endif                     !End ... if matched
             enddo                       !End loop over clusters
-          endif                         !End ... if inside fiducial volume
-        endif                           !End ... if number of clusters > 0
+          endif                         !End ... if number of clusters > 0
+        endif                           
       enddo                             !End loop over detector tracks
-*
-* (SAW Dec 10, 1998) Why do we use data events rather than ped events to 
-* get the pedestals.  Anyway, The following does nothing with the tubes on
-* the negative ends.
-      if (sdebugcalcpeds.ne.0) then
-        if(sntracks_fp.eq.1) then   !use blocks not on track to find pedestal
-          do hit=1,scal_tot_hits
-            blk=scal_row(hit)+smax_cal_rows*(scal_column(hit)-1)
-            xblk=scal_block_xc(blk)
-            if (abs(xf-xblk).ge.20. .and. abs(xb-xblk).ge.20.) then !blk not hit
-* Neg tubes?
-             if (scal_zero_num(blk).le.2000) then !avoid overflow in sum**2
-                scal_zero_sum(blk)=scal_zero_sum(blk)+scal_adc_pos(hit)
-                scal_zero_sum2(blk)=scal_zero_sum2(blk)
-     &                             +scal_adc_pos(hit)*scal_adc_pos(hit)
-                scal_zero_num(blk)=scal_zero_num(blk)+1
-              endif
-            endif
-          enddo
-        endif
-      endif
 
   100 continue
       if(sdbg_tracks_cal.gt.0) call s_prt_cal_tracks
-*
+
       return
       end
