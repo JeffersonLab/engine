f90bb54106c755efda397a6ddc8371a449d460dc
csa <>
1999-02-23 16:39:34 +0000

    Fixed thinko on last rev

--- ENGINE/c_clear_event.f
+++ ENGINE/c_clear_event.f
@@ -13,6 +13,9 @@
 *-   Created  29-Oct-1993   Kevin B. Beard
 *-   Modified  6-Dec-1993   K.B.Beard: adopt new errors
 * $Log$
+* Revision 1.5  1999/02/23 16:39:34  csa
+* Fixed thinko on last rev
+*
 * Revision 1.4  1999/02/10 17:44:35  csa
 * Added call to c_ntuple_clear
 *
@@ -44,15 +47,10 @@
 *
 *--------------------------------------------------------
 *
+      call c_ntuple_clear
+
       ABORT= .FALSE.
       err= ' '
-
-*     csa 4/15/97
-*
-*     added c_ntuple_clear.f to set ntuple vars to 
-*     zero so all events can go to ntuple
-*
-      call c_ntuple_clear
 
       RETURN
       END
