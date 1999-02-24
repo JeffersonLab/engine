711339d37276651df0face5b21bdc7dc5da6aa96
saw <>
1999-02-24 15:29:17 +0000

    Add to CVS tree

---
+++ PORT/ran_wrappers.f
@@ -0,0 +1,26 @@
+*
+*     Wrappers for the ran function for f2c fortran
+*
+* $Log$
+* Revision 1.1  1999/02/24 15:29:17  saw
+* Add to CVS tree
+*
+*
+      real*4 function ran(seed)
+      integer*4 seed
+      logical started
+      save started
+      real*4 RAND_MAX
+      parameter(RAND_MAX=2147483647)
+      integer*4 random
+      real*4 fran
+
+      data started /.false./
+      
+c
+c     Ignore any seeds
+c
+      fran = (random()+1.0)/(RAND_MAX+1.0)
+      ran = fran
+      return
+      end
---
+++ PORT/trig_wrappers.f
@@ -0,0 +1,30 @@
+*
+*     Wrappers for F2C intrinsic functions that have different names
+*     from standard f77s.
+*
+* $Log$
+* Revision 1.1  1999/02/24 15:29:17  saw
+* Add to CVS tree
+*
+* Revision 1.1  1996/09/09 13:34:26  saw
+* Initial revision
+*
+*
+
+      real*4 function sind(x)
+      real*4 x
+      sind = sin(x*3.1415926535/180)
+      return
+      end
+
+      real*4 function cosd(x)
+      real*4 x
+      cosd = cos(x*3.1415926535/180)
+      return
+      end
+
+      real*4 function tand(x)
+      real*4 x
+      tand = tan(x*3.1415926535/180)
+      return
+      end
