e5eaa36df452ded349c0b54649e4d16fbdb082f8
saw <>
1999-02-24 14:52:36 +0000

    Dummy routine

---
+++ ENGINE/h_ntuple_clear.f
@@ -0,0 +1,30 @@
+      subroutine h_Ntuple_clear
+*----------------------------------------------------------------------
+*
+*     Purpose : Clear vars that go to the HMS Ntuple
+*
+*     csa 4/15/97
+*
+* $Log$
+* Revision 1.1  1999/02/24 14:52:36  saw
+* Dummy routine
+*
+*
+*----------------------------------------------------------------------
+      implicit none
+      save
+*
+*      character*13 here
+*      parameter (here='h_Ntuple_clear')
+*
+*      logical ABORT
+*      character*(*) err
+*
+
+*     csa 2/2/99 -- This is a dummy routine in the CVS tree. The
+*     real thing gets created in the user's Oscar directory based
+*     on which variables are in the ntuple.lst file.
+
+*
+      RETURN
+      END      
---
+++ ENGINE/s_ntuple_clear.f
@@ -0,0 +1,30 @@
+      subroutine s_Ntuple_clear
+*----------------------------------------------------------------------
+*
+*     Purpose : Clear vars that go to the SOS Ntuple
+*
+*     csa 4/15/97
+*
+* $Log$
+* Revision 1.1  1999/02/24 14:50:24  saw
+* Dummy routine
+*
+*
+*----------------------------------------------------------------------
+      implicit none
+      save
+*
+*      character*13 here
+*      parameter (here='s_Ntuple_clear')
+*
+*      logical ABORT
+*      character*(*) err
+*
+
+*     csa 2/2/99 -- This is a dummy routine in the CVS tree. The
+*     real thing gets created in the user's Oscar directory based
+*     on which variables are in the ntuple.lst file.
+
+*
+      RETURN
+      END      
