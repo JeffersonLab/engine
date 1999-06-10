badf567d3a3262cd1e0dbcd6a37d7df8aba08547
csa <>
1999-06-10 16:57:38 +0000

    (JRA) Removed adc_max, added adc sign test, structural and cosmetic changes

--- STRACKING/s_sparsify_cal.f
+++ STRACKING/s_sparsify_cal.f
@@ -13,6 +13,9 @@
 *-                                Change name of print routines
 *-                5 Apr 1994      DFG Move print routine to s_raw_dump_all
 * $Log$
+* Revision 1.12  1999/06/10 16:57:38  csa
+* (JRA) Removed adc_max, added adc sign test, structural and cosmetic changes
+*
 * Revision 1.11  1999/02/25 20:18:40  saw
 * Vardan Tadevosyan shower code updates
 *
@@ -48,36 +51,31 @@
 *
 *-----------------------------------------------------------------------
 *
-*
       implicit none
       save
-*
+
       logical abort
       character*(*) errmsg
       character*14 here
       parameter (here='S_SPARSIFY_CAL')
-*
+
       integer*4 nh        !Loop variable for raw hits
       integer*4 nb        !Block number
       integer*4 row,col   !Row & column numbers
       integer*4 adc_pos   !ADC value
       integer*4 adc_neg   !ADC value
-      integer*4 adc_max   !Max. channel #
-      parameter (adc_max=4095)
 
       include 'sos_data_structures.cmn'
       include 'sos_calorimeter.cmn'
       include 'sos_id_histid.cmn'
-*
-*
-*
-      errmsg=' '
 
+
+      errmsg=' '
       if(scal_tot_hits.lt.0.or.scal_tot_hits.gt.smax_cal_blocks) then
-         write(6,*) here,':scal_tot_hits = ',scal_tot_hits
-         return
+        write(6,*) here,':scal_tot_hits = ',scal_tot_hits
+        return
       endif
-*
+
       scal_num_hits=0
       do nb = 1 , smax_cal_blocks
         scal_realadc_pos(nb)=-100
@@ -88,69 +86,48 @@
 *      Loop over raw hits
 *
       do nh=1,scal_tot_hits      
-         row=scal_row(nh)
-         col=scal_column(nh)
-         adc_pos=scal_adc_pos(nh)
-         adc_neg=scal_adc_neg(nh)
-*
-*------Check the validity of raw data
-c         abort=row.le.0.or.row.gt.smax_cal_rows
-c         if(abort) then
-c            write(errmsg,*) ':scal_row(',nh,') = ',row
-c            call g_prepend(here,errmsg)
-c            return
-c         endif
-*
-c         abort=col.le.0.or.col.gt.smax_cal_columns
-c         if(abort) then
-c            write(errmsg,*) ':scal_column(',nh,') = ',col
-c            call g_prepend(here,errmsg)
-c            return
-c         endif
-*
-c         abort=adc.le.0.or.adc.gt.adc_max
-c         if(abort) then
-c            write(errmsg,*) ':scal_adc(',nh,') = ',adc
-c            call g_prepend(here,errmsg)
-c            return
-c         endif
-*
-*------Sparsify the raw data
-         nb =row+smax_cal_rows*(col-1)
+        row=scal_row(nh)
+        col=scal_column(nh)
+        nb =row+smax_cal_rows*(col-1)
+        adc_pos=scal_adc_pos(nh)
+        adc_neg=scal_adc_neg(nh)
 
-ccc         scal_realadc_pos(nb) = 1.0
-ccc         scal_realadc_neg(nb) = 1.0
-*     Need to do this right
-         scal_realadc_pos(nb) = float(adc_pos) - scal_pos_ped_mean(nb)
-         scal_realadc_neg(nb) = float(adc_neg) - scal_neg_ped_mean(nb)
-         if (scal_realadc_pos(nb).le.200)
+        if (adc_pos.ge.0) then        ! =-1 if no ADC value was read.
+          scal_realadc_pos(nb) = float(adc_pos) - scal_pos_ped_mean(nb)
+          if (scal_realadc_pos(nb).le.200)
      &        call hf1(sidcalsumadc,scal_realadc_pos(nb),1.)
-         if (scal_realadc_neg(nb).le.200)
+	endif
+
+        if (adc_neg.ge.0) then        ! =-1 if no ADC value was read.
+          scal_realadc_neg(nb) = float(adc_neg) - scal_neg_ped_mean(nb)
+          if (scal_realadc_neg(nb).le.200)
      &        call hf1(sidcalsumadc,scal_realadc_neg(nb),1.)
-* ??
-         if(scal_realadc_pos(nb).gt.scal_pos_threshold(nb) .or.
+	endif
+*
+*      Sparsify the raw data
+*
+        if(scal_realadc_pos(nb).gt.scal_pos_threshold(nb) .or.
      &        scal_realadc_neg(nb).gt.scal_neg_threshold(nb)) then
-            scal_num_hits           =scal_num_hits+1
-            scal_rows(scal_num_hits)=row
-            scal_cols(scal_num_hits)=col
-ccc            if(adc_pos.lt.0) then
-            if(scal_realadc_pos(nb).lt.scal_pos_threshold(nb)) then
-               scal_adcs_pos(scal_num_hits)= 0.0
-            else
-               scal_adcs_pos(scal_num_hits)=scal_realadc_pos(nb)
-            endif
-ccc            if(adc_neg.lt.0) then
-            if(scal_realadc_neg(nb).lt.scal_neg_threshold(nb)) then
-               scal_adcs_neg(scal_num_hits)= 0.0
-            else
-               scal_adcs_neg(scal_num_hits)=scal_realadc_neg(nb)
-            endif
-         endif
+
+           scal_num_hits=scal_num_hits+1
+           scal_rows(scal_num_hits)=row
+           scal_cols(scal_num_hits)=col
+           if(scal_realadc_pos(nb).lt.scal_pos_threshold(nb)) then
+              scal_adcs_pos(scal_num_hits)= 0.0
+           else
+              scal_adcs_pos(scal_num_hits)=scal_realadc_pos(nb)
+           endif
+           if(scal_realadc_neg(nb).lt.scal_neg_threshold(nb)) then
+              scal_adcs_neg(scal_num_hits)= 0.0
+           else
+              scal_adcs_neg(scal_num_hits)=scal_realadc_neg(nb)
+           endif
+        endif
       enddo                      !End loop over raw hits
-*
+
       if(sdbg_sparsified_cal.gt.0) call s_prt_cal_sparsified
-*
+
       call s_fill_cal_hist(abort,errmsg)
-*
+
       return
       end
