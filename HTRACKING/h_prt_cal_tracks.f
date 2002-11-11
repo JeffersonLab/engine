661cbc7661a6970a4fb67a61c38bee5bc6857326
jones <>
2002-11-11 18:52:43 +0000

    Modified to keep count of number of hits in each block and try to even out the number of hits for each block in the calibration file.

--- HTRACKING/h_prt_cal_tracks.f
+++ HTRACKING/h_prt_cal_tracks.f
@@ -21,13 +21,28 @@ c     calibration with electrons.
       integer hcal_calib_event  !max.number of events to be calibrated.
       parameter (hcal_calib_event = 50 000)
 
-      common/hcal_calib/ncall
+      integer nct_hit_blk(78),ipmt
+      logical write_out
+c
+      common/hcal_calib/nct_hit_blk,ncall,spare_id
       integer ncall
       data ncall/0/
 
       real thr_lo,thr_hi        !thresholds on sammed raw calorimeter signal.
-
+c
+      integer spare_id
+      logical ABORT
+      character*80 err
+c
       ncall=ncall+1
+
+      if (ncall .eq. 1) then
+	call g_IO_control(spare_id,'ANY',ABORT,err)  !get IO channel
+         open(spare_id)
+         do ipmt=1,78
+            nct_hit_blk(ipmt)=0
+         enddo
+      endif
 
 c     Choose clean single electron tracks within HMS momentum acceptance.
       if(  (hntracks_fp.eq.1).and.
@@ -35,10 +50,21 @@ c     Choose clean single electron tracks within HMS momentum acceptance.
      &     (hntracks_cal.eq.1).and.
      &     (hcer_npe_sum.gt.4).and.
      &     (abs(hdelta_tar(1)).lt.10.).and.
-     &     (abs(hbeta(1)-1.).lt.0.05)  ) then
+     &     (abs(hbeta(1)-1.).lt.0.05)  .and.
+     &     spare_id .ne. 0 ) then
 ***     &     (hbeta_chisq(1).ge.0.).and.(hbeta_chisq(1).lt.1.)  ) then
 
-         write(hlun_dbg_cal,'(i2,1x,f7.4,2(1x,f5.1,1x,f9.6))')
+c
+         write_out = .false.
+         do ihit=1,hcal_num_hits
+            nblk=(hcal_cols(ihit)-1)*hmax_cal_rows+hcal_rows(ihit)
+            nct_hit_blk(nblk) = nct_hit_blk(nblk) + 1
+            if (nct_hit_blk(nblk) .lt. 4000) write_out = .true.
+         enddo
+c
+         if (write_out) then
+c
+         write(spare_id,'(i2,1x,f7.4,2(1x,f5.1,1x,f9.6))')
      &        hcal_num_hits,hp_tar(1),
      &        htrack_xc(1),hxp_fp(1),htrack_yc(1),hyp_fp(1)
 
@@ -52,34 +78,34 @@ c     Choose clean single electron tracks within HMS momentum acceptance.
             adc_pos=hcal_adcs_pos(ihit)
             nblk=(hcal_cols(ihit)-1)*hmax_cal_rows+hcal_rows(ihit)
 
-            write(hlun_dbg_cal,'(2(f9.3,1x),i2)'),
+            write(spare_id,'(2(f9.3,1x),i2)'),
      &           adc_pos,adc_neg,nblk
 
          end do
-
+       endif ! if write_out
+c
       end if
 
       if((ncall/hcal_calib_event)*hcal_calib_event.eq.ncall) then
 
-         close(hlun_dbg_cal)
+         close(spare_id)
+
 
          print*,'=========================================================='
          print*,'Calibrating HMS Calorimeter at event #',gen_event_id_number
 
-         call hcal_raw_thr(hlun_dbg_cal,thr_lo,thr_hi)
+         call hcal_raw_thr(spare_id,thr_lo,thr_hi)
          print*,'lo & hi thresholds:', thr_lo,thr_hi
-         call hcal_clb_det(hlun_dbg_cal,gen_run_number,thr_lo,thr_hi)
+         call hcal_clb_det(spare_id,gen_run_number,thr_lo,thr_hi)
 
          print*,'=========================================================='
 
 D         pause
-
-         open(hlun_dbg_cal,access='append')
+           
+c         spare_id = 0
+         open(spare_id,access='append')
 
       end if
 
       end
 
-*=======================================================================
-      include 'hcal_raw_thr.f'
-      include 'hcal_clb_det.f'
