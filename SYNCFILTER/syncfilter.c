01ee2b7a20e03d1fb7a07490c401d38c647e4cc2
jones <>
2003-12-19 17:46:30 +0000

    Add code to work with segmented data files.

--- SYNCFILTER/syncfilter.c
+++ SYNCFILTER/syncfilter.c
@@ -36,6 +36,81 @@ void writeoutthebigbuffer(int ohandle,int *really_big_buffer,int *endpointer,
 void writeinsyncevent(int ohandle,int nev);
 void writeearlyendevent(int ohandle,int nev);
 
+
+int GetNextEvent(char *fname,int *handle,int *buffer) {
+  int status;
+  char *filename;
+  char *xfilename;
+
+  static char infileext[3]; 
+  static int segno = -1;
+
+  /*  
+  If current value of HANDLE is -1, the file has not been opened yet
+  so open it and start reading
+  If read returns error, check segment number: if it's -1, then
+  we're in single segment mode and the run has ended.  If segno
+  is 0 or larger, we are in multi-segment mode and need to switch
+  segments
+  */
+  
+  if (*handle == -1) {    /* open first file */
+     filename = (char *)malloc(strlen(fname)+1);
+     strcpy(filename,fname);
+         fprintf(stderr,"///syncfilter: filename %s\n",
+	  filename);
+     status = evOpen(filename,"r",handle);
+     if(status==0) {
+     	 segno = -1;
+         fprintf(stderr,"///syncfilter: Processing singular input file %s\n",
+	  filename);
+     } else {
+     	 segno = 0;
+     	 infileext[0] = '.';
+     	 infileext[1] = '0';
+     	 infileext[2] = '\0';
+     	 xfilename = (char *)malloc(strlen(fname)+3);
+     	 strcpy(xfilename,fname);
+     	 strcat(xfilename,infileext);    
+     	 status=evOpen(xfilename,"r",handle);
+     	 if(status!=0) {
+             fprintf(stderr,"///syncfilter TROUBLE: ");
+	     fprintf(stderr,"Error opening input file (%d)\n\n",status);
+             fprintf(stderr,"///syncfilter: Processing input file segment %s\n\n",
+	  xfilename);
+	     free(xfilename);
+     	     return(status);
+     	 }
+         fprintf(stderr,"///syncfilter: Processing input file segment %s\n\n",
+	  xfilename);
+	 free(xfilename);
+     }
+  }
+
+  status=evRead(*handle,buffer,MAX_EVENT_LEN);
+  if(status!=0 && segno>=0) {
+        evClose(*handle);
+     	segno++;
+	infileext[1]++;
+     	xfilename = (char *)malloc(strlen(fname)+3);
+     	strcpy(xfilename,fname);
+     	strcat(xfilename,infileext);
+	status=evOpen(xfilename,"r",handle);
+        if(status==0) {
+           fprintf(stderr,"///syncfilter: Continuing with ");
+	   fprintf(stderr,"input segment %d (%s)\n\n",segno,xfilename);
+           status=evRead(*handle,buffer,MAX_EVENT_LEN);
+	}
+	free(xfilename);
+  }
+   
+  if (status == 1) evClose(*handle);
+	      
+  return(status);
+}
+
+
+
 main(int argc, char **argv)
 {
   int nevents,evtype,count,diff;
@@ -55,29 +130,39 @@ main(int argc, char **argv)
   int num_between_scalread;
   int num_event4;
   int nscalread;
+  char *infilename;
+ char *outfilename;
 
   really_big_buffer = (int *) malloc(MAX_REALLYBIGBUFFER*sizeof(int));
   writeoutput = 1;
   if(argc>1) {
-    status = evOpen(argv[1],"r",&ihandle);
-    if(argc>2) {
-      status = evOpen(argv[2],"w",&ohandle);
-    } else {
-      status = evOpen("-","w",&ohandle);
-    }
-    if(status!=0) {
-      writeoutput=0;
-      fprintf(stderr,"Couldn't open output file, scanning input for errors\n");
-    }
+     infilename = (char *)malloc(strlen(argv[1])+1); 
+     strcpy(infilename,argv[1]);
+     if(argc>2) {
+        outfilename = (char *)malloc(strlen(argv[2])+1); 
+        strcpy(outfilename,argv[2]);
+     } else {
+        outfilename = "-";
+     }
   } else {
-    status = evOpen("-","r",&ihandle);
-    status = evOpen("-","w",&ohandle);
+      infilename = "-";
+      outfilename = "-";
+  }
+  ihandle=-1;
+  if (DEBUG)  fprintf(stderr,"\n///syncfilter infilename %s ",infilename);
+  if (DEBUG)  fprintf(stderr,"\n///syncfilter outfilename %s ",outfilename);
+  
+  status = evOpen(outfilename,"w",&ohandle);
+  if(status!=0) {
+    fprintf(stderr,"\n///syncfilter TROUBLE: couldn't open ");
+    fprintf(stderr,"output file, scanning input for errors\n");
+    status = evOpen("/dev/null","w",&ohandle);
   }
 
   for(i=0;i<16;i++) {
     goodevs[i] = badevs[i] = endevs[i] = 0; /* Initialize event counters */
   }
-    
+
   nevents = 0;
   num_between_scalread=0;
   endpointer = 0;
@@ -87,8 +172,7 @@ main(int argc, char **argv)
   nscalread=0;
   writeinsyncevent(ohandle,nevents);
 
-  while ((status=evRead(ihandle,buffer,MAX_EVENT_LEN)) == 0) {
-	 /*	 || status==1081278465) {*/
+    while ((status=GetNextEvent(infilename,&ihandle,buffer)) == 0) { 
     nevents++;
     sumlen = 1;
     evtype = buffer[1]>>16;
@@ -96,22 +180,27 @@ main(int argc, char **argv)
     datatype = (buffer[1] & 0xff00) >> 8;
     
 
-    if(!outofsync && problems(nevents,buffer)) {
+    /*    if(!outofsync && problems(nevents,buffer)) {
       outofsync = 1;
-    }
+      }*/
+        if( problems(nevents,buffer) && !outofsync ) {
+      outofsync = 1;
+      } 
 
     if(evtype==4) {
       num_event4++;
+      if(DEBUG) fprintf(stderr,"%d: %d %d\n",nevents,evtype,num_event4);
     }
     if(evtype==20) {
       fprintf(stderr,"END event found, number of events since last scaler read : %d\n",num_between_scalread);
+      appendevent(really_big_buffer,buffer,&endpointer);
       break;
     }
     if(evtype==16 || evtype==0 || evtype==18 ) {
       if(nevents>1000) {
 	if(DEBUG) fprintf(stderr,"%d: %d\n",nevents,evtype);
       }
-      if ( num_event4 != 1000) {
+      if ( num_event4 != 1000 && evtype==0 ) {
 	fprintf(stderr,"Number of Scaler reads before 1000 event type 4 =%d\n",nscalread);
       }
       if (evtype==0) {
@@ -147,15 +236,17 @@ main(int argc, char **argv)
       writeearlyendevent(ohandle,n_at_outofsync);
       writeoutthebigbuffer(ohandle,really_big_buffer,&endpointer,endevs);
     } else if(outofsync) {
+      fprintf(stderr,"Last buffer sent (out-of-sync)\n");
       writeoutofsyncevent(ohandle,n_at_outofsync);
       writeoutthebigbuffer(ohandle,really_big_buffer,&endpointer,badevs);
     } else {
+      fprintf(stderr,"Last buffer sent (in-sync) \n");
       writeoutthebigbuffer(ohandle,really_big_buffer,&endpointer,goodevs);
     }
 
     if(DEBUG) fprintf(stderr,"Events=%d\n",nevents);
   }
-  fprintf(stderr,"%d:%s\n",status,err);
+  if (DEBUG) fprintf(stderr,"%d:%s\n",status,err);
   fprintf(stderr,"%d events in run\n",nevents);
   evClose(ihandle);
   evClose(ohandle); 
@@ -251,7 +342,7 @@ void writeoutofsyncevent(int ohandle,int nev)
   evbuf[3] = 1<<8;		/* Integer type */
   evbuf[4] = OUTSYNC;
   evWrite(ohandle,evbuf);
-  fprintf(stderr,"OUT OF SYNC at %d\n",nev);
+  fprintf(stderr,"SF: OUT OF SYNC at %d\n",nev);
 }
 void writeinsyncevent(int ohandle,int nev)
 {
@@ -262,7 +353,7 @@ void writeinsyncevent(int ohandle,int nev)
   evbuf[3] = 1<<8;		/* Integer type */
   evbuf[4] = INSYNC;
   evWrite(ohandle,evbuf);
-  fprintf(stderr,"IN OF SYNC at %d\n",nev);
+  fprintf(stderr,"SF: IN OF SYNC at %d\n",nev);
 }
 void writeearlyendevent(int ohandle,int nev)
 {
