204b1a1296889aeeef0ef055961d43877534d958
puckett <>
2007-07-17 22:30:24 +0000

    new cluster finding algorithm and data structures

--- BTRACKING/b_calc_physics.f
+++ BTRACKING/b_calc_physics.f
@@ -12,6 +12,7 @@
       include 'bigcal_data_structures.cmn'
 c      include 'gen_units.par'
       include 'gen_constants.par'
+      include 'gen_data_structures.cmn'
 
       integer i,j,k,ntrack,itrackmax,nprot,nrcs,nmid
       real E,x,y,z,t,R,L,tof,Rperp
@@ -19,7 +20,7 @@ c      include 'gen_units.par'
       real thetadeg,thetarad,phideg,phirad
       real Sinth,Costh
       real m_e
-      real mom,beta,c
+      real mom,beta,c,eloss,gamma,log10betagamma
       real maxetot
 
       ABORT=.false.
@@ -36,192 +37,70 @@ c$$$      nprot = 0
 c$$$      nrcs = 0
 c$$$      nmid = 0
 
-      if(BIGCAL_PROT_NCLSTR.gt.0) then
-         do i=1,BIGCAL_PROT_NCLSTR
+      if(BIGCAL_ALL_NCLSTR.gt.0) then
+         do i=1,BIGCAL_ALL_NCLSTR
             ntrack = ntrack + 1
 c            nprot = nprot + 1
-            x = BIGCAL_PROT_CLSTR_X(i)
-            y = BIGCAL_PROT_CLSTR_Y(i)
-            E = BIGCAL_PROT_CLSTR_ETOT(i)
-            t = BIGCAL_PROT_CLSTR_T8BEST(i)
+            x = BIGCAL_ALL_CLSTR_X(i)
+            y = BIGCAL_ALL_CLSTR_Y(i)
+            E = BIGCAL_ALL_CLSTR_ETOT(i)
+            t = BIGCAL_ALL_CLSTR_T8MEAN(i)
 
+c     correct every track for energy loss. BigCal is always electron arm
+c     need to set up eloss params for BigCal absorber!
             xrot = x * Costh + R * Sinth
             zrot = -x * Sinth + R * Costh
             
             L = sqrt(xrot**2 + zrot**2 + y**2)
             ! all length units are cm
-            mom = sqrt(E**2 - m_e**2) 
-            beta = mom/E
-            tof = L/(beta*c)
 
             thetarad = acos(zrot/L)
             thetadeg = 180./tt * thetarad
-            
-            Rperp = L*sin(thetarad)
 
             phirad = atan2(y,xrot)
             phideg = 180./tt * phirad
+ 
+            gamma = E / m_e
+            beta = sqrt(1. - 1./gamma**2)
 
-            BIGCAL_TRACK_THETARAD(ntrack) = thetarad
-            BIGCAL_TRACK_THETADEG(ntrack) = thetadeg
-            BIGCAL_TRACK_PHIRAD(ntrack) = phirad
-            BIGCAL_TRACK_PHIDEG(ntrack) = phideg
-            BIGCAL_TRACK_ENERGY(ntrack) = E
-            BIGCAL_TRACK_TIME(ntrack) = t
-            BIGCAL_TRACK_XFACE(ntrack) = xrot
-            BIGCAL_TRACK_YFACE(ntrack) = y
-            BIGCAL_TRACK_ZFACE(ntrack) = zrot
-            BIGCAL_TRACK_PX(ntrack) = mom * sin(thetarad) * cos(phirad)
-            BIGCAL_TRACK_PY(ntrack) = mom * sin(thetarad) * sin(phirad)
-            BIGCAL_TRACK_PZ(ntrack) = mom * cos(thetarad)
-            BIGCAL_TRACK_BETA(ntrack) = beta
-            BIGCAL_TRACK_TOF(ntrack) = tof
-            BIGCAL_TRACK_COIN_TIME(ntrack) = t - tof ! "vertex" time
-         enddo
-      endif
+            log10betagamma = log(beta*gamma) / log(10.)
 
-      if(BIGCAL_RCS_NCLSTR.gt.0) then
-         do i=1,BIGCAL_RCS_NCLSTR
-            ntrack = ntrack + 1
-c            nrcs = nrcs + 1
-            x = BIGCAL_RCS_CLSTR_X(i)
-            y = BIGCAL_RCS_CLSTR_Y(i)
-            E = BIGCAL_RCS_CLSTR_ETOT(i)
-            t = BIGCAL_RCS_CLSTR_T8BEST(i)
-
-            xrot = x * Costh + R * Sinth
-            zrot = -x * Sinth + R * Costh
-            
-            L = sqrt(xrot**2 + zrot**2 + y**2)
-            ! all length units are cm
-            mom = sqrt(E**2 - m_e**2) 
-            beta = mom/E
-            tof = L/(beta*c)
-
-            thetarad = acos(zrot/L)
-            thetadeg = 180./tt * thetarad
-            
-            Rperp = L*sin(thetarad)
-
-            phirad = atan2(y,xrot)
-            phideg = 180./tt * phirad
-
-            BIGCAL_TRACK_THETARAD(ntrack) = thetarad
-            BIGCAL_TRACK_THETADEG(ntrack) = thetadeg
-            BIGCAL_TRACK_PHIRAD(ntrack) = phirad
-            BIGCAL_TRACK_PHIDEG(ntrack) = phideg
-            BIGCAL_TRACK_ENERGY(ntrack) = E
-            BIGCAL_TRACK_TIME(ntrack) = t
-            BIGCAL_TRACK_XFACE(ntrack) = xrot
-            BIGCAL_TRACK_YFACE(ntrack) = y
-            BIGCAL_TRACK_ZFACE(ntrack) = zrot
-            BIGCAL_TRACK_PX(ntrack) = mom * sin(thetarad) * cos(phirad)
-            BIGCAL_TRACK_PY(ntrack) = mom * sin(thetarad) * sin(phirad)
-            BIGCAL_TRACK_PZ(ntrack) = mom * cos(thetarad)
-            BIGCAL_TRACK_BETA(ntrack) = beta
-            BIGCAL_TRACK_TOF(ntrack) = tof
-            BIGCAL_TRACK_COIN_TIME(ntrack) = t - tof ! "vertex" time
-         enddo
-      endif
-
-      if(BIGCAL_MID_NCLSTR.gt.0) then
-         do i=1,BIGCAL_MID_NCLSTR
-            ntrack = ntrack + 1
-c            nmid = nmid + 1
-
-            x = BIGCAL_MID_CLSTR_X(i)
-            y = BIGCAL_MID_CLSTR_Y(i)
-            E = BIGCAL_MID_CLSTR_ETOT(i)
-            t = BIGCAL_MID_CLSTR_T8BEST(i)
-
-            xrot = x * Costh + R * Sinth
-            zrot = -x * Sinth + R * Costh
-            
-            L = sqrt(xrot**2 + zrot**2 + y**2)
-            ! all length units are cm
-            mom = sqrt(E**2 - m_e**2) 
-            beta = mom/E
-            tof = L/(beta*c)
-
-            thetarad = acos(zrot/L)
-            thetadeg = 180./tt * thetarad
-            
-            Rperp = L*sin(thetarad)
-
-            phirad = atan2(y,xrot)
-            phideg = 180./tt * phirad
-
-            BIGCAL_TRACK_THETARAD(ntrack) = thetarad
-            BIGCAL_TRACK_THETADEG(ntrack) = thetadeg
-            BIGCAL_TRACK_PHIRAD(ntrack) = phirad
-            BIGCAL_TRACK_PHIDEG(ntrack) = phideg
-            BIGCAL_TRACK_ENERGY(ntrack) = E
-            BIGCAL_TRACK_TIME(ntrack) = t
-            BIGCAL_TRACK_XFACE(ntrack) = xrot
-            BIGCAL_TRACK_YFACE(ntrack) = y
-            BIGCAL_TRACK_ZFACE(ntrack) = zrot
-            BIGCAL_TRACK_PX(ntrack) = mom * sin(thetarad) * cos(phirad)
-            BIGCAL_TRACK_PY(ntrack) = mom * sin(thetarad) * sin(phirad)
-            BIGCAL_TRACK_PZ(ntrack) = mom * cos(thetarad)
-            BIGCAL_TRACK_BETA(ntrack) = beta
-            BIGCAL_TRACK_TOF(ntrack) = tof
-            BIGCAL_TRACK_COIN_TIME(ntrack) = t - tof ! "vertex" time
-         enddo
-      endif
-
-      BIGCAL_PHYS_NTRACK = ntrack
-
-      ! for now, simply choose track with greatest energy as "best" track
-      ! don't distinguish which section of the calorimeter the track comes from
-
-      if(ntrack.gt.0) then
-
-         itrackmax = 0
-         maxetot = 0.
-         
-         do i=1,ntrack
-            if(BIGCAL_TRACK_ENERGY(i).gt.maxetot) then
-               maxetot = BIGCAL_TRACK_ENERGY(i)
-               itrackmax = i
+            if(gtarg_z(gtarg_num).gt.0) then
+               call total_eloss(3,.true.,thetarad,log10betagamma,eloss)
+            else 
+               eloss = 0.
             endif
+c     for now, set eloss to zero for monte carlo analysis!
+c$$$            if(gen_bigcal_mc.ne.0) then
+c$$$               eloss = 0.
+c$$$            endif
+
+            E = E + eloss
+
+            mom = sqrt(E**2 - m_e**2) 
+            beta = mom/E
+            tof = L/(beta*c)
+
+c            Rperp = L*sin(thetarad)
+
+            BIGCAL_TRACK_THETARAD(ntrack) = thetarad
+            BIGCAL_TRACK_THETADEG(ntrack) = thetadeg
+            BIGCAL_TRACK_PHIRAD(ntrack) = phirad
+            BIGCAL_TRACK_PHIDEG(ntrack) = phideg
+            BIGCAL_TRACK_ENERGY(ntrack) = E
+            BIGCAL_TRACK_TIME(ntrack) = t
+            BIGCAL_TRACK_XFACE(ntrack) = xrot
+            BIGCAL_TRACK_YFACE(ntrack) = y
+            BIGCAL_TRACK_ZFACE(ntrack) = zrot
+            BIGCAL_TRACK_PX(ntrack) = mom * sin(thetarad) * cos(phirad)
+            BIGCAL_TRACK_PY(ntrack) = mom * sin(thetarad) * sin(phirad)
+            BIGCAL_TRACK_PZ(ntrack) = mom * cos(thetarad)
+            BIGCAL_TRACK_BETA(ntrack) = beta
+            BIGCAL_TRACK_TOF(ntrack) = tof
+            BIGCAL_TRACK_COIN_TIME(ntrack) = t - tof ! "vertex" time
          enddo
-         
-         nprot = BIGCAL_PROT_NCLSTR
-         nrcs = BIGCAL_RCS_NCLSTR
-         nmid = BIGCAL_MID_NCLSTR
-         
-         if(itrackmax.le.nprot) then
-            BIGCAL_BEST_CLSTR_ISECTION = 1
-            BIGCAL_BEST_CLSTR_ICLUSTER = itrackmax
-         else if(itrackmax-nprot.le.nrcs) then
-            BIGCAL_BEST_CLSTR_ISECTION = 2
-            BIGCAL_BEST_CLSTR_ICLUSTER = itrackmax - nprot
-         else if(itrackmax-nprot-nrcs.le.nmid) then
-            BIGCAL_BEST_CLSTR_ISECTION = 3
-            BIGCAL_BEST_CLSTR_ICLUSTER = itrackmax - nprot - nrcs
-         else                   ! itrackmax out of range. should never happen
-            BIGCAL_BEST_CLSTR_ISECTION = 0
-            BIGCAL_BEST_CLSTR_ICLUSTER = 0
-         endif
-         
-         
-         BIGCAL_BEST_THETARAD = BIGCAL_TRACK_THETARAD(itrackmax)
-         BIGCAL_BEST_THETADEG = BIGCAL_TRACK_THETADEG(itrackmax)
-         BIGCAL_BEST_PHIRAD = BIGCAL_TRACK_PHIRAD(itrackmax)
-         BIGCAL_BEST_PHIDEG = BIGCAL_TRACK_PHIDEG(itrackmax)
-         BIGCAL_BEST_ENERGY = BIGCAL_TRACK_ENERGY(itrackmax)
-         BIGCAL_BEST_TIME = BIGCAL_TRACK_TIME(itrackmax)
-         BIGCAL_BEST_XFACE = BIGCAL_TRACK_XFACE(itrackmax)
-         BIGCAL_BEST_YFACE = BIGCAL_TRACK_YFACE(itrackmax)
-         BIGCAL_BEST_ZFACE = BIGCAL_TRACK_ZFACE(itrackmax)
-         BIGCAL_BEST_PX = BIGCAL_TRACK_PX(itrackmax)
-         BIGCAL_BEST_PY = BIGCAL_TRACK_PY(itrackmax)
-         BIGCAL_BEST_PZ = BIGCAL_TRACK_PZ(itrackmax)
-         BIGCAL_BEST_BETA = BIGCAL_TRACK_BETA(itrackmax)
-         BIGCAL_BEST_TOF = BIGCAL_TRACK_TOF(itrackmax)
-         BIGCAL_BEST_COIN_TIME = BIGCAL_TRACK_COIN_TIME(itrackmax)
-
+         bigcal_phys_ntrack = ntrack
       endif
-
+     
       return 
       end
