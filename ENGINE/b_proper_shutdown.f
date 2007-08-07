dffed02f61e932b461e05282f8b57064f2be4d79
puckett <>
2007-08-07 19:00:28 +0000

    added efficiency calculations at end of run

--- ENGINE/b_proper_shutdown.f
+++ ENGINE/b_proper_shutdown.f
@@ -12,6 +12,7 @@
       include 'bigcal_data_structures.cmn'
       include 'bigcal_filenames.cmn'
       include 'bigcal_bypass_switches.cmn'
+      include 'bigcal_hist_id.cmn'
 
       logical abort, report_abort
       character*(*) err
@@ -20,10 +21,47 @@
       integer ierr
       character*132 file
 
+      integer irow,icol,icell
+      real Eavg,eff
+
       abort=.false.
       err=' '
 
       call b_report_bad_data(lunout,ABORT,err)
+
+c     calculate final "efficiencies" and fill histograms
+      
+      do icell=1,bigcal_all_maxhits
+         if(icell.le.bigcal_prot_maxhits) then
+            irow=(icell-1)/32 + 1
+            icol=mod(icell-1,32) + 1
+         else
+            irow=(icell-1-bigcal_prot_maxhits)/30 + 33
+            icol=mod(icell-1-bigcal_prot_maxhits,30)+1
+         endif
+
+         if(b_all_run_Enum(icell).gt.0) then
+            Eavg = b_all_run_Esum(icell)/float(b_all_run_Enum(icell))
+         else 
+            Eavg = 0.
+         endif
+         call hf2(bid_bcal_exy,float(icol),float(irow),Eavg)
+
+         if(b_all_run_clst_good(icell)+b_all_run_clst_bad(icell)
+     $        .gt.0)then
+            eff = float(b_all_run_clst_good(icell)) / 
+     $           float(b_all_run_clst_good(icell) + 
+     $           b_all_run_clst_bad(icell))
+         else 
+            eff = 0.
+         endif
+         if(icell.le.bigcal_prot_maxhits) then
+            call hf1(bid_bcal_prot_eff,float(icell),eff)
+         else
+            call hf1(bid_bcal_rcs_eff,float(icell-bigcal_prot_maxhits),eff)
+         endif
+      enddo
+
 
       if(b_report_blockname.ne.' '.and.
      $     b_report_output_filename.ne.' ') then
