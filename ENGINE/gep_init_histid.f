449eff1d71c37069acb10b320617d118bb8cdfd8
puckett <>
2007-08-15 21:45:13 +0000

    histogram initialization routine for gep (coincidence) histograms

---
+++ ENGINE/gep_init_histid.f
@@ -0,0 +1,23 @@
+      subroutine gep_init_histid(abort,err)
+
+      implicit none
+      save
+
+      character*15 here
+      parameter(here='gep_init_histid')
+
+      logical abort
+      character*(*) err
+      external thgetid
+      integer*4 thgetid
+     
+      include 'gep_data_structures.cmn'
+      include 'gep_hist_id.cmn'
+
+c     do nothing until we set up some histograms...
+
+      abort=.false.
+      err=' '
+
+      return 
+      end
