189f956f58b51890f9885932f185f0c6e1f45123
puckett <>
2007-08-27 18:38:48 +0000

    Added call to calibration routines for Monte Carlo analysis

--- BTRACKING/b_reconstruction.f
+++ BTRACKING/b_reconstruction.f
@@ -153,9 +153,20 @@ c$$$         endif
             return
          endif
       endif
-
+c     if dealing with real data and not monte carlo, then the appropriate
+c     place to do the calibration matrix is in gep_reconstruction, because
+c     we need to know the incident electron energy, hence we need the hms
+c     info.
+      if(gen_bigcal_mc.ne.0.and.bigcal_do_calibration.ne.0) then
+         call b_matrix_accum(abort,err)
+         if(abort) then
+            call g_add_path(here,err)
+            return
+         endif
+      endif
+      
       !write(*,*) 'done with reconstruction'
-
+      
 *
       return
       end
