ffa3f0e37807ebaabded4733fd29c89de770ed52
puckett <>
2007-08-27 19:08:42 +0000

    Added call to BigCal calibration routine

--- ENGINE/gep_reconstruction.f
+++ ENGINE/gep_reconstruction.f
@@ -12,6 +12,7 @@
       include 'gep_data_structures.cmn'
       include 'gen_constants.par'
       include 'gen_units.par'
+      include 'bigcal_bypass_switches.cmn'
 c      include 'gep_bypass_switches.cmn'
 
       abort=.false.
@@ -24,6 +25,14 @@ c      if(gepbypass_physics.eq.0) then
          return
       endif
       
+      if(bigcal_do_calibration.ne.0) then
+         call b_matrix_accum(abort,err)
+         if(abort) then
+            call g_add_path(here,err)
+            return
+         endif
+      endif
+
 c     successful return
 
       abort = .false.
