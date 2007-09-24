9152e5e49d32120616cc24f0dc82e291e706c57e
puckett <>
2007-09-24 20:37:50 +0000

    added BigCal debugging output file

--- ENGINE/g_init_filenames.f
+++ ENGINE/g_init_filenames.f
@@ -23,6 +23,9 @@
 *-    Modified   3-Dec-1993 Kevin Beard, Hampton U.
 *-    Modified   8-Dec-1993 Kevin Beard; rewrote parsing,added 'data' type
 * $Log$
+* Revision 1.19.6.6  2007/09/24 20:37:50  puckett
+* added BigCal debugging output file
+*
 * Revision 1.19.6.5  2007/09/07 16:05:29  puckett
 * removed initialization of filenames for bigcal calibration reports, no longer used, this info will go in regular bigcal report
 *
@@ -194,6 +197,7 @@ c     is reached (bigcal_min_calib_events)
       
       b_calib_matrix_filename = ' '
       b_calib_parm_filename = ' '
+      b_debug_output_filename = ' '
 c      b_calib_report_filename = ' ' ! not used
       
 *
