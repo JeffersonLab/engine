3f1cbf951552211ca9159a62687493d25033a0ab
cdaq <>
2007-10-17 15:52:29 +0000

    *** empty log message ***

--- ENGINE/g_trans_misc.f
+++ ENGINE/g_trans_misc.f
@@ -6,6 +6,9 @@
 * g_trans_misc fills the gen_decoded_misc common block
 *
 * $Log$
+* Revision 1.2.24.1  2007/10/17 15:52:29  cdaq
+* *** empty log message ***
+*
 * Revision 1.2  2002/09/25 14:37:32  jones
 * character*1024 errmsg changed to character*(*) errmsgCVS: ----------------------------------------------------------------------
 *
@@ -35,7 +38,19 @@
         gmisc_dec_data(ihit,2) = -1     ! Clear ADC's
       enddo
       
+c      write(*,*) 'gmisc_tot_hits=',gmisc_tot_hits
+
       do ihit = 1 , gmisc_tot_hits
+c$$$         if((gmisc_raw_addr2(ihit).eq.1.or.gmisc_raw_addr2(ihit).eq.2)
+c$$$     $        .and. gmisc_raw_addr1(ihit).eq.2) then
+c$$$            if(gmisc_raw_addr2(ihit).eq.1) then ! h+
+c$$$               write(*,*) 'h+ hit, raw ADC=',gmisc_raw_data(ihit)
+c$$$            endif
+c$$$            if(gmisc_raw_addr2(ihit).eq.2) then ! h-
+c$$$               write(*,*) 'h- hit, raw ADC=',gmisc_raw_data(ihit)
+c$$$            endif
+c$$$         endif
+
         gmisc_dec_data(gmisc_raw_addr2(ihit),gmisc_raw_addr1(ihit)) =
      $       gmisc_raw_data(ihit)
       enddo
