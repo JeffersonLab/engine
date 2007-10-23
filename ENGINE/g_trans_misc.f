8a19257ecb87f45f6f250327fa74f91075e39c70
cdaq <>
2007-10-23 13:23:32 +0000

    Added filling of raw trigger TDC histograms, signals are in the gmisc hit array

--- ENGINE/g_trans_misc.f
+++ ENGINE/g_trans_misc.f
@@ -6,6 +6,9 @@
 * g_trans_misc fills the gen_decoded_misc common block
 *
 * $Log$
+* Revision 1.2.24.2  2007/10/23 13:23:32  cdaq
+* Added filling of raw trigger TDC histograms, signals are in the gmisc hit array
+*
 * Revision 1.2.24.1  2007/10/17 15:52:29  cdaq
 * *** empty log message ***
 *
@@ -20,6 +23,7 @@
       implicit none
 
       include 'gen_data_structures.cmn'
+      include 'gep_hist_id.cmn'
 
       logical abort
       character*(*) errmsg
@@ -51,7 +55,30 @@ c$$$               write(*,*) 'h- hit, raw ADC=',gmisc_raw_data(ihit)
 c$$$            endif
 c$$$         endif
 
-        gmisc_dec_data(gmisc_raw_addr2(ihit),gmisc_raw_addr1(ihit)) =
+         if(gmisc_raw_addr1(ihit).eq.1) then
+c$$$            write(*,*) 'gmisc TDC hit ctr,TDCraw=',gmisc_raw_addr2(ihit),
+c$$$     $           gmisc_raw_data(ihit)
+            if(gmisc_raw_addr2(ihit).eq.1) then !HMS1 trigger: fill hist
+               if(gepid_gep_HMS1_rawtdc.gt.0) then
+                  call hf1(gepid_gep_HMS1_rawtdc,float(gmisc_raw_data(ihit)),
+     $                 1.)
+               endif
+            endif
+            if(gmisc_raw_addr2(ihit).eq.2) then !HMS2 trigger: fill hist
+               if(gepid_gep_HMS2_rawtdc.gt.0) then
+                  call hf1(gepid_gep_HMS2_rawtdc,float(gmisc_raw_data(ihit)),
+     $                 1.)
+               endif
+            endif
+            if(gmisc_raw_addr2(ihit).eq.3) then !BigCal trigger: fill hist
+               if(gepid_gep_bigcal_rawtdc.gt.0) then
+                  call hf1(gepid_gep_bigcal_rawtdc,float(gmisc_raw_data(ihit)),
+     $                 1.)
+               endif
+            endif
+         endif
+
+         gmisc_dec_data(gmisc_raw_addr2(ihit),gmisc_raw_addr1(ihit)) =
      $       gmisc_raw_data(ihit)
       enddo
 
