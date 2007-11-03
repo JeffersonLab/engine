e770c2379dbc5e59842c3dbbbc8f864355222e3c
cdaq <>
2007-11-03 05:42:36 +0000

    only read a calib. matrix filename if it is defined by the user

--- ENGINE/b_initialize.f
+++ ENGINE/b_initialize.f
@@ -67,56 +67,62 @@ c     default filename of 'outfiles/bigcal_calib'
       if(bigcal_do_calibration.ne.0.and. .not. abort) then 
          if(b_calib_matrix_filename.ne.' ') then
             filename = b_calib_matrix_filename
-         else 
-            b_calib_matrix_filename = 'DATFILES/bigcal_calib'
-            filename = b_calib_matrix_filename
-         endif
-         call g_IO_control(iochan,'ANY',ABORT,err)
-         if(abort) then
-            call g_add_path(here,err)
-            return 
-         endif
          
-         bigcal_matr_iochan = iochan
-         
-         open(unit=iochan,file=filename,status='old',
-     $        form='unformatted',err=34)
-         
+            call g_IO_control(iochan,'ANY',ABORT,err)
+            if(abort) then
+               call g_add_path(here,err)
+               return 
+            endif
+            
+            bigcal_matr_iochan = iochan
+            
+            open(unit=iochan,file=filename,status='old',
+     $           form='unformatted',err=34)
+            
 c     successful open: read BigCal matrix and number of events:
-         read(iochan,end=35,err=35) bigcal_nmatr_event
-         read(iochan,end=35,err=35) bigcal_vector
-         read(iochan,end=35,err=35) bigcal_matrix
-         
-         write(*,*) 'read old BigCal calibration matrix from '//filename
-         write(*,*) 'Number of events in matrix = ',bigcal_nmatr_event
-         
+            read(iochan,end=35,err=35) bigcal_nmatr_event
+            read(iochan,end=35,err=35) bigcal_vector
+            read(iochan,end=35,err=35) bigcal_matrix
+            
+            write(*,*) 'read old BigCal calibration matrix from '//filename
+            write(*,*) 'Number of events in matrix = ',bigcal_nmatr_event
+            
 c     any read problems will trigger a reset of the calibration 
 c     quantities to zero. 
 c     a successful read means the calib. matrix quantities are 
 c     successfully initialized to their "old" values.
-         
-         goto 36
-         
- 34      write(*,*) filename//' does not exist'//
-     $        ', initializing BigCal calib. matrix to zero'
-         
+            
+            goto 36
+            
+ 34         write(*,*) filename//' does not exist'//
+     $           ', initializing BigCal calib. matrix to zero'
+            
 c     if there is any trouble reading in the values, scrap and 
 c     start over:
-         
- 35      bigcal_nmatr_event = 0
-         do i=1,bigcal_all_maxhits
-            bigcal_vector(i) = 0.
-            do j=1,bigcal_all_maxhits
-               bigcal_matrix(i,j) = 0.
-            enddo
-         enddo  
+            
+ 35         bigcal_nmatr_event = 0
+            do i=1,bigcal_all_maxhits
+               bigcal_vector(i) = 0.
+               do j=1,bigcal_all_maxhits
+                  bigcal_matrix(i,j) = 0.
+               enddo
+            enddo  
 c     shut down the file: 
- 36      call g_IO_control(iochan,'FREE',abort,err)
-         if(abort) then
-            call g_add_path(here,err)
-            return
+ 36         call g_IO_control(iochan,'FREE',abort,err)
+            if(abort) then
+               call g_add_path(here,err)
+               return
+            endif
+            close(iochan)
+         else 
+            bigcal_nmatr_event = 0
+            do i=1,bigcal_all_maxhits
+               bigcal_vector(i) = 0.
+               do j=1,bigcal_all_maxhits
+                  bigcal_matrix(i,j) = 0.
+               enddo
+            enddo  
          endif
-         close(iochan)
       endif
 c     initialize debugging output file:
       if(bdebug_print_adc.ne.0 .or. bdebug_print_tdc.ne.0.or.bdebug_print_trig
