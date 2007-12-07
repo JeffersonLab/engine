5d98b323e6f23179537dd52ab7c5acfba3fd7545
puckett <>
2007-12-07 21:35:29 +0000

    added clearing of some of the newer variables

--- ENGINE/gep_clear_event.f
+++ ENGINE/gep_clear_event.f
@@ -9,7 +9,22 @@
       logical ABORT
       character*(*) err
       
+      integer i
+
       include 'gep_data_structures.cmn'
+
+      ntrigh1 = 0
+      ntrigh2 = 0
+      ntrigb = 0
+
+      do i=1,8
+         gep_h1time(i) = 0.
+         gep_h2time(i) = 0.
+         gep_btime(i) = 0.
+      enddo
+
+      gep_btime_raw = 0.
+      gep_btime_corr = 0.
 
       gep_ctime_hms = 0.
       gep_ctime_cal = 0.
@@ -20,6 +35,14 @@
       gep_P_proton = 0.
       gep_delta_P = 0.
       gep_epsilon = 0.
+      gep_xfp_p = 0.
+      gep_yfp_p = 0.
+      gep_xpfp_p = 0.
+      gep_ypfp_p = 0.
+      gep_xptar_p = 0.
+      gep_yptar_p = 0.
+      gep_ytar_p = 0.
+
       gep_etheta_deg = 0.
       gep_ephi_deg = 0.
       gep_ptheta_deg = 0.
@@ -34,6 +57,8 @@
 
       gep_bx_expect_H = 0.
       gep_by_expect_H = 0.
+      gep_etheta_expect_H = 0.
+      gep_ephi_expect_h = 0.
 
       call gep_ntuple_clear
 
