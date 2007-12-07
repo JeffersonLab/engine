295627facbc1d92a37f506d84885e3900c1583a3
puckett <>
2007-12-07 21:34:56 +0000

    updated timing calculations for BigCal best cluster selection

--- ENGINE/gep_physics.f
+++ ENGINE/gep_physics.f
@@ -8,8 +8,9 @@
 
       logical abort
       character*(*) err
-      
+
       include 'gen_run_info.cmn'
+      include 'gen_event_info.cmn'
       include 'gen_data_structures.cmn'
       include 'hms_data_structures.cmn'
       include 'hms_scin_parms.cmn'
@@ -51,11 +52,13 @@ c      logical fixed_bigcal
       real gamma_corr,beta_corr,tof
       real Q2_cal,Q2_hms,Q2_htheta,nu_htheta,pp_htheta
       real Ee_btheta 
+      real hoffset_ctime,boffset_ctime,htrigt,btrigt,mindiff
 
       real Mp
       parameter(Mp=.938272)
       
       real Me
+
       integer i,ibest_cal
 
       real PI
@@ -92,6 +95,60 @@ c     if the user has not defined something reasonable, then set by hand here:
          return
       endif
 
+c     here we compute the expected cluster time in BigCal (using the focal-plane time for the 
+c     good track):
+
+      hoffset_ctime = hstime_at_fp - hstart_time_center + hspath_cor ! should be around zero for most
+
+      mindiff = 0.
+
+      if(ntrigb.gt.0) then
+         do i=1,ntrigb
+            if(i.eq.1.or.abs(gep_btime(i)-gep_btime_elastic).lt.mindiff) then
+               btrigt = gep_btime(i)
+               mindiff = abs(gep_btime(i)-gep_btime_elastic)
+            endif
+         enddo
+      else
+         btrigt = gep_btime_elastic
+      endif
+
+c     invert (common-stop) hms trigger times using the same user parameter as for BigCal:
+
+      if(ntrigh2.gt.0.and.ntrigh1.eq.0) then 
+         htrigt = bigcal_end_time - gep_h2time(1)
+      else if(ntrigh1.gt.0.and.ntrigh2.eq.0) then
+         htrigt = bigcal_end_time - gep_h1time(1)
+      else if(ntrigh1.gt.0.and.ntrigh2.gt.0) then ! use trig. time 2
+         htrigt = bigcal_end_time - gep_h2time(1)
+      else ! shouldn't hardcode coin. trigger delay, but most data is taken with 
+c     16 ns delay of HMS trigger relative to BigCal trigger. Eventually set up a user param.
+         htrigt = bigcal_end_time - gep_btime_elastic + gep_htrig_delay
+      endif
+      
+c     here are the possible scenarios for the coincidence trigger: 
+c     1. BigCal comes in first by about 16 ns (or gep_htrig_delay). This is most likely for good 
+c     elastics, but
+c     the timing resolution of BigCal is comparatively poor (several ns as opposed to sub-ns 
+c     in the case of the hms scintillators), and the tail of the BigCal trigger time peak has some
+c     overlap with the HMS self-timing peak.
+c     2. the BigCal trigger comes in later than the HMS trigger, and the ADC gate/TDC stop comes 
+c     from the BigCal self-timing peak. 
+c     Now, suppose we have a good elastic event for which the hms trigger self-times and picks out
+c     a track with focal-plane time near hstart_time_center (normal case). Then, we should look for
+c     BigCal clusters coming in 16 ns (gep_htrig_delay) earlier than htrig + hoffset_ctime! 
+c     Now, suppose we have a good elastic event for which the bigcal trigger self-times and the HMS
+c     trigger comes in earlier. IF BigCal self-times, then we also expect the good cluster time to be
+c     in the self-timing peak for BigCal, and an earlier HMS time. But essentially, we should always 
+c     look for the BigCal cluster 16 ns before the HMS focal-plane time, because that is where our 
+c     elastics are:
+
+      tcal_hexpect = htrigt + hoffset_ctime - gep_htrig_delay
+
+c$$$      write(*,*) 'tcal_hexpect=',tcal_hexpect
+c$$$      write(*,*) 'hoffset_ctime=',hoffset_ctime
+c$$$      write(*,*) 'htrigt=',htrigt
+
       Me = mass_electron ! convenient shorthand
 
 c     calculate nu for elastic-ep:
@@ -99,7 +156,6 @@ c     calculate nu for elastic-ep:
       nu = sqrt(Mp**2 + hsp**2) - Mp
 
 c     expected electron energy:
-
       Eprime = gebeam - nu
 
       Ecal_hexpect = Eprime
@@ -200,7 +256,7 @@ c     now rotate into calo-centered coordinate system:
 c      tcal_hexpect= hstime_at_fp - hstart_time_center + hspath_cor
 
 c     for now, just take time difference relative to bigcal_window center
-      tcal_hexpect = bigcal_window_center
+c      tcal_hexpect = bigcal_window_center
       
       if(gen_bigcal_mc.eq.3) then
          tcal_hexpect = 0.0
@@ -236,6 +292,8 @@ c     this routine should only get called once per event!!!!!!!!!!!!!!!!
       ibest_cal = pick_best_cal_track(tcal_hexpect,gep_etheta_expect_h,
      $     gep_ephi_expect_h,gep_bx_expect_h,gep_by_expect_h,Ecal_hexpect)
 
+      
+
 
 c     now compute "missing" quantities using the track we have selected.
 c     first correct best calo track for vertex information which we now know from HMS reconstruction:
@@ -267,8 +325,9 @@ c     correct tof:
 c     could get a "NaN" error here: check:
       if(gamma_corr.lt.1.) gamma_corr = 1.
 
-      beta_corr = sqrt(1.-1./gamma_corr**2)
+      beta_corr = sqrt(max(0.,1.-1./gamma_corr**2))
 
+      if(beta_corr.eq.0.) beta_corr = 1.
       bigcal_beta = beta_corr
       bigcal_tof = epathlength / (beta_corr*speed_of_light)
       
@@ -280,7 +339,9 @@ c     could get a "NaN" error here: check:
 
       bigcal_eloss = bigcal_track_eloss(ibest_cal)
       bigcal_time = bigcal_track_time(ibest_cal)
-      bigcal_ctime = bigcal_track_time(ibest_cal) - bigcal_tof
+      bigcal_tof_cor = bigcal_tof - bigcal_tof_central
+      bigcal_ctime = bigcal_track_time(ibest_cal) - bigcal_tof_cor - 
+     $     (bigcal_end_time - gep_btime_elastic)
 
       gep_ctime_hms = hstime_at_fp - hstart_time_center + hspath_cor
       gep_ctime_cal = bigcal_ctime
@@ -364,7 +425,7 @@ c     GEP_Q2 = .5*(Q2_cal + Q2_hms)
             PH_cal = bigcal_track_phirad(itrack) + PI/2.
 
 
-            T_cal = bigcal_track_time(itrack)
+            T_cal = bigcal_track_time(itrack) - bigcal_track_tof_cor(itrack)
             X_cal = bigcal_all_clstr_x(itrack)
             Y_cal = bigcal_all_clstr_y(itrack)
 
@@ -386,7 +447,7 @@ c     GEP_Q2 = .5*(Q2_cal + Q2_hms)
                endif
             endif
 
-            bigcal_all_clstr_chi2(itrack) = diffsum
+            bigcal_all_clstr_chi2(itrack) = diffsum/6.
             bigcal_all_clstr_chi2contr(itrack,1) = ( (E_cal - E_H)/GEP_sigma_Ediff )**2
             bigcal_all_clstr_chi2contr(itrack,2) = ( (TH_cal - TH_H)/GEP_sigma_thdiff )**2
             bigcal_all_clstr_chi2contr(itrack,3) = ( (PH_cal - PH_H)/GEP_sigma_phdiff )**2
