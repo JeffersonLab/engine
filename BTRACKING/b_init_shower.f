80c623a05b269c9635ddacd29e9fc948b0673baf
puckett <>
2008-02-11 21:17:09 +0000

    added initialization of "shower map" parameters for lookup-table based position reconstruction

--- BTRACKING/b_init_shower.f
+++ BTRACKING/b_init_shower.f
@@ -9,7 +9,7 @@
       logical ABORT
       character*(*) err
 
-      integer irow,icol,ipar,icell
+      integer irow,icol,ipar,icell,j
 
       include 'bigcal_data_structures.cmn'
       include 'bigcal_shower_parms.cmn'
@@ -61,6 +61,88 @@ c$$$      write(*,*) 'prot_xpar = ',bigcal_prot_xpar
 c$$$      write(*,*) 'prot_ypar = ',bigcal_prot_ypar
 c$$$      write(*,*) 'rcs_xpar = ',bigcal_rcs_xpar
 c$$$      write(*,*) 'rcs_ypar = ',bigcal_rcs_ypar
+
+      bigcal_pxmap_nbin(1) = bigcal_px1map_nbin
+      bigcal_pxmap_nbin(2) = bigcal_px2map_nbin
+      bigcal_pxmap_nbin(3) = bigcal_px3map_nbin
+      bigcal_pxmap_nbin(4) = bigcal_px4map_nbin
+
+      bigcal_pymap_nbin(1) = bigcal_py1map_nbin
+      bigcal_pymap_nbin(2) = bigcal_py2map_nbin
+      bigcal_pymap_nbin(3) = bigcal_py3map_nbin
+      bigcal_pymap_nbin(4) = bigcal_py4map_nbin
+
+      bigcal_rxmap_nbin(1) = bigcal_rx1map_nbin
+      bigcal_rxmap_nbin(2) = bigcal_rx2map_nbin
+      bigcal_rxmap_nbin(3) = bigcal_rx3map_nbin
+      bigcal_rxmap_nbin(4) = bigcal_rx4map_nbin
+
+      bigcal_rymap_nbin(1) = bigcal_ry1map_nbin
+      bigcal_rymap_nbin(2) = bigcal_ry2map_nbin
+      bigcal_rymap_nbin(3) = bigcal_ry3map_nbin
+c      bigcal_rymap_nbin(4) = bigcal_ry4map_nbin
+
+      bigcal_pxmap_mmin(1) = bigcal_px1map_xmom_min
+      bigcal_pxmap_mmin(2) = bigcal_px2map_xmom_min
+      bigcal_pxmap_mmin(3) = bigcal_px3map_xmom_min
+      bigcal_pxmap_mmin(4) = bigcal_px4map_xmom_min
+
+      bigcal_pymap_mmin(1) = bigcal_py1map_xmom_min
+      bigcal_pymap_mmin(2) = bigcal_py2map_xmom_min
+      bigcal_pymap_mmin(3) = bigcal_py3map_xmom_min
+      bigcal_pymap_mmin(4) = bigcal_py4map_xmom_min
+
+      bigcal_rxmap_mmin(1) = bigcal_rx1map_xmom_min
+      bigcal_rxmap_mmin(2) = bigcal_rx2map_xmom_min
+      bigcal_rxmap_mmin(3) = bigcal_rx3map_xmom_min
+      bigcal_rxmap_mmin(4) = bigcal_rx4map_xmom_min
+
+      bigcal_rymap_mmin(1) = bigcal_ry1map_xmom_min
+      bigcal_rymap_mmin(2) = bigcal_ry2map_xmom_min
+      bigcal_rymap_mmin(3) = bigcal_ry3map_xmom_min
+
+
+
+      bigcal_pxmap_mmax(1) = bigcal_px1map_xmom_max
+      bigcal_pxmap_mmax(2) = bigcal_px2map_xmom_max
+      bigcal_pxmap_mmax(3) = bigcal_px3map_xmom_max
+      bigcal_pxmap_mmax(4) = bigcal_px4map_xmom_max
+
+      bigcal_pymap_mmax(1) = bigcal_py1map_xmom_max
+      bigcal_pymap_mmax(2) = bigcal_py2map_xmom_max
+      bigcal_pymap_mmax(3) = bigcal_py3map_xmom_max
+      bigcal_pymap_mmax(4) = bigcal_py4map_xmom_max
+
+      bigcal_rxmap_mmax(1) = bigcal_rx1map_xmom_max
+      bigcal_rxmap_mmax(2) = bigcal_rx2map_xmom_max
+      bigcal_rxmap_mmax(3) = bigcal_rx3map_xmom_max
+      bigcal_rxmap_mmax(4) = bigcal_rx4map_xmom_max
+
+      bigcal_rymap_mmax(1) = bigcal_ry1map_xmom_max
+      bigcal_rymap_mmax(2) = bigcal_ry2map_xmom_max
+      bigcal_rymap_mmax(3) = bigcal_ry3map_xmom_max
+
+      
+      do j=1,bigcal_map_maxbins
+         bigcal_pxmap_frac(1,j) = bigcal_px1map_xmom_frac(j)
+         bigcal_pxmap_frac(2,j) = bigcal_px2map_xmom_frac(j)
+         bigcal_pxmap_frac(3,j) = bigcal_px3map_xmom_frac(j)
+         bigcal_pxmap_frac(4,j) = bigcal_px4map_xmom_frac(j)
+         
+         bigcal_pymap_frac(1,j) = bigcal_py1map_xmom_frac(j)
+         bigcal_pymap_frac(2,j) = bigcal_py2map_xmom_frac(j)
+         bigcal_pymap_frac(3,j) = bigcal_py3map_xmom_frac(j)
+         bigcal_pymap_frac(4,j) = bigcal_py4map_xmom_frac(j)
+         
+         bigcal_rxmap_frac(1,j) = bigcal_rx1map_xmom_frac(j)
+         bigcal_rxmap_frac(2,j) = bigcal_rx2map_xmom_frac(j)
+         bigcal_rxmap_frac(3,j) = bigcal_rx3map_xmom_frac(j)
+         bigcal_rxmap_frac(4,j) = bigcal_rx4map_xmom_frac(j)
+         
+         bigcal_rymap_frac(1,j) = bigcal_ry1map_xmom_frac(j)
+         bigcal_rymap_frac(2,j) = bigcal_ry2map_xmom_frac(j)
+         bigcal_rymap_frac(3,j) = bigcal_ry3map_xmom_frac(j)
+      enddo
 
       return 
       end
