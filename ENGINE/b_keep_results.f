6c83d159f79ed1315558969902ed6630cd95d0c4
puckett <>
2008-07-29 16:22:24 +0000

    require at least one "good" cluster to keep event in ntuple

--- ENGINE/b_keep_results.f
+++ ENGINE/b_keep_results.f
@@ -7,6 +7,7 @@
       include 'b_ntuple.cmn'
       include 'gen_event_info.cmn'
       INCLUDE 'bigcal_bypass_switches.cmn'
+      include 'gep_data_structures.cmn'
 
       character*14 here
       parameter(here='b_keep_results')
@@ -27,16 +28,16 @@ c$$$      endif
 
       if (gen_event_type .le. 2) return
 
-      if ( .not.  b_passed_cointime_cut) return
+      if (.not.(b_passed_cointime_cut.and.h_passed_cointime_cut ))return
 
       if(bigcal_ntuple_type.eq.1)then
-        if(bigcal_all_nclstr.gt.0.or.bigcal_nmaxima.gt.0) then
+        if(bigcal_all_nclust_good.gt.0) then
           call b_ntuple_keep(ABORT,err)
         endif
       else if(bigcal_ntuple_type.eq.2)then 
          call b_ntuple_keep(ABORT,err)
       else if(bigcal_ntuple_type.eq.3) then
-         if(bigcal_all_nclstr.gt.0) call b_ntuple_keep(ABORT,err)
+         if(bigcal_all_nclust_good.gt.0) call b_ntuple_keep(ABORT,err)
       endif
       
       if(abort) then
