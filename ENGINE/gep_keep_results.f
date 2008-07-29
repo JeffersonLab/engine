5ac7fd39230d435dec8bde8dc25c2072c7baeee3
puckett <>
2008-07-29 16:12:08 +0000

    added HMS cointime cut to "keep" event criteria

--- ENGINE/gep_keep_results.f
+++ ENGINE/gep_keep_results.f
@@ -18,7 +18,8 @@ c      include 'gen_event_info.cmn'
       abort=.false.
       err=' '
 
-      if(HSNUM_FPTRACK.gt.0 .and. BIGCAL_PHYS_NTRACK .gt. 0 .and. b_passed_cointime_cut) then
+      if(HSNUM_FPTRACK.gt.0 .and. BIGCAL_PHYS_NTRACK .gt. 0 .and. 
+     $     b_passed_cointime_cut.and.h_passed_cointime_cut) then
 c         gep_evid = gen_event_id_number
          call gep_ntuple_keep(abort,err)
          call gep_fill_hist(abort,err)
