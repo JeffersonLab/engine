ed2d0071008b581bb9fb47a39c892853b1c5f925
puckett <>
2008-10-09 15:52:49 +0000

    added best track selection method, choose one track in each FPP

---
+++ HTRACKING/h_fpp_select_best_track_prune.f
@@ -0,0 +1,194 @@
+      subroutine h_fpp_select_best_track_prune(abort,err)
+
+      implicit none
+      save
+
+      character*29 here
+      parameter(here='h_fpp_select_best_track_prune')
+
+      logical abort 
+      character*(*) err
+
+      include 'hms_data_structures.cmn'
+      include 'gen_detectorids.par'
+      include 'gen_decode_common.cmn'
+      include 'hms_fpp_event.cmn'
+      include 'hms_geometry.cmn'
+
+      integer ifpp,itrack ! = 1, 2 for FPP1, FPP2
+      integer ngood(2)
+      integer besttrack(2)
+
+      real zslop ! theta-dependent tolerance parameter for zclose
+
+      real*4 chi2,minchi2(2)
+      real*4 criterion,mincriterion(2),scloseweight(2)
+      logical firsttry
+
+      logical keep(2,h_fpp_max_tracks)
+
+c     what prune tests should we include? 
+c     definitely sclose, but also zclose:
+c     prune_thetamin and prune_thetamax:
+c     if multiple tracks pass prune tests, 
+c     use Sitnik's selection: select on minimum of 
+c     chi2(HMS) + chi2(FPP) + weight * sclose^2,
+c     where weight = chi2(HMS) + chi2best(FPP)
+
+c     first step: initialize keep on all tracks to true:
+c     while we're at it, find the track with minimum chi2 in each FPP:
+
+      do ifpp=1,2
+         firsttry=.true.
+         do itrack=1,hfpp_n_tracks(ifpp)
+            keep(ifpp,itrack) = .true.
+
+            chi2 = hfpp_track_chi2(ifpp,itrack)
+            if(firsttry.or.chi2.lt.minchi2(ifpp)) then
+               minchi2(ifpp) = chi2
+               firsttry = .false.
+            endif
+
+         enddo
+      enddo
+c     zeroth prune test = conetest:
+      do ifpp=1,2
+         ngood(ifpp) = 0
+         do itrack=1,hfpp_n_tracks(ifpp)
+            if(hfpp_track_conetest(ifpp,itrack).eq.1.and.
+     $           keep(ifpp,itrack)) then
+               ngood(ifpp) = ngood(ifpp) + 1
+            endif
+         enddo
+         
+         if(ngood(ifpp).gt.0) then
+            do itrack=1,hfpp_n_tracks(ifpp)
+               if(hfpp_track_conetest(ifpp,itrack).eq.0) then
+                  keep(ifpp,itrack) = .false.
+               endif
+            enddo
+         endif
+      enddo
+c     first prune tests: prune separately on minimum and maximum 
+c     polar scattering angle theta>thetamin:
+
+      do ifpp=1,2
+         ngood(ifpp) = 0
+         do itrack=1,hfpp_n_tracks(ifpp)
+            if(hfpp_track_theta(ifpp,itrack).ge.hfpp_prune_thetamin(ifpp)
+     $           .and.keep(ifpp,itrack))then
+               ngood(ifpp) = ngood(ifpp) + 1
+            endif
+         enddo
+
+         if(ngood(ifpp).gt.0) then
+            do itrack=1,hfpp_n_tracks(ifpp)
+               if(hfpp_track_theta(ifpp,itrack).lt.hfpp_prune_thetamin(ifpp)) 
+     $              then
+                  keep(ifpp,itrack) = .false.
+               endif
+            enddo
+         endif
+      enddo
+
+c     second test: polar scattering angle theta<thetamax:
+      
+      do ifpp=1,2
+         ngood(ifpp) = 0
+         do itrack=1,hfpp_n_tracks(ifpp)
+            if(keep(ifpp,itrack).and.hfpp_track_theta(ifpp,itrack).le.
+     $           hfpp_prune_thetamax(ifpp)) then
+               ngood(ifpp) = ngood(ifpp) + 1
+            endif
+         enddo
+         
+         if(ngood(ifpp).gt.0) then
+            do itrack=1,hfpp_n_tracks(ifpp)
+               if(hfpp_track_theta(ifpp,itrack).gt.hfpp_prune_thetamax(ifpp))
+     $              then
+                  keep(ifpp,itrack) = .false.
+               endif
+            enddo
+         endif
+      enddo
+      
+c     third prune test: sclose = distance of closest approach between two tracks:
+      
+      do ifpp=1,2
+         ngood(ifpp) = 0
+         do itrack=1,hfpp_n_tracks(ifpp)
+            if(keep(ifpp,itrack).and.hfpp_track_sclose(ifpp,itrack).le.
+     $           hfpp_prune_sclose(ifpp)) then
+               ngood(ifpp) = ngood(ifpp) + 1
+            endif
+         enddo
+         
+         if(ngood(ifpp).gt.0) then
+            do itrack=1,hfpp_n_tracks(ifpp)
+               if(hfpp_track_sclose(ifpp,itrack).gt.
+     $              hfpp_prune_sclose(ifpp)) then
+                  keep(ifpp,itrack) = .false.
+               endif
+            enddo
+         endif
+      enddo
+
+c     fourth prune test: zclose = z of point of closest approach between the two tracks:
+c     ideally want zclose within the analyzer. However, we don't measure zclose very well at 
+c     small values of theta. zclose resolution diverges as tan^-1 theta at small theta
+c     We would like to have a "slop" parameter for zclose that 
+c     allows for zclose values well outside the analyzer in the situation that theta is small:
+
+      do ifpp=1,2
+         ngood(ifpp) = 0
+         do itrack=1,hfpp_n_tracks(ifpp)
+            zslop = hfpp_prune_zslop(ifpp) * 
+     $           max(1.0,min(1000.0,1.0/tan(hfpp_track_theta(ifpp,itrack))))
+            if(keep(ifpp,itrack).and.hfpp_track_zclose(ifpp,itrack).ge.
+     $           hfpp_prune_zclose(ifpp,1)-zslop.and.
+     $           hfpp_track_zclose(ifpp,itrack).le.
+     $           hfpp_prune_zclose(ifpp,2)+zslop) then
+               ngood(ifpp) = ngood(ifpp) + 1
+            endif
+         enddo
+
+         if(ngood(ifpp).gt.0) then
+            do itrack=1,hfpp_n_tracks(ifpp)
+               zslop = hfpp_prune_zslop(ifpp) * 
+     $              max(1.0,min(1000.0,1.0/tan(hfpp_track_theta(ifpp,itrack))))
+               if(hfpp_track_zclose(ifpp,itrack).lt.
+     $              hfpp_prune_zclose(ifpp,1)-zslop.or.
+     $              hfpp_track_zclose(ifpp,itrack).gt.
+     $              hfpp_prune_zclose(ifpp,2)+zslop) then
+                  keep(ifpp,itrack) = .false.
+               endif
+            enddo
+         endif
+      enddo
+      
+c     now we choose the best track based on Sitnik's criterion:
+
+      do ifpp=1,2
+         firsttry=.true.
+         scloseweight(ifpp) = minchi2(ifpp) + hschi2perdeg
+
+         besttrack(ifpp) = 0
+
+         do itrack=1,hfpp_n_tracks(ifpp)
+            criterion = hschi2perdeg + hfpp_track_chi2(ifpp,itrack) + 
+     $           scloseweight(ifpp) * (hfpp_track_sclose(ifpp,itrack))**2
+            if(firsttry.or.criterion.lt.mincriterion(ifpp)) then
+               mincriterion(ifpp) = criterion
+               firsttry = .false.
+               besttrack(ifpp) = itrack
+            endif
+         enddo
+         
+         hfpp_best_track(ifpp) = besttrack(ifpp)
+      enddo
+
+      abort = .false.
+      err=''
+
+      return
+      end
