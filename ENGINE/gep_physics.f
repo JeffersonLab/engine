3e89c0d7dca4db0045e3f447b05cdbdcdc1854fc
jones <>
2009-09-15 20:30:48 +0000

    Modify code to track elastic electron back to BigCal

--- ENGINE/gep_physics.f
+++ ENGINE/gep_physics.f
@@ -57,6 +57,11 @@ c      logical fixed_bigcal
       real nu_btheta
       real Ee_btheta 
       real hoffset_ctime,boffset_ctime,htrigt,btrigt,mindiff
+
+      real dx_ang,dy_ang,func1_mom,func2_mom,func4_x,func3_y,func5_mom 
+      real func1_dx,func2_dx,func3_dx,func4_dx,func5_x,func5_y
+      real*8 theta_angle_diff,phi_angle_diff
+      
 cc
 c
 c     Implementetion of the magnetic field for electron tracking to BIGCAL
@@ -64,7 +69,7 @@ c
 cc
       REAL*8 TARGET_COORD(6),Eprot,Pprot
       COMMON/TARGET_GENRECON/TARGET_COORD,Eprot,Pprot
-      real*8 u(6),dl,temp
+      real*8 u(6),dl,temp,zfinal
       real*8 Eb,theta_big, phi_big!,ccx,ccy,ccz
       common/FAKEBIG/Eb,theta_big, phi_big
 c      real*8 P1_bigcal(3),P2_bigcal(3),P3_bigcal(3)
@@ -270,9 +275,41 @@ c     so we are calculating the intersection point of the e- trajectory expected
 
 c     now rotate into calo-centered coordinate system:
 
-      xcal_hexpect=xint_hexpect*bigcal_costheta-zint_hexpect*bigcal_sintheta
-      ycal_hexpect=yint_hexpect
+cc      xcal_hexpect=xint_hexpect*bigcal_costheta-zint_hexpect*bigcal_sintheta
+cc      ycal_hexpect=yint_hexpect
 c      tcal_hexpect= hstime_at_fp - hstart_time_center + hspath_cor
+
+********** use parameter correlated functions insted of tgd field correction subtoutines *************************
+      xcal_hexpect_B0=xint_hexpect*bigcal_costheta-zint_hexpect*bigcal_sintheta
+      ycal_hexpect_B0=yint_hexpect
+
+      dy_ang=ycal_hexpect_B0/bigcal_r_tgt
+      dx_ang=xcal_hexpect_B0/bigcal_r_tgt
+    
+      EprimeMeV = Eprime*1000
+      func1_mom =-15.358+(0.0122386*(Eprime*1000))+(-3.57333e-6*(Eprime*1000)**2)+(3.63059e-10*(Eprime*1000)*(Eprime*1000)**2)
+      func2_mom =-78.556+(0.0340849*(Eprime*1000))+(-5.45359e-6*(Eprime*1000)**2)+(1.87087e-10*(Eprime*1000)*(Eprime*1000)**2)
+      func3_y   = -0.03322 + (1.19167*vy)
+      func4_x   = 0.00001 + (0.09876*vx)
+cc      func5_mom = -6.5252+(0.0015859*(Eprime*1000))+(0.53662e-6*(Eprime*1000)**2)+(-0.14983e-9*(Eprime*1000)*(Eprime*1000)**2) 
+cc      xdiff_shift=func1_mom + (func2_mom*dy_ang) + func3_y + func4_x
+      xdiff_shift=func1_mom + (func2_mom*dy_ang) + func3_y + func4_x 
+
+      func1_dx = -108.84+(163.98*dx_ang)+(-135.00*dx_ang**2) 
+      func2_dx = 0.079489+(-0.12047*dx_ang)+(0.13244*dx_ang**2)
+      func3_dx = -0.24567e-4+(0.37428e-4*dx_ang)+(-0.51771e-4*dx_ang**2)
+      func4_dx = 2.7249e-9+(-0.41655e-8*dx_ang)+(0.68318e-8*dx_ang**2)
+cc      func5_x  = 0.01204+(-0.39499*vx)
+cc      func5_y  = 0.00906+(0.00070*vy)
+cc      ydiff_shift = (func1_dx+(func2_dx*(Eprime*1000))+(func3_dx*(Eprime*1000)*(Eprime*1000))+(func4_dx*(Eprime*1000)
+cc     &     *(Eprime*1000)*(Eprime*1000))+func5_x - func5_y)
+      ydiff_shift = (func1_dx+(func2_dx*(Eprime*1000))+(func3_dx*(Eprime*1000)*(Eprime*1000))+(func4_dx*(Eprime*1000)
+     &     *(Eprime*1000)*(Eprime*1000)))
+
+c      xcal_hexpect =xcal_hexpect_B0- xdiff_shift
+c      ycal_hexpect =ycal_hexpect_B0+ ydiff_shift
+      
+***********************************************************************************************************************
 
 c     for now, just take time difference relative to bigcal_window center
 c      tcal_hexpect = bigcal_window_center
@@ -289,82 +326,85 @@ c     Implementetion of the magnetic field for electron tracking to BIGCAL
 c
 cc
       if(SANE_TGTFIELD_B.gt.0)then
-      if(a_bigcal.eq.0.and.b_bigcal.eq.0.and.c_bigcal.eq.0)then
+c      if(a_bigcal.eq.0.and.b_bigcal.eq.0.and.c_bigcal.eq.0)then
 c     
 c     Define Bigcal plane
 c     
-               P1_bigcal(1) = 0 
-               P1_bigcal(2) = 0 
-               P1_bigcal(3) = Bigcal_SHIFT(3)
-               P2_bigcal(1) = 0 
-               P2_bigcal(2) = 1 
-               P2_bigcal(3) = Bigcal_SHIFT(3)
-               P3_bigcal(1) = 1 
-               P3_bigcal(2) = 0 
-               P3_bigcal(3) =  Bigcal_SHIFT(3)
+c               P1_bigcal(1) = 0 
+c               P1_bigcal(2) = 0 
+c               P1_bigcal(3) = Bigcal_SHIFT(3)
+c               P2_bigcal(1) = 0 
+c               P2_bigcal(2) = 1 
+c               P2_bigcal(3) = Bigcal_SHIFT(3)
+c               P3_bigcal(1) = 1 
+c               P3_bigcal(2) = 0 
+c               P3_bigcal(3) =  Bigcal_SHIFT(3)
 
 
-               call ROTATE(P1_bigcal, 0., -Bigcal_SHIFT(4)*3.141/180., 0. ,P1_bigcal_r)
-               call ROTATE(P2_bigcal, 0., -Bigcal_SHIFT(4)*3.141/180., 0. ,P2_bigcal_r)
-               call ROTATE(P3_bigcal, 0., -Bigcal_SHIFT(4)*3.141/180., 0. ,P3_bigcal_r)
-               temp           = P1_bigcal_r(2)
-               P1_bigcal_r(2) = P1_bigcal_r(1)
-               P1_bigcal_r(1) = temp
-               temp           = P2_bigcal_r(2)
-               P2_bigcal_r(2) = P2_bigcal_r(1)
-               P2_bigcal_r(1) = temp
-               temp           = P3_bigcal_r(2)
-               P3_bigcal_r(2) = P3_bigcal_r(1)
-               P3_bigcal_r(1) = temp
-               call Plane(P1_bigcal_r,P2_bigcal_r,P3_bigcal_r,
-     ,              a_bigcal,b_bigcal,c_bigcal,d_bigcal)
+c               call ROTATE(P1_bigcal, 0., -Bigcal_SHIFT(4)*3.141/180., 0. ,P1_bigcal_r)
+c               call ROTATE(P2_bigcal, 0., -Bigcal_SHIFT(4)*3.141/180., 0. ,P2_bigcal_r)
+c               call ROTATE(P3_bigcal, 0., -Bigcal_SHIFT(4)*3.141/180., 0. ,P3_bigcal_r)
+c               temp           = P1_bigcal_r(2)
+c               P1_bigcal_r(2) = P1_bigcal_r(1)
+c               P1_bigcal_r(1) = temp
+c               temp           = P2_bigcal_r(2)
+c               P2_bigcal_r(2) = P2_bigcal_r(1)
+c               P2_bigcal_r(1) = temp
+c               temp           = P3_bigcal_r(2)
+c               P3_bigcal_r(2) = P3_bigcal_r(1)
+c               P3_bigcal_r(1) = temp
+c               call Plane(P1_bigcal_r,P2_bigcal_r,P3_bigcal_r,
+c     ,              a_bigcal,b_bigcal,c_bigcal,d_bigcal)
 c               write(*,*)P1_bigcal_r,P2_bigcal_r,P3_bigcal_r
+c      endif
+      theta_angle_diff=abs(SANE_BETA_ANGLE_THETA-SANE_FIELD_ANGLE_THETA)
+      SANE_BETA_ANGLE_PHI = 0
+      SANE_FIELD_ANGLE_PHI = 0
+      if (SANE_BETA_ANGLE_PHI .eq. SANE_FIELD_ANGLE_PHI) then
+        if (SANE_BETA_ANGLE_THETA .ge. SANE_FIELD_ANGLE_THETA) phi_angle_diff=0.
+        if (SANE_BETA_ANGLE_THETA .lt. SANE_FIELD_ANGLE_THETA) phi_angle_diff=180.
       endif
-      CALL trgInitFieldANGLES(SANE_BETA_OMEGA,SANE_BETA_PHI)
+      if (SANE_FIELD_ANGLE_THETA .eq. 0 ) then
+        if (SANE_BETA_ANGLE_PHI .eq. 0 ) phi_angle_diff=0.
+        if (SANE_BETA_ANGLE_PHI .eq. 180. ) phi_angle_diff=180.
+      endif
+      if (SANE_FIELD_ANGLE_THETA .eq. 180 ) then
+        if (SANE_BETA_ANGLE_PHI .eq. 0 ) phi_angle_diff=180.
+        if (SANE_BETA_ANGLE_PHI .eq. 180. ) phi_angle_diff=0.
+      endif
+      theta_angle_diff=40.0d00
+      phi_angle_diff=180.0d00
+c use +x vertical down, +y large angle, +z towards beta for tracking thru field
+      SANE_FIELD_THETA =theta_angle_diff 
+      SANE_FIELD_PHI =phi_angle_diff
+      CALL trgInitFieldANGLES(theta_angle_diff,phi_angle_diff)
          
       Eb   =  -Eprime*1000.
-      U(1) =  TARGET_COORD(1)
-      U(2) =  TARGET_COORD(2)
-      U(3) =  TARGET_COORD(3)
-c      U(4) =  -TARGET_COORD(4)
-c      U(5) =  -TARGET_COORD(5)
-c      U(6) =  29.979-TARGET_COORD(6)
-
-c      write(*,*)TARGET_COORD,Pprot,eprot
-c      write(*,*)etheta_expect*57.3,hsphi*57.3-270+180,ephi_expect*57.3,(ephi_expect-pi)*57.3
+      U(1) =  0 ! vertical position (-y_beta)  cm 
+      U(2) =  0 ! horizontal position (+x_beta) cm in Beta coodinates
+      U(3) =  0 ! z position (z_beta) cm in Beta coodinates
       theta_big = etheta_expect
-c      write(*,*)'1 ',etheta_expect*57.3
-ccc Lets ty this
-c      theta_big = -etheta_expect+2*SANE_HMS_ANGLE_THETA*3.1415/180.
-c      write(*,*)'2 ',theta_big*57.3
       phi_big   = (ephi_expect)
-      U(4) =  sin(theta_big)*cos(phi_big)*29.979
-      U(5) =  sin(theta_big)*sin(phi_big)*29.979
-      U(6) =  cos(theta_big)*29.979
-      dl   =  0.1
+      dy_ang=-ycal_hexpect_B0/bigcal_r_tgt
+      dx_ang=xcal_hexpect_B0/bigcal_r_tgt
+      U(6) =  29.979/sqrt(1+dy_ang**2+dx_ang**2)
+      U(4) =  dy_ang*u(6)  ! dy_beta/dz_beta*(speed of light) cm/ns
+      U(5) =  dx_ang*u(6) ! dx_beta/dz_beta*(speed of light) cm/ns
+      dl   =  1.0
       ok   = .TRUE. 
-c      write(*,*)SANE_BETA_OMEGA,SANE_BETA_PHI
-c      write(*,*)a_bigcal,b_bigcal,c_bigcal,d_bigcal
-c      write(*,*)'Angles ',atan2()
-c      write(*,*)'At Target ',U
+c  give trgTrackToPlane the starting positions and angles in U vector and it
+c  returns the positions and angles at the plane 
+c      write(*,*) ' call trackto plane'
+      zfinal=-bigcal_r_tgt
       call  trgTrackToPlane(U,Eb,dl,
-     ,     a_bigcal,b_bigcal,c_bigcal,d_bigcal,ok)
-      p1_bigcal_r(1) =  u(2)
-      p1_bigcal_r(2) =  -u(1)
-      p1_bigcal_r(3) =  u(3)
-      call ROTATE( P1_bigcal_r , 0., Bigcal_SHIFT(4)*3.141/180., 0. , P1_bigcal )
-c     
-c      write(*,*)'At BIGCAL ',U
-c      write(*,*)pthetarad*180/3.141,(hsphi - 3.*PI/2.)*180/3.141,etheta_expect*180/3.141,ephi_expect*180/3.141
-
-c      if(abs(P1_bigcal(2)).lt.120.and.abs(P1_bigcal(1)).lt.120)then
-c         write(*,*)'At BIGCAL ',P1_bigcal
-c         write(*,*)acos(ezhat_tar)*180/3.141,atan2(u(5)/29.97,u(4)/29.97)*180/3.141
-c      endif
-      
-ccccccccccccccccccccccc
-      gep_bx_expect_H = P1_bigcal(1)
-      gep_by_expect_H = P1_bigcal(2)
+     ,     0.d00,0.d00,1.d00,zfinal,ok)
+c      write(*,*) 'retrun  call trackto plane'
+      gep_bx_expect_H = u(2)
+      gep_by_expect_H = -u(1)
+c      if ( eprime .gt. 1.0 .and. abs((gep_p_proton - gep_pel_htheta) / hpcentral +0.01) .le. .02) then
+c       write(*,*) ' positions = ',xcal_hexpect_B0,ycal_hexpect_B0,gep_bx_expect_H,gep_by_expect_H,dx_ang,dy_ang
+c       write(*,*) eb,zfinal,' U = ',u
+c       endif
       else
          gep_bx_expect_H = xcal_hexpect
          gep_by_expect_H = ycal_hexpect
@@ -376,15 +416,15 @@ c     how to choose? pick the track for which the quadrature sum of
 c     sum( ((Eclust-Eexpect)/sigma)**2 + ((xclust-xexpect)/sigma)**2 + ((yclust-yexpect)/sigma)**2 ) is minimum
 c     the resolution parameters sigma should be CTP parms
 
-      if(b_use_bad_chan_list.ne.0) then
+c      if(b_use_bad_chan_list.ne.0) then
 c     check bigcal clusters, and, if necessary, fix clusters near the 
 c     expected electron position containing channels in the "bad" list
 
 c         fixed_bigcal = .false.
 *     fixed_bigcal will be true if any channels from the bad channel list are 
 *     filled with a guess and if any cluster is rebuilt or a new cluster is found
-         call gep_check_bigcal(gep_bx_expect_H,gep_by_expect_H,Eprime)
-      endif
+c         call gep_check_bigcal(gep_bx_expect_H,gep_by_expect_H,Eprime)
+c      endif
 
       call b_calc_physics(abort,err) ! reconstruct physics quantities for BigCal. 
 c     this routine should only get called once per event!!!!!!!!!!!!!!!!
@@ -392,10 +432,10 @@ c     this routine should only get called once per event!!!!!!!!!!!!!!!!
 *     now pick up where we left off:
 
 
-      ibest_cal = pick_best_cal_track(tcal_hexpect,gep_etheta_expect_h,
-     $     gep_ephi_expect_h,gep_bx_expect_h,gep_by_expect_h,Ecal_hexpect)
+c      ibest_cal = pick_best_cal_track(tcal_hexpect,gep_etheta_expect_h,
+c     $     gep_ephi_expect_h,gep_bx_expect_h,gep_by_expect_h,Ecal_hexpect)
 
-      
+       ibest_cal = 1     
 
 
 c     now compute "missing" quantities using the track we have selected.
@@ -556,11 +596,11 @@ c     GEP_Q2 = .5*(Q2_cal + Q2_hms)
             
             bigcal_all_clstr_chi2(itrack) = diffsum/6.
             bigcal_all_clstr_chi2contr(itrack,1) = ( (E_cal - E_H)/GEP_sigma_Ediff )**2
-            bigcal_all_clstr_chi2contr(itrack,2) = ( (TH_cal - TH_H)/GEP_sigma_thdiff )**2
-            bigcal_all_clstr_chi2contr(itrack,3) = ( (PH_cal - PH_H)/GEP_sigma_phdiff )**2
-            bigcal_all_clstr_chi2contr(itrack,4) = ( (X_cal - X_H)/GEP_sigma_xdiff )**2
-            bigcal_all_clstr_chi2contr(itrack,5) = ( (Y_cal - Y_H)/GEP_sigma_ydiff )**2
-            bigcal_all_clstr_chi2contr(itrack,6) = ( (T_cal - T_H)/GEP_sigma_Tdiff )**2
+c            bigcal_all_clstr_chi2contr(itrack,2) = ( (TH_cal - TH_H)/GEP_sigma_thdiff )**2
+c            bigcal_all_clstr_chi2contr(itrack,3) = ( (PH_cal - PH_H)/GEP_sigma_phdiff )**2
+c            bigcal_all_clstr_chi2contr(itrack,4) = ( (X_cal - X_H)/GEP_sigma_xdiff )**2
+c            bigcal_all_clstr_chi2contr(itrack,5) = ( (Y_cal - Y_H)/GEP_sigma_ydiff )**2
+c            bigcal_all_clstr_chi2contr(itrack,6) = ( (T_cal - T_H)/GEP_sigma_Tdiff )**2
             
             if(restore_E) E_cal = E_cal * 1000.
             
