9a6ee7afc85d866b27ed72fbc82d220f06de12be
jones <>
2011-03-03 20:09:01 +0000

    Add check for 64bit by looking for LP64

--- CTP/thClient.c
+++ CTP/thClient.c
@@ -1,4 +1,4 @@
-#ifdef __osf__
+#if defined(__osf__) || defined(__LP64__)
 #define BIT64
 #endif
 /*-----------------------------------------------------------------------------
@@ -19,6 +19,9 @@
  *
  * Revision History:
  *  $Log$
+ *  Revision 1.5.26.1  2011/03/03 20:06:21  jones
+ *  Add check for 64bit by looking for LP64
+ *
  *  Revision 1.5  2003/02/21 20:55:24  saw
  *  Clean up some types and casts to reduce compiler warnings.
  *
