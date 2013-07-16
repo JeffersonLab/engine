0b55771b6cf23d8285aa4d41404e4314240e2fe6
Mark Jones <jones@jlab.org>
2013-07-16 12:18:01 -0400

    updated for inclusive analysis

--- ENGINE/c_ntuple_keep.f
+++ ENGINE/c_ntuple_keep.f
@@ -7,7 +7,16 @@
 *           : err        - reason for failure, if any
 *
 *     Created: 11-Apr-1994  K.B.Beard, Hampton U.
-* $Log$
+* $Log: c_ntuple_keep.f,v $
+* Revision 1.10.10.3  2004/07/12 03:07:34  cdaq
+* problem with mmx fixed?
+*
+* Revision 1.10.10.2  2004/06/23 19:32:25  cdaq
+* removed ssnum_fp and hsnum_fp from c ntuple
+*
+* Revision 1.10.10.1  2004/06/18 15:27:19  cdaq
+* replaced with fpi2 version
+*
 * Revision 1.10  2004/02/17 17:26:34  jones
 * Changes to enable possiblity of segmenting rzdat files
 *
@@ -68,10 +77,16 @@
       INCLUDE 'sos_calorimeter.cmn'
       INCLUDE 'hms_scin_tof.cmn'
       INCLUDE 'sos_scin_tof.cmn'
+      INCLUDE 'sos_physics_sing.cmn'  ! for online purpose xucc june 20,2003
+      INCLUDE 'hms_aero_parms.cmn'    ! for online purpose xucc june 21,2003
 *
       logical HEXIST    !CERNLIB function
 *
       integer m
+*     xucc added begin 
+      real*4 ztar_dummy,ztard1,ztard2,ddegrad
+      parameter (ddegrad=0.0174533)  ! Z target information
+*     xucc added end
 *
 *--------------------------------------------------------
       err= ' '
@@ -128,6 +143,16 @@ c
       m= m+1
       c_Ntuple_contents(m)= HSY_TAR     ! HMS Target
       m= m+1
+*    xucc added begin
+      ztard1=abs(htheta_lab)*ddegrad
+      ztard2=ztard1-HSYP_TAR
+      ztar_dummy=HSY_TAR*(sin(ztard1)+cos(ztard1)/tan(ztard2))
+      c_Ntuple_contents(m)= ztar_dummy   
+      m= m+1
+*     xucc added end
+
+      c_Ntuple_contents(m)= HSP	        ! Lab momentum
+      m= m+1
       c_Ntuple_contents(m)= HSXP_TAR    ! 
       m= m+1
       c_Ntuple_contents(m)= HSYP_TAR    ! 
@@ -135,6 +160,17 @@ c
       c_Ntuple_contents(m)= HSDELTA     !
       m= m+1
       c_Ntuple_contents(m)= SSY_TAR     ! SOS Target
+      m= m+1
+
+*     xucc added begin
+      ztard1=abs(stheta_lab)*ddegrad
+      ztard2=ztard1+SSYP_TAR
+      ztar_dummy=SSY_TAR*(sin(ztard1)+cos(ztard1)/tan(ztard2))
+      c_Ntuple_contents(m)= ztar_dummy   
+      m= m+1
+*     xucc added end 
+*      print *,'ssp2 =',ssp,' m= ',m
+      c_Ntuple_contents(m)= SSP	! Lab momentum of chosen track in GeV/c
       m= m+1
       c_Ntuple_contents(m)= SSXP_TAR    ! 
       m= m+1
@@ -144,13 +180,13 @@ c
       m= m+1
       c_Ntuple_contents(m)= HCER_NPE_SUM ! HMS Particle Id.
       m= m+1
-      c_Ntuple_contents(m)= HSSHSUM  !
+      c_Ntuple_contents(m)= HSSHSUM  !  xucc what's wrong with this?
       m= m+1
       c_Ntuple_contents(m)= HSSHTRK  !
       m= m+1
       c_Ntuple_contents(m)= HSPRTRK !
       m= m+1
-      c_Ntuple_contents(m)= HBETA_NOTRK !
+      c_Ntuple_contents(m)= HSBETA_P !
       m= m+1
       c_Ntuple_contents(m)= HSBETA      !
       m= m+1
@@ -158,26 +194,42 @@ c
       m= m+1
       c_Ntuple_contents(m)= SCER_NPE_SUM ! SOS Particle Id.
       m= m+1
-      c_Ntuple_contents(m)= SSSHSUM  !
+      c_Ntuple_contents(m)= SSSHSUM  ! xucc what's wrong with this again?
       m= m+1
       c_Ntuple_contents(m)= SSSHTRK  !
       m= m+1
       c_Ntuple_contents(m)= SSPRTRK !
       m= m+1
-      c_Ntuple_contents(m)= SBETA_NOTRK      !
+      c_Ntuple_contents(m)= SSBETA_P      !
       m= m+1
       c_Ntuple_contents(m)= SSBETA      !
       m= m+1
       c_Ntuple_contents(m)= SSDEDX(1)   !
       m= m+1
-      c_Ntuple_contents(m)= gbcm1_charge ! Charge of last scaler event
+      c_Ntuple_contents(m)= gbcm2_charge ! Charge of last scaler event
       m= m+1
       c_Ntuple_contents(m)= FLOAT(gen_event_ID_number)
+      m= m+1
+      c_Ntuple_contents(m)= FLOAT(gen_event_type)
       m= m+1
       c_Ntuple_contents(m)= cmissing_e  ! missing energy
       m= m+1
       c_Ntuple_contents(m)= cmissing_mass ! missing mass
       m= m+1
+* 
+*    xucc added begin
+*      c_Ntuple_contents(m)= ce_exc ! Excitation Energy
+*      m= m+1
+*      c_Ntuple_contents(m)= ce_excx ! Excitation Energy
+*      m= m+1
+*     xucc added end
+
+      c_Ntuple_contents(m)= cmex  ! missing energy
+      m= m+1
+      c_Ntuple_contents(m)= cmmx ! missing mass
+      m= m+1
+* 
+ 
       c_Ntuple_contents(m)= cmissing_mom ! Missing Momentum
       m= m+1
       c_Ntuple_contents(m)= cmissing_mom_par ! pm parallel to q
@@ -186,9 +238,92 @@ c
       m= m+1
       c_Ntuple_contents(m)= cmissing_mom_oop ! pm out of plane
       m= m+1
-      c_Ntuple_contents(m)= cthetapq
+      c_Ntuple_contents(m)= c_omega 
       m= m+1
-      c_Ntuple_contents(m)= cphipq
+      c_Ntuple_contents(m)= c_bigq2  ! q2?
+      m= m+1
+*    xucc added begin
+      c_Ntuple_contents(m)= c_invmass
+*    xucc added end
+
+      m= m+1
+      c_Ntuple_contents(m)= c_w2
+      m= m+1
+      c_Ntuple_contents(m)= X_bj
+      m= m+1
+      c_Ntuple_contents(m)= qabs
+      m= m+1
+      c_Ntuple_contents(m)= z_m
+      m= m+1
+      c_Ntuple_contents(m)= pt2
+
+
+      m= m+1
+      c_Ntuple_contents(m)= cmin_t
+      m= m+1
+      c_Ntuple_contents(m)= cthetapq 
+      m= m+1
+      c_Ntuple_contents(m)= cphipi ! originally cphipq 
+
+*    ! cphipq=asin(p_rot_y/p_rot_mag_check)/deg_rad
+*    ! Azimuthal angle of hadron about q, but in original code it never calculate it 
+*    ! anywhere so I change it to be cphipi
+*    !         cphipi = acos(p_new_x/sqrt(p_new_x**2+p_new_y**2))
+*    !         It is an angel about pi particle
+*    !         why is it p_new_x? p_new_y? not yet know. 
+
+*     xucc added begin
+      m= m+1
+      c_Ntuple_contents(m)= c_epsilon
+      m= m+1
+      c_Ntuple_contents(m)= c_gamma_v
+*    xucc added end
+
+
+* on June 21,2003, xucc added following for online purpose
+      m=m+1
+      c_Ntuple_contents(m)=  ssx_cal
+      m=m+1
+      c_ntuple_contents(m)=  ssy_cal
+      m=m+1
+      c_ntuple_contents(m)=  hsmass2
+      m=m+1
+      c_ntuple_contents(m)=  haero_pos_npe_sum
+      m=m+1
+      c_ntuple_contents(m)=  haero_neg_npe_sum
+      m=m+1
+      c_ntuple_contents(m)=  hcer_adc(1)
+      m=m+1
+      c_ntuple_contents(m)=  hcer_adc(2)
+      m=m+1
+      c_ntuple_contents(m)=  hsx_cer
+      m=m+1
+      c_ntuple_contents(m)=  (sspath_cor - hspath_cor)
+      m=m+1
+      c_ntuple_contents(m)=   sszbeam
+      m=m+1
+      c_ntuple_contents(m)=   hszbeam      
+      m=m+1
+      c_ntuple_contents(m)=   ctphix
+      m=m+1
+      c_ntuple_contents(m)=   ctphiy
+      m= m+1
+      c_Ntuple_contents(m)= HBETA_NOTRK 
+c     xempt adding on June 22 '04
+      m= m+1
+      c_Ntuple_contents(m)= HSTHETA ! Lab Scattering angle in radians
+      m= m+1
+      c_Ntuple_contents(m)= HSPHI	! Lab Scattering angle in radians
+      m= m+1
+      c_Ntuple_contents(m)= SSTHETA	! Lab Scattering angle in radians
+      m= m+1
+      c_Ntuple_contents(m)= SSPHI	! Lab Azymuthal angle in radians
+
+c       write(6,*)'ssz_beam =',sszbeam
+c       write(6,*)'hsmass2 =',hsmass2
+* end of xucc adding on June 21,2003
+
+
 
 *      m= m+1
 *      c_Ntuple_contents(m)= P_HMS_CORR  ! Corrected hms singles
